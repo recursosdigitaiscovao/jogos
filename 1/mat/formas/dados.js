59526d59bd3d7cd0717d9ad0da9119ac0098eedf
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-01 14:17:36 +0100

    Update dados.js

--- 1/mat/formas/dados.js
+++ 1/mat/formas/dados.js
@@ -1,1 +1,14 @@
-
+// dados.js
+const JOGO_CONFIG = {
+    // Caminho base para a pasta das imagens (ex: "img/" ou "" se estiverem na mesma pasta)
+    caminhoImg: "../../../assets/img/", 
+    
+    // Ícones que aparecem no Menu Lateral e no Cabeçalho
+    iconesMenu: {
+        pre: "icone_pre.png",
+        ano1: "icone_ano1.png",
+        ano2: "icone_ano2.png",
+        ano3: "icone_ano3.png",
+        ano4: "icone_ano4.png"
+    }
+};
