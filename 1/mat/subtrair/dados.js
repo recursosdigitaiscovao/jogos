36b49a4355c0ae03dca995fe3c0de4ba5e88564a
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-02 00:06:52 +0100

    Update dados.js

--- 1/mat/subtrair/dados.js
+++ 1/mat/subtrair/dados.js
@@ -1,1 +1,22 @@
-
+// dados.js
+const JOGO_CONFIG = {
+    caminhoIcons: "../../../icons/", // Três níveis acima
+    caminhoImg: "../../../img/",     // Três níveis acima
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    frutas: [
+        "ananas.png", "banana.png", "cereja.png", "kiwi.png", 
+        "laranja.png", "maca.png", "melancia.png", "morango.png", 
+        "papaia.png", "pera.png", "uvas.png"
+    ],
+    mensagens: {
+        excelente: "Incrível! És um mestre da matemática! 🌟",
+        bom: "Muito bem! Estás no bom caminho! 👍",
+        tenta: "Bom esforço! Continua a praticar! 💪"
+    }
+};
