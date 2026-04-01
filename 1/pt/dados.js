5ac1f96cdd723d1167a3f12fab267d02cf0251d9
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-01 20:52:18 +0100

    Update dados.js

--- 1/pt/dados.js
+++ 1/pt/dados.js
@@ -1,1 +1,14 @@
-
+// dados.js
+const JOGO_CONFIG = {
+    caminhoIcons: "../../icons/", // Pasta no nível anterior
+    icones: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png",
+        pt: "pt.png",
+        mat: "mat.png",
+        em: "em.png"
+    }
+};
