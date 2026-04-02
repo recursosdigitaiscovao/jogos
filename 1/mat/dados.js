f3b7232b3a9f55c20c6f6dafaf99e0d7b625bf26
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-02 23:23:38 +0100

    Update dados.js

--- 1/mat/dados.js
+++ 1/mat/dados.js
@@ -2,11 +2,24 @@
 const JOGO_CONFIG = {
     // Se estiveres na pasta /1/mat/, o caminho para /icons/ é ../../icons/
     caminhoIcons: "../../icons/", 
+    // Caminho para os icons dos jogos (mesma diretoria)
+    caminhoIconJogos: "./iconjogos/",
     icones: {
         pre: "iconpre.png",
         ano1: "icon1.png",
         ano2: "icon2.png",
         ano3: "icon3.png",
         ano4: "icon4.png"
+    },
+    // Mapeamento dos ficheiros da imagem que enviaste
+    iconesJogos: {
+        conta: "conta.png",
+        formas: "formas.png",
+        soma: "soma.png",
+        subtrair: "subtrair.png",
+        liga: "liga.png",
+        compara: "compara.png",
+        sequencias: "sequencias.png",
+        parouimpar: "parouimpar.png"
     }
 };
