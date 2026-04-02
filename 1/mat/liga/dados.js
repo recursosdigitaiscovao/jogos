69aefbd4adac53b3ae792b5bc52cf84cb1f78891
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-02 14:14:33 +0100

    Update dados.js

--- 1/mat/liga/dados.js
+++ 1/mat/liga/dados.js
@@ -1,1 +1,30 @@
-
+// dados.js
+const JOGO_CONFIG = {
+    caminhoIcons: "../../../icons/", // Três níveis acima
+    caminhoImg: "../../../img/",     // Três níveis acima
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    numeros: [
+        { valor: 0, palavra: "ZERO" },
+        { valor: 1, palavra: "UM" },
+        { valor: 2, palavra: "DOIS" },
+        { valor: 3, palavra: "TRÊS" },
+        { valor: 4, palavra: "QUATRO" },
+        { valor: 5, palavra: "CINCO" },
+        { valor: 6, palavra: "SEIS" },
+        { valor: 7, palavra: "SETE" },
+        { valor: 8, palavra: "OITO" },
+        { valor: 9, palavra: "NOVE" },
+        { valor: 10, palavra: "DEZ" }
+    ],
+    mensagens: {
+        excelente: "Incrível! És um mestre da matemática! 🌟",
+        bom: "Muito bem! Estás no bom caminho! 👍",
+        tenta: "Bom esforço! Continua a praticar! 💪"
+    }
+};
