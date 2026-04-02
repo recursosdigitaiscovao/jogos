5fca6b1365c540ff98ed4f8c400f0f375b523587
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-02 16:31:13 +0100

    Update dados.js

--- 1/mat/sequencias/dados.js
+++ 1/mat/sequencias/dados.js
@@ -1,1 +1,35 @@
-
+// dados.js
+const JOGO_CONFIG = {
+    caminhoIcons: "../../../icons/", 
+    caminhoImg: "../../../img/",     
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    numeros: [
+        { valor: 0, imagem: "zero.png" },
+        { valor: 1, imagem: "um.png" },
+        { valor: 2, imagem: "dois.png" },
+        { valor: 3, imagem: "tres.png" },
+        { valor: 4, imagem: "quatro.png" },
+        { valor: 5, imagem: "cinco.png" },
+        { valor: 6, imagem: "seis.png" },
+        { valor: 7, imagem: "sete.png" },
+        { valor: 8, imagem: "oito.png" },
+        { valor: 9, imagem: "nove.png" },
+        { valor: 10, imagem: "dez.png" }
+    ],
+    // Adicionado para este jogo
+    veiculos: {
+        trator: "trator.png",
+        reboque: "reboque.png"
+    },
+    mensagens: {
+        excelente: "Incrível! És um mestre da matemática! 🌟",
+        bom: "Muito bem! Estás no bom caminho! 👍",
+        tenta: "Bom esforço! Continua a praticar! 💪"
+    }
+};
