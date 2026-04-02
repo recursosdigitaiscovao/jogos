3ad5fed439a5cc177413b503af176c87d3e149fa
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-02 18:42:35 +0100

    Update dados.js

--- 1/mat/parouimpar/dados.js
+++ 1/mat/parouimpar/dados.js
@@ -1,1 +1,25 @@
-
+// dados.js
+const JOGO_CONFIG = {
+    caminhoIcons: "../../../icons/", 
+    caminhoImg: "../../../img/",     
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    // Imagens dos balões fornecidas
+    baloes: [
+        "balao_c.png",  // Ciano/Azul claro
+        "balao_vd.png", // Verde
+        "balao_a.png",  // Azul
+        "balao_v.png",  // Vermelho
+        "balao_l.png"   // Laranja
+    ],
+    mensagens: {
+        excelente: "Incrível! És um mestre dos números pares! 🌟",
+        bom: "Muito bem! Estás no bom caminho! 👍",
+        tenta: "Bom esforço! Continua a praticar! 💪"
+    }
+};
