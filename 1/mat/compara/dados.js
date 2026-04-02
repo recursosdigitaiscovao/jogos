ceb3069acdf65fd5c4720f5ca65cf399531e4975
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-02 16:02:56 +0100

    Update dados.js

--- 1/mat/compara/dados.js
+++ 1/mat/compara/dados.js
@@ -1,1 +1,29 @@
-
+const JOGO_CONFIG = {
+    caminhoIcons: "../../../icons/", 
+    caminhoImg: "../../../img/",     
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    numeros: [
+        { valor: 0, palavra: "ZERO", imagem: "zero.png" },
+        { valor: 1, palavra: "UM", imagem: "um.png" },
+        { valor: 2, palavra: "DOIS", imagem: "dois.png" },
+        { valor: 3, palavra: "TRÊS", imagem: "tres.png" },
+        { valor: 4, palavra: "QUATRO", imagem: "quatro.png" },
+        { valor: 5, palavra: "CINCO", imagem: "cinco.png" },
+        { valor: 6, palavra: "SEIS", imagem: "seis.png" },
+        { valor: 7, palavra: "SETE", imagem: "sete.png" },
+        { valor: 8, palavra: "OITO", imagem: "oito.png" },
+        { valor: 9, palavra: "NOVE", imagem: "nove.png" },
+        { valor: 10, palavra: "DEZ", imagem: "dez.png" }
+    ],
+    mensagens: {
+        excelente: "Incrível! És um mestre da matemática! 🌟",
+        bom: "Muito bem! Estás no bom caminho! 👍",
+        tenta: "Bom esforço! Continua a praticar! 💪"
+    }
+};
