622e3c19322ebf85d8dce193e9c2b4894b0c0709
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-03 03:04:54 +0100

    Update dados.js

--- 1/mat/dezenas/dados.js
+++ 1/mat/dezenas/dados.js
@@ -1,1 +1,23 @@
-
+// dados.js
+const JOGO_CONFIG = {
+    caminhoIcons: "../../icons/", 
+    caminhoImg: "../../img/",     
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    // Imagens para este jogo
+    veiculos: {
+        camiao: "camiao.png",
+        dezena: "barra.png",   // Barra de 10
+        unidade: "cubo.png"    // Cubo de 1
+    },
+    mensagens: {
+        excelente: "Incrível! És um mestre das dezenas! 🌟",
+        bom: "Muito bem! Estás no bom caminho! 👍",
+        tenta: "Bom esforço! Continua a praticar! 💪"
+    }
+};
