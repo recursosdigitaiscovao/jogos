e2fc64c8279faae9cbde2726442d670cea76115d
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-04 20:27:31 +0100

    Update dados.js

--- 1/mat/dados.js
+++ 1/mat/dados.js
@@ -16,14 +16,14 @@ const JOGO_CONFIG = {
 
     // Lista de jogos - O sistema vai criar os cards sozinho
     listaJogos: [
-        { nome: "Contar os Números", icon: "conta.png", link: "contar/" },
+        { nome: "Contar os Números", icon: "conta.png", link: "conta/" },
         { nome: "Formas Geométricas", icon: "formas.png", link: "formas/" },
-        { nome: "Somas Divertidas", icon: "soma.png", link: "somas/" },
-        { nome: "Subtração", icon: "subtrair.png", link: "subtracao/" },
-        { nome: "Liga os Pontos", icon: "liga.png", link: "ligar/" },
-        { nome: "Compara Números", icon: "compara.png", link: "comparar/" },
+        { nome: "Somas Divertidas", icon: "soma.png", link: "soma/" },
+        { nome: "Subtração", icon: "subtrair.png", link: "subtrair/" },
+        { nome: "Liga os Pontos", icon: "liga.png", link: "liga/" },
+        { nome: "Compara Números", icon: "compara.png", link: "compara/" },
         { nome: "Sequências", icon: "sequencias.png", link: "sequencias/" },
-        { nome: "Par ou Ímpar", icon: "parouimpar.png", link: "par-impar/" },
+        { nome: "Par ou Ímpar", icon: "parouimpar.png", link: "parouimpar/" },
         { nome: "As Dezenas", icon: "dezenas.png", link: "dezenas/" }
     ]
 };
