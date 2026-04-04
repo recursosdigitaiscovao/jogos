9098bae2d864ef3d65eb669a7392377c00cc7ac1
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-04 20:11:21 +0100

    Update dados.js

--- 1/mat/dados.js
+++ 1/mat/dados.js
@@ -1,29 +1,29 @@
 const JOGO_CONFIG = {
-    // Caminho para os icons do menu (subindo dois níveis até à pasta jogos/icons)
+    // Caminho para os icons do menu (sobe dois níveis para jogos/icons/)
     caminhoIconsMenu: "../../icons/",
     
-    // Caminho para os icons dos jogos (na mesma pasta do index)
+    // Caminho para os icons dos jogos (dentro da pasta local iconjogos/)
     caminhoIconsJogos: "iconjogos/",
 
-    // Nomes dos ficheiros de ícones do menu/sidebar
+    // Nomes dos ficheiros de ícones do menu/sidebar (conforme solicitado)
     iconesMenu: {
-           pre: "iconpre.png",
+        pre: "iconpre.png",
         ano1: "icon1.png",
         ano2: "icon2.png",
         ano3: "icon3.png",
         ano4: "icon4.png"
     },
 
-    // Lista de jogos desta página
+    // Lista de jogos - O sistema vai criar os cards sozinho
     listaJogos: [
         { nome: "Contar os Números", icon: "conta.png", link: "contar/" },
         { nome: "Formas Geométricas", icon: "formas.png", link: "formas/" },
-        { nome: "Somas Divertidas", icon: "soma.png", link: "soma/" },
-        { nome: "Subtração", icon: "subtrair.png", link: "subtrair/" },
-        { nome: "Liga os Pontos", icon: "liga.png", link: "liga/" },
-        { nome: "Compara Números", icon: "compara.png", link: "compara/" },
+        { nome: "Somas Divertidas", icon: "soma.png", link: "somas/" },
+        { nome: "Subtração", icon: "subtrair.png", link: "subtracao/" },
+        { nome: "Liga os Pontos", icon: "liga.png", link: "ligar/" },
+        { nome: "Compara Números", icon: "compara.png", link: "comparar/" },
         { nome: "Sequências", icon: "sequencias.png", link: "sequencias/" },
-        { nome: "Par ou Ímpar", icon: "parouimpar.png", link: "parouimpar/" },
+        { nome: "Par ou Ímpar", icon: "parouimpar.png", link: "par-impar/" },
         { nome: "As Dezenas", icon: "dezenas.png", link: "dezenas/" }
     ]
 };
