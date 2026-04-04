4bf1364291396e45a8cc6e8e944f3f71c7d5e7a6
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-04 23:42:57 +0100

    Update dados.js

--- 1/mat/formas/dados.js
+++ 1/mat/formas/dados.js
@@ -1,11 +1,13 @@
 // dados.js
 const JOGO_CONFIG = {
-    caminhoIcons: "../../../icons/", // Três níveis acima
-    icones: {
+    // Caminho para os icons do menu (sobe dois níveis até à pasta jogos/icons/)
+    caminhoIcons: "../../../icons/",
+    
+    // Nomes dos ficheiros de ícones do menu/sidebar e título
+    iconesMenu: {
         pre: "iconpre.png",
         ano1: "icon1.png",
         ano2: "icon2.png",
         ano3: "icon3.png",
         ano4: "icon4.png"
-    }
-};
+    },
