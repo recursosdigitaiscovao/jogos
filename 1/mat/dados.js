530525e2acf1497d3b02ddcdc6a2134017eebade
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-04 20:04:45 +0100

    Update dados.js

--- 1/mat/dados.js
+++ 1/mat/dados.js
@@ -7,11 +7,11 @@ const JOGO_CONFIG = {
 
     // Nomes dos ficheiros de ícones do menu/sidebar
     iconesMenu: {
-        pre: "pre.png",
-        ano1: "ano1.png",
-        ano2: "ano2.png",
-        ano3: "ano3.png",
-        ano4: "ano4.png"
+           pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
     },
 
     // Lista de jogos desta página
