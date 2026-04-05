38c734638c48532f9d10add32055d12059c3e261
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-05 12:55:46 +0100

    Update dados.js

--- 1/mat/teste1/dados.js
+++ 1/mat/teste1/dados.js
@@ -1,1 +1,41 @@
+// dados.js
+const JOGO_CONFIG = {
+    caminhoIcons: "../../../icons/",
+    caminhoImg: "../../../img/frutas/", 
+    caminhoAnimais: "../../../img/animaisdomesticos/", 
 
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+
+    animais: [
+        "galinha.png", "cabra.png", "ovelha.png", "coelho.png", 
+        "burro.png", "peru.png", "porco.png", "cavalo.png", 
+        "vaca.png", "pato.png", "gato.png", "cao.png"
+    ],
+
+    frutas: [
+        "ananas.png", "banana.png", "cereja.png", "kiwi.png", 
+        "laranja.png", "maca.png", "melancia.png", "morango.png", 
+        "papaia.png", "pera.png", "uvas.png"
+    ],
+
+    mensagens: {
+        excelente: "Incrível! És um mestre da matemática! 🌟",
+        bom: "Bom esforço! Estás no bom caminho! 👍",
+        tenta: "Esforça-te mais! Continua a praticar! 💪"
+    },
+
+    relatorio: {
+        titulo: "Fim do Jogo!",
+        pontosTotal: "Pontuação Total:",
+        tempoTotal: "Tempo Total:",
+        labelNivel: "Nível",
+        labelCertas: "Certas",
+        labelErradas: "Erradas"
+    }
+};
