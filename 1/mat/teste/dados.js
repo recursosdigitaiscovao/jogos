e36e9e64aaf7e97de7086eda73fd9e2c82d43b39
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-05 02:58:25 +0100

    Update dados.js

--- 1/mat/teste/dados.js
+++ 1/mat/teste/dados.js
@@ -1,1 +1,24 @@
-
+// dados.js
+const JOGO_CONFIG = {
+    caminhoIcons: "../../../icons/",
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    mensagens: {
+        excelente: "Incrível! És um mestre das coordenadas! 🌟",
+        bom: "Bom esforço! Estás no bom caminho! 👍",
+        tenta: "Esforça-te mais! Continua a praticar! 💪"
+    },
+    relatorio: {
+        titulo: "Fim do Jogo!",
+        pontosTotal: "Pontuação Total:",
+        tempoTotal: "Tempo Total:",
+        labelNivel: "Nível",
+        labelCertas: "Certas",
+        labelErradas: "Erradas"
+    }
+};
