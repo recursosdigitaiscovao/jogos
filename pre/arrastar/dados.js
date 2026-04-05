44de9a44759ab7066c1864a53822c450240af27f
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-06 00:23:17 +0100

    Update dados.js

--- pre/arrastar/dados.js
+++ pre/arrastar/dados.js
@@ -1,1 +1,42 @@
+// dados.js
+const JOGO_CONFIG = {
+    caminhoIcons: "../../../icons/",
+    caminhoGeralImg: "../../../img/",
 
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+
+    // Categorias para o jogo de sombras
+    categorias: {
+        animais: {
+            nome: "Animais",
+            caminho: "../../../img/animaisdomesticos/",
+            lista: ["galinha.png", "cabra.png", "ovelha.png", "coelho.png", "burro.png", "peru.png", "porco.png", "cavalo.png", "vaca.png", "pato.png", "gato.png", "cao.png"]
+        },
+        frutas: {
+            nome: "Frutas",
+            caminho: "../../../img/frutas/",
+            lista: ["ananas.png", "banana.png", "cereja.png", "kiwi.png", "laranja.png", "maca.png", "melancia.png", "morango.png", "papaia.png", "pera.png", "uvas.png"]
+        }
+    },
+
+    mensagens: {
+        excelente: "Incrível! És um mestre! 🌟",
+        bom: "Bom esforço! Estás no bom caminho! 👍",
+        tenta: "Esforça-te mais! Continua a praticar! 💪"
+    },
+
+    relatorio: {
+        titulo: "Fim do Jogo!",
+        pontosTotal: "Pontuação Total:",
+        tempoTotal: "Tempo Total:",
+        labelNivel: "Nível",
+        labelCertas: "Certas",
+        labelErradas: "Erradas"
+    }
+};
