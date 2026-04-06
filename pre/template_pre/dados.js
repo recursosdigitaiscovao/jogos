0b97b3b70032621b58c325f6ffd1488328a00e70
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-06 16:33:57 +0100

    Update dados.js

--- pre/template_pre/dados.js
+++ pre/template_pre/dados.js
@@ -1,1 +1,26 @@
+const JOGO_CONFIG = {
+    caminhoIcons: "../../icons/",
+    
+    // Ícones para o Menu Lateral (Hambúrguer)
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
 
+    // Caminho para as imagens do trator/reboque (usadas na animação)
+    caminhoImg: "../../img/geral/", 
+    
+    veiculos: {
+        trator: "trator.png",
+        reboque: "reboque.png"
+    },
+    
+    numeros: [
+        { valor: 3, imagem: "n3.png" },
+        { valor: 4, imagem: "n4.png" },
+        { valor: 5, imagem: "n5.png" }
+    ]
+};
