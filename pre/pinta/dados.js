05b33ce093be434867f9dfd2a0e78ef14e687f3f
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-07 13:48:52 +0100

    Update dados.js

--- pre/pinta/dados.js
+++ pre/pinta/dados.js
@@ -1,6 +1,6 @@
 const JOGO_CONFIG = {
     caminhoIcons: "../../icons/", 
-    caminhoImg: "../../img/",
+    caminhoImg: "../../img/", // O rd.png será buscado aqui
     iconesMenu: {
         pre: "iconpre.png",
         ano1: "icon1.png",
@@ -8,30 +8,15 @@ const JOGO_CONFIG = {
         ano3: "icon3.png",
         ano4: "icon4.png"
     },
-    // No jogo de colorir, as categorias agora representam os níveis
+    // Níveis definidos como categorias para aproveitar o menu RD
     categorias: {
-        nivel1: {
-            nome: "Nível 1 (3x3)",
-            tamanho: 3,
-            caminho: "../../img/niveis/", // Apenas para manter a estrutura
-            lista: ["nivel1.png"]
-        },
-        nivel2: {
-            nome: "Nível 2 (4x4)",
-            tamanho: 4,
-            caminho: "../../img/niveis/",
-            lista: ["nivel2.png"]
-        },
-        nivel3: {
-            nome: "Nível 3 (5x5)",
-            tamanho: 5,
-            caminho: "../../img/niveis/",
-            lista: ["nivel3.png"]
-        }
+        nivel1: { nome: "Nível 1 (3x3)", tamanho: 3 },
+        nivel2: { nome: "Nível 2 (4x4)", tamanho: 4 },
+        nivel3: { nome: "Nível 3 (5x5)", tamanho: 5 }
     },
-    coresPaleta: ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#FF9800", "#795548", "#000000"],
+    coresJogo: ["#ff4757", "#2ed573", "#1e90ff", "#ffa502", "#ffffff"], // Vermelho, Verde, Azul, Laranja, Branco
     relatorio: {
-        titulo: "Boa! Grelha Completa!",
+        titulo: "Excelente Trabalho!",
         pontosTotal: "Pontos:",
         tempoTotal: "Tempo:"
     }
