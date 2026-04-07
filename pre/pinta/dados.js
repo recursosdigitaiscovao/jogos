afd2dd829a6f2736ade711e3e5fd9b41fbca0344
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-07 13:53:26 +0100

    Update dados.js

--- pre/pinta/dados.js
+++ pre/pinta/dados.js
@@ -1,6 +1,6 @@
 const JOGO_CONFIG = {
     caminhoIcons: "../../icons/", 
-    caminhoImg: "../../img/", // O rd.png será buscado aqui
+    caminhoImg: "../../img/",
     iconesMenu: {
         pre: "iconpre.png",
         ano1: "icon1.png",
@@ -8,15 +8,26 @@ const JOGO_CONFIG = {
         ano3: "icon3.png",
         ano4: "icon4.png"
     },
-    // Níveis definidos como categorias para aproveitar o menu RD
     categorias: {
-        nivel1: { nome: "Nível 1 (3x3)", tamanho: 3 },
-        nivel2: { nome: "Nível 2 (4x4)", tamanho: 4 },
-        nivel3: { nome: "Nível 3 (5x5)", tamanho: 5 }
+        nivel1: { 
+            nome: "Nível 1 (3x3)", 
+            tamanho: 3, 
+            img: "nivel1.png" // Imagem que aparecerá no menu RD
+        },
+        nivel2: { 
+            nome: "Nível 2 (4x4)", 
+            tamanho: 4, 
+            img: "nivel2.png" 
+        },
+        nivel3: { 
+            nome: "Nível 3 (5x5)", 
+            tamanho: 5, 
+            img: "nivel3.png" 
+        }
     },
-    coresJogo: ["#ff4757", "#2ed573", "#1e90ff", "#ffa502", "#ffffff"], // Vermelho, Verde, Azul, Laranja, Branco
+    coresPaleta: ["#ff4757", "#2ed573", "#1e90ff", "#ffa502", "#ffffff"],
     relatorio: {
-        titulo: "Excelente Trabalho!",
+        titulo: "Fim do Jogo!",
         pontosTotal: "Pontos:",
         tempoTotal: "Tempo:"
     }
