073ec4b36b81930117861a2529040085f6f08459
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-07 17:12:42 +0100

    Update dados.js

--- pre/monstrocomilao/dados.js
+++ pre/monstrocomilao/dados.js
@@ -1,1 +1,30 @@
-
+const JOGO_CONFIG = {
+    caminhoImg: "img/",
+    caminhoIcons: "img/icons/",
+    iconesMenu: {
+        pre: "pre.png",
+        ano1: "1.png",
+        ano2: "2.png",
+        ano3: "3.png",
+        ano4: "4.png"
+    },
+    categorias: {
+        nivel1: {
+            nome: "Nível 1",
+            img: "monstro.png",
+            maxNum: 5,
+            quantidadeOpcoes: 4
+        },
+        nivel2: {
+            nome: "Nível 2",
+            img: "monstro.png",
+            maxNum: 10,
+            quantidadeOpcoes: 6
+        }
+    },
+    relatorio: {
+        titulo: "Muito Bem!",
+        pontosTotal: "Acertaste:",
+        tempoTotal: "Tempo:"
+    }
+};
