ef6154d83646a3e6f78541b444b0792efa15bed0
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-08 19:19:33 +0100

    Update dados.js

--- pre/sem_par/dados.js
+++ pre/sem_par/dados.js
@@ -1,1 +1,33 @@
-
+const JOGO_CONFIG = {
+    caminhoIcons: "../../icons/", // Ajustar conforme necessário
+    caminhoImg: "../../img/",
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    categorias: {
+        animais: {
+            nome: "Animais",
+            caminho: "../../img/animaisdomesticos/",
+            lista: ["galinha.png", "cabra.png", "ovelha.png", "coelho.png", "burro.png", "peru.png", "porco.png", "cavalo.png", "vaca.png", "pato.png", "gato.png", "cao.png"]
+        },
+        frutas: {
+            nome: "Frutas",
+            caminho: "../../img/frutas/",
+            lista: ["ananas.png", "banana.png", "cereja.png", "kiwi.png", "laranja.png", "maca.png", "melancia.png", "morango.png", "papaia.png", "pera.png", "uvas.png"]
+        },
+        materialescolar: {
+            nome: "Material Escolar",
+            caminho: "../../img/materialescolar/",
+            lista: ["cola.png", "folha.png", "tesoura.png", "capa.png", "estojo.png", "livro.png", "esquadro.png", "regua.png", "mochila.png", "borracha.png", "caneta.png", "pincel.png", "lapis.png", "afia.png"]
+        }
+    },
+    relatorio: {
+        titulo: "Fim do Jogo!",
+        pontosTotal: "Pontos:",
+        tempoTotal: "Tempo:"
+    }
+};
