c02c998d6b93a4075d46abf345dd838ded65499c
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-08 13:09:54 +0100

    Update dados.js

--- pre/procurar/dados.js
+++ pre/procurar/dados.js
@@ -1,1 +1,51 @@
-
+const JOGO_CONFIG = {
+    caminhoIcons: "../../icons/", 
+    caminhoImg: "../../img/",
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    categorias: {
+        animais: {
+            nome: "Animais",
+            caminho: "../../img/animaisdomesticos/",
+            lista: ["galinha.png", "cabra.png", "ovelha.png", "coelho.png", "burro.png", "peru.png", "porco.png", "cavalo.png", "vaca.png", "pato.png", "gato.png", "cao.png"]
+        },
+        frutas: {
+            nome: "Frutas",
+            caminho: "../../img/frutas/",
+            lista: ["ananas.png", "banana.png", "cereja.png", "kiwi.png", "laranja.png", "maca.png", "melancia.png", "morango.png", "papaia.png", "pera.png", "uvas.png"]
+        },
+        materialescolar: {
+            nome: "Material Escolar",
+            caminho: "../../img/materialescolar/",
+            lista: ["cola.png", "folha.png", "tesoura.png", "capa.png", "estojo.png", "livro.png", "esquadro.png", "regua.png", "mochila.png", "borracha.png", "caneta.png", "pincel.png", "lapis.png", "afia.png"]
+        },
+        numeros: {
+            nome: "Números",
+            iconeEspecial: "123.png", // Ícone localizado em ../../img/
+            caminho: "../../img/numeros/",
+            lista: ["zero.png", "um.png", "dois.png", "tres.png", "quatro.png", "cinco.png", "seis.png", "sete.png", "oito.png", "nove.png", "dez.png"]
+        },
+        letras: {
+            nome: "Letras",
+            iconeEspecial: "abc.png", // Ícone localizado em ../../img/
+            caminho: "../../img/letras/",
+            lista: [
+                "letra_a.png", "letra_b.png", "letra_c.png", "letra_d.png", "letra_e.png", 
+                "letra_f.png", "letra_g.png", "letra_h.png", "letra_i.png", "letra_j.png", 
+                "letra_k.png", "letra_l.png", "letra_m.png", "letra_n.png", "letra_o.png", 
+                "letra_p.png", "letra_q.png", "letra_r.png", "letra_s.png", "letra_t.png", 
+                "letra_u.png", "letra_v.png", "letra_w.png", "letra_x.png", "letra_y.png", "letra_z.png"
+            ]
+        }
+    },
+    relatorio: {
+        titulo: "Fim do Jogo!",
+        pontosTotal: "Pontos:",
+        tempoTotal: "Tempo:"
+    }
+};
