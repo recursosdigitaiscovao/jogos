99b3d67de10bbfa6c9531940eb162821a9d3af52
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-08 19:37:06 +0100

    Update dados.js

--- pre/dados.js
+++ pre/dados.js
@@ -1,11 +1,25 @@
-// dados.js
 const JOGO_CONFIG = {
-    caminhoIcons: "../icons/", // Caminho para a pasta um nível acima
-    icones: {
+    // Caminho para os icons do menu (sobe dois níveis para jogos/icons/)
+    caminhoIconsMenu: "../../icons/",
+    
+    // Caminho para os icons dos jogos (dentro da pasta local iconjogos/)
+    caminhoIconsJogos: "iconjogos/",
+
+    // Nomes dos ficheiros de ícones do menu/sidebar (conforme solicitado)
+    iconesMenu: {
         pre: "iconpre.png",
         ano1: "icon1.png",
         ano2: "icon2.png",
         ano3: "icon3.png",
         ano4: "icon4.png"
-    }
+    },
+
+    // Lista de jogos atualizada com os novos ficheiros
+    listaJogos: [
+        { nome: "Encontra o Par", icon: "encontraopar.png", link: "encontraopar/" },
+        { nome: "Monstro Comilão", icon: "mostrocomilao.png", link: "mostrocomilao/" },
+        { nome: "Pintar", icon: "pinta.png", link: "pinta/" },
+        { nome: "Sem Par", icon: "sem_par.png", link: "sem_par/" },
+        { nome: "Sombras Mágicas", icon: "sombrasmagicas.png", link: "sombrasmagicas/" }
+    ]
 };
