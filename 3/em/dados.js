63b823f2f01a036d4d08a07007f5120e1ea11605
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-09 20:02:07 +0100

    Update dados.js

--- 3/em/dados.js
+++ 3/em/dados.js
@@ -1,1 +1,26 @@
+const JOGO_CONFIG = {
+    // Caminho para os icons do menu (sobe dois níveis para jogos/icons/)
+    caminhoIconsMenu: "../../icons/",
+    
+    // Caminho para os icons dos jogos (dentro da pasta local iconjogos/)
+    caminhoIconsJogos: "iconjogos/",
 
+    // Nomes dos ficheiros de ícones do menu/sidebar (conforme solicitado)
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+
+    // Lista de jogos - O sistema vai criar os cards sozinho
+    listaJogos: [
+        { nome: "jogo1", icon: "1.png", link: "nome1/" },
+        { nome: "jogo2", icon: "2.png", link: "nome2/" },
+        { nome: "jogo3", icon: "3.png", link: "nome3/" },
+        { nome: "jogo4", icon: "4.png", link: "nome4/" },
+        { nome: "jogo5", icon: "5.png", link: "nome5/" },
+
+    ]
+};
