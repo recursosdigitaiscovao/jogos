b2f0f10137be97000ba894c519c477ccd55b0351
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-10 00:31:28 +0100

    Update dados.js

--- pre/labirinto/dados.js
+++ pre/labirinto/dados.js
@@ -8,27 +8,23 @@ const JOGO_CONFIG = {
         ano3: "icon3.png",
         ano4: "icon4.png"
     },
-    // Configuração específica para o labirinto
     labirinto: {
         caminho: "../../img/labirinto/",
         categorias: {
             cao: {
                 nome: "Cão",
                 sprite: "l_cao.png",
-                objetivo: "l_comida_cao.png",
-                parede: "l_parede_cao.png"
+                objetivo: "l_comida_cao.png"
             },
             gato: {
                 nome: "Gato",
                 sprite: "l_gato.png",
-                objetivo: "l_comida_gato.png",
-                parede: "l_parede_gato.png"
+                objetivo: "l_comida_gato.png"
             },
             passaro: {
                 nome: "Pássaro",
                 sprite: "l_passaro.png",
-                objetivo: "l_comida_passaro.png",
-                parede: "l_parede_passaro.png"
+                objetivo: "l_comida_passaro.png"
             }
         }
     },
