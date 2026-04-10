4764afa65c4edf4eebab59a40a0ab82c3dc86250
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-10 18:42:04 +0100

    Update dados.js

--- 1/pt/silabas/dados.js
+++ 1/pt/silabas/dados.js
@@ -1,1 +1,76 @@
-
+const JOGO_CONFIG = {
+    caminhoImg: "../../img/",
+    caminhoIcons: "../../icons/",
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    categorias: {
+        animais: {
+            nome: "Animais",
+            img: "animaisdomesticos/gato.png",
+            pasta: "animaisdomesticos/",
+            itens: [
+                { nome: "BURRO", img: "burro.png", silabas: 2 },
+                { nome: "CABRA", img: "cabra.png", silabas: 2 },
+                { nome: "CÃO", img: "cao.png", silabas: 1 },
+                { nome: "CAVALO", img: "cavalo.png", silabas: 3 },
+                { nome: "COELHO", img: "coelho.png", silabas: 3 },
+                { nome: "GALINHA", img: "galinha.png", silabas: 3 },
+                { nome: "GATO", img: "gato.png", silabas: 2 },
+                { nome: "OVELHA", img: "ovelha.png", silabas: 3 },
+                { nome: "PATO", img: "pato.png", silabas: 2 },
+                { nome: "PERU", img: "peru.png", silabas: 2 },
+                { nome: "PORCO", img: "porco.png", silabas: 2 },
+                { nome: "VACA", img: "vaca.png", silabas: 2 }
+            ]
+        },
+        material: {
+            nome: "Material Escolar",
+            img: "materialescolar/mochila.png",
+            pasta: "materialescolar/",
+            itens: [
+                { nome: "AFIA", img: "afia.png", silabas: 2 },
+                { nome: "BORRACHA", img: "borracha.png", silabas: 3 },
+                { nome: "CANETA", img: "caneta.png", silabas: 3 },
+                { nome: "CAPA", img: "capa.png", silabas: 2 },
+                { nome: "COLA", img: "cola.png", silabas: 2 },
+                { nome: "ESQUADRO", img: "esquadro.png", silabas: 3 },
+                { nome: "ESTOJO", img: "estojo.png", silabas: 3 },
+                { nome: "FOLHA", img: "folha.png", silabas: 2 },
+                { nome: "LÁPIS", img: "lapis.png", silabas: 2 },
+                { nome: "LIVRO", img: "livro.png", silabas: 2 },
+                { nome: "MOCHILA", img: "mochila.png", silabas: 3 },
+                { nome: "PINCEL", img: "pincel.png", silabas: 2 },
+                { nome: "RÉGUA", img: "regua.png", silabas: 2 },
+                { nome: "TESOURA", img: "tesoura.png", silabas: 3 }
+            ]
+        },
+        frutas: {
+            nome: "Frutas",
+            img: "frutas/maca.png",
+            pasta: "frutas/",
+            itens: [
+                { nome: "ANANÁS", img: "ananas.png", silabas: 3 },
+                { nome: "BANANA", img: "banana.png", silabas: 3 },
+                { nome: "CEREJA", img: "cereja.png", silabas: 3 },
+                { nome: "KIWI", img: "kiwi.png", silabas: 2 },
+                { nome: "LARANJA", img: "laranja.png", silabas: 3 },
+                { nome: "MAÇÃ", img: "maca.png", silabas: 2 },
+                { nome: "MELANCIA", img: "melancia.png", silabas: 4 },
+                { nome: "MORANGO", img: "morango.png", silabas: 3 },
+                { nome: "PAPAIA", img: "papaia.png", silabas: 3 },
+                { nome: "PERA", img: "pera.png", silabas: 2 },
+                { nome: "UVAS", img: "uvas.png", silabas: 2 }
+            ]
+        }
+    },
+    relatorio: {
+        titulo: "Incrível!",
+        pontosTotal: "Acertaste:",
+        tempoTotal: "Tempo:"
+    }
+};
