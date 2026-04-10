1d19b0ae76a095a40f835de09901e42f57883c84
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-10 12:03:28 +0100

    Update dados.js

--- pre/labirinto/dados.js
+++ pre/labirinto/dados.js
@@ -12,13 +12,22 @@ const JOGO_CONFIG = {
         caminho: "../../img/labirinto/",
         obstaculos: ["pedra.png", "buraco.png"],
         categorias: {
-            cao: { nome: "Cão", sprite: "l_cao.png", objetivo: "l_comida_cao.png", corParede: "#e691a7" },
-            gato: { nome: "Gato", sprite: "l_gato.png", objetivo: "l_comida_gato.png", corParede: "#d47a92" },
-            passaro: { nome: "Pássaro", sprite: "l_passaro.png", objetivo: "l_comida_passaro.png", corParede: "#8db596" }
+            cao: { 
+                nome: "Cão", sprite: "l_cao.png", objetivo: "l_comida_cao.png", 
+                corLabirinto: "#3498db" // Azul para o cão
+            },
+            gato: { 
+                nome: "Gato", sprite: "l_gato.png", objetivo: "l_comida_gato.png", 
+                corLabirinto: "#e67e22" // Laranja para o gato
+            },
+            passaro: { 
+                nome: "Pássaro", sprite: "l_passaro.png", objetivo: "l_comida_passaro.png", 
+                corLabirinto: "#27ae60" // Verde para o pássaro
+            }
         }
     },
     relatorio: {
-        titulo: "Fim da Aventura!",
+        titulo: "Missão Cumprida!",
         pontosTotal: "Pontos:",
         tempoTotal: "Tempo:"
     }
