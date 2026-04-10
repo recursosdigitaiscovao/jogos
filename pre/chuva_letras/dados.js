899efac25db00b8a744b7984f5efb436678f6f07
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-10 12:35:23 +0100

    Update dados.js

--- pre/chuva_letras/dados.js
+++ pre/chuva_letras/dados.js
@@ -1,1 +1,23 @@
-
+const JOGO_CONFIG = {
+    caminhoIcons: "../../icons/", 
+    caminhoImg: "../../img/",
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    letras: {
+        categorias: {
+            vogais: { nome: "Vogais", lista: "AEIOU" },
+            consoantes: { nome: "Consoantes", lista: "BCDFGHJKLMNPQRSTVWXYZ" },
+            alfabeto: { nome: "Alfabeto", lista: "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
+        }
+    },
+    relatorio: {
+        titulo: "Excelente Trabalho!",
+        pontosTotal: "Pontos:",
+        tempoTotal: "Tempo:"
+    }
+};
