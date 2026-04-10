08805f9090411869faedc5f69bcd6b648a851c72
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-10 14:21:26 +0100

    Update dados.js

--- pre/chuva_letras/dados.js
+++ pre/chuva_letras/dados.js
@@ -1,1 +1,27 @@
-
+const JOGO_CONFIG = {
+    caminhoIcons: "../../icons/", 
+    caminhoImg: "../../img/",
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    letras: {
+        caminho: "../../img/chuva_letras/",
+        categorias: {
+            vogais_min: { nome: "Vogais (abc)", lista: "aeiou", modo: "min", img: "vogais_min.png" },
+            vogais_mai: { nome: "Vogais (ABC)", lista: "AEIOU", modo: "mai", img: "vogais_mai.png" },
+            consoantes_min: { nome: "Consoantes (abc)", lista: "bcdfghjklmnpqrstvwxyz", modo: "min", img: "consoantes_min.png" },
+            consoantes_mai: { nome: "Consoantes (ABC)", lista: "BCDFGHJKLMNPQRSTVWXYZ", modo: "mai", img: "consoantes_mai.png" },
+            alfabeto_min: { nome: "Alfabeto (abc)", lista: "abcdefghijklmnopqrstuvwxyz", modo: "min", img: "alfabeto_min.png" },
+            alfabeto_mai: { nome: "Alfabeto (ABC)", lista: "ABCDEFGHIJKLMNOPQRSTUVWXYZ", modo: "mai", img: "alfabeto_mai.png" }
+        }
+    },
+    relatorio: {
+        titulo: "Excelente Trabalho!",
+        pontosTotal: "Pontos:",
+        tempoTotal: "Tempo:"
+    }
+};
