1f2835df3823f697f08ffdc82ba3ce988bc651e6
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-11 14:44:23 +0100

    Update dados.js

--- 1/pt/sl00/dados.js
+++ 1/pt/sl00/dados.js
@@ -1,1 +1,17 @@
-
+const JOGO_CONFIG = {
+    caminhoImg: "../../../img/",
+    caminhoIcons: "../../../icons/",
+    iconesMenu: { pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" },
+    categorias: {
+        animais: {
+            nome: "Animais",
+            img: "animaisdomesticos/vaca.png",
+            itens: [
+                { letras: "CAO", palavras: ["CAO"] },
+                { letras: "GATO", palavras: ["GATO"] },
+                { letras: "PATO", palavras: ["PATO"] }
+            ]
+        }
+    },
+    relatorio: { titulo: "MUITO BEM!", pontosTotal: "Palavras:", tempoTotal: "Tempo:" }
+};
