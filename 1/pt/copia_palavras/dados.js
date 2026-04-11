96d395a1a007e1ef96cd3e07bac84d40c0e3c170
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-12 00:11:53 +0100

    Update dados.js

--- 1/pt/copia_palavras/dados.js
+++ 1/pt/copia_palavras/dados.js
@@ -1,1 +1,69 @@
-
+const JOGO_CONFIG = {
+    caminhoImg: "../../../img/",
+    caminhoIcons: "../../../icons/",
+    caminhoLetras: "../../../img/letras/", // Onde estão as imagens das letras (A.png, B.png...)
+    caminhoObjetos: "../../../img/objetos/", // Onde estão as imagens dos itens
+    iconesMenu: { 
+        pre: "iconpre.png", 
+        ano1: "icon1.png", 
+        ano2: "icon2.png", 
+        ano3: "icon3.png", 
+        ano4: "icon4.png" 
+    },
+    categorias: {
+        A: {
+            nome: "Letra A", 
+            img: "A.png",
+            itens: [
+                { palavra: "AVIÃO", img: "aviao.png" }, { palavra: "ABELHA", img: "abelha.png" },
+                { palavra: "ANEL", img: "anel.png" }, { palavra: "ARANHA", img: "aranha.png" },
+                { palavra: "ÁRVORE", img: "arvore.png" }, { palavra: "ABACAXI", img: "abacaxi.png" },
+                { palavra: "APITO", img: "apito.png" }, { palavra: "ARCO-ÍRIS", img: "arcoiris.png" },
+                { palavra: "ALFACE", img: "alface.png" }, { palavra: "ALMOFADA", img: "almofada.png" },
+                { palavra: "ASTRONAUTA", img: "astronauta.png" }, { palavra: "AMORA", img: "amora.png" },
+                { palavra: "ANANÁS", img: "ananas.png" }, { palavra: "AVENTAL", img: "avental.png" },
+                { palavra: "AGULHA", img: "agulha.png" }, { palavra: "ALHO", img: "alho.png" },
+                { palavra: "AZUL", img: "azul.png" }, { palavra: "ALICATE", img: "alicate.png" },
+                { palavra: "ÂNCORA", img: "ancora.png" }, { palavra: "ANTENA", img: "antena.png" }
+            ]
+        },
+        B: {
+            nome: "Letra B", 
+            img: "B.png",
+            itens: [
+                { palavra: "BOLA", img: "bola.png" }, { palavra: "BONECA", img: "boneca.png" },
+                { palavra: "BALEIA", img: "baleia.png" }, { palavra: "BANANA", img: "banana.png" },
+                { palavra: "BARCO", img: "barco.png" }, { palavra: "BALÃO", img: "balao.png" },
+                { palavra: "BOTA", img: "bota.png" }, { palavra: "BICICLETA", img: "bicicleta.png" },
+                { palavra: "BORBOLETA", img: "borboleta.png" }, { palavra: "BATATA", img: "batata.png" },
+                { palavra: "BOCA", img: "boca.png" }, { palavra: "BALDE", img: "balde.png" },
+                { palavra: "BIGODE", img: "bigode.png" }, { palavra: "BOLO", img: "bolo.png" },
+                { palavra: "BULE", img: "bule.png" }, { palavra: "BURRO", img: "burro.png" },
+                { palavra: "BANCO", img: "banco.png" }, { palavra: "BACALHAU", img: "bacalhau.png" },
+                { palavra: "BERINGELA", img: "beringela.png" }, { palavra: "BINÓCULOS", img: "binoculos.png" }
+            ]
+        },
+        C: {
+            nome: "Letra C", 
+            img: "C.png",
+            itens: [
+                { palavra: "CASA", img: "casa.png" }, { palavra: "CARRO", img: "carro.png" },
+                { palavra: "CAVALO", img: "cavalo.png" }, { palavra: "COELHO", img: "coelho.png" },
+                { palavra: "CADERNO", img: "caderno.png" }, { palavra: "CAMISA", img: "camisa.png" },
+                { palavra: "CEREJA", img: "cereja.png" }, { palavra: "CABRA", img: "cabra.png" },
+                { palavra: "CADEIRA", img: "cadeira.png" }, { palavra: "COPO", img: "copo.png" },
+                { palavra: "CAMA", img: "cama.png" }, { palavra: "CABELO", img: "cabelo.png" },
+                { palavra: "CAMELO", img: "camelo.png" }, { palavra: "CANETA", img: "caneta.png" },
+                { palavra: "CASTELO", img: "castelo.png" }, { palavra: "CENOURA", img: "cenoura.png" },
+                { palavra: "CHAVE", img: "chave.png" }, { palavra: "CHOCOLATE", img: "chocolate.png" },
+                { palavra: "COBRA", img: "cobra.png" }, { palavra: "COGUMELO", img: "cogumelo.png" }
+            ]
+        }
+        // Repetir para D, E, F... até Z
+    },
+    relatorio: { 
+        titulo: "PARABÉNS!", 
+        pontosTotal: "Pontos:", 
+        tempoTotal: "Tempo:" 
+    }
+};
