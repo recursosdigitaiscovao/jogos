7f9eea6c6408b8447219eb0076cafb04b1690a43
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-11 14:57:58 +0100

    Update dados.js

--- 1/pt/sl00/dados.js
+++ 1/pt/sl00/dados.js
@@ -4,12 +4,12 @@ const JOGO_CONFIG = {
     iconesMenu: { pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" },
     categorias: {
         animais: {
-            nome: "Animais", img: "animaisdomesticos/vaca.png",
+            nome: "Animais Domésticos", img: "animaisdomesticos/vaca.png",
             itens: [
                 { letras: "CÃO", palavras: ["CÃO"] },
                 { letras: "GATO", palavras: ["GATO"] },
-                { letras: "LEÃO", palavras: ["LEÃO"] },
                 { letras: "PATO", palavras: ["PATO"] },
+                { letras: "PERU", palavras: ["PERU"] },
                 { letras: "VACA", palavras: ["VACA"] }
             ]
         },
@@ -24,7 +24,7 @@ const JOGO_CONFIG = {
             ]
         },
         material: {
-            nome: "Material", img: "materialescolar/afia.png",
+            nome: "Material Escolar", img: "materialescolar/afia.png",
             itens: [
                 { letras: "AFIA", palavras: ["AFIA"] },
                 { letras: "LÁPIS", palavras: ["LÁPIS"] },
@@ -32,7 +32,37 @@ const JOGO_CONFIG = {
                 { letras: "COLA", palavras: ["COLA"] },
                 { letras: "LIVRO", palavras: ["LIVRO"] }
             ]
+        },
+        selvagens: {
+            nome: "Animais Selvagens", img: "animaisdomesticos/silabas_1.png",
+            itens: [
+                { letras: "LEÃO", palavras: ["LEÃO"] },
+                { letras: "TIGRE", palavras: ["TIGRE"] },
+                { letras: "ZEBRA", palavras: ["ZEBRA"] },
+                { letras: "GIRAFA", palavras: ["GIRAFA"] },
+                { letras: "COBRA", palavras: ["COBRA"] }
+            ]
+        },
+        paises: {
+            nome: "Países", img: "rd.png",
+            itens: [
+                { letras: "PORTUGAL", palavras: ["PORTUGAL"] },
+                { letras: "BRASIL", palavras: ["BRASIL"] },
+                { letras: "ANGOLA", palavras: ["ANGOLA"] },
+                { letras: "FRANÇA", palavras: ["FRANÇA"] },
+                { letras: "ITÁLIA", palavras: ["ITÁLIA"] }
+            ]
+        },
+        transportes: {
+            nome: "Transportes", img: "materialescolar/mochila.png",
+            itens: [
+                { letras: "AVIÃO", palavras: ["AVIÃO"] },
+                { letras: "BARCO", palavras: ["BARCO"] },
+                { letras: "CARRO", palavras: ["CARRO"] },
+                { letras: "MOTO", palavras: ["MOTO"] },
+                { letras: "TREM", palavras: ["TREM"] }
+            ]
         }
     },
-    relatorio: { titulo: "MUITO BEM!", pontosTotal: "Acertos:", tempoTotal: "Tempo:" }
+    relatorio: { titulo: "MUITO BEM!", pontosTotal: "Palavras:", tempoTotal: "Tempo:" }
 };
