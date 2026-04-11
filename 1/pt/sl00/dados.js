4e88c8b9eda5d84bc86e16169cede4d9d0ceaeb6
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-11 14:49:00 +0100

    Update dados.js

--- 1/pt/sl00/dados.js
+++ 1/pt/sl00/dados.js
@@ -4,12 +4,33 @@ const JOGO_CONFIG = {
     iconesMenu: { pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" },
     categorias: {
         animais: {
-            nome: "Animais",
-            img: "animaisdomesticos/vaca.png",
+            nome: "Animais", img: "animaisdomesticos/vaca.png",
             itens: [
                 { letras: "CAO", palavras: ["CAO"] },
+                { letras: "PATO", palavras: ["PATO"] },
                 { letras: "GATO", palavras: ["GATO"] },
-                { letras: "PATO", palavras: ["PATO"] }
+                { letras: "PERU", palavras: ["PERU"] },
+                { letras: "VACA", palavras: ["VACA"] }
+            ]
+        },
+        frutos: {
+            nome: "Frutos", img: "frutas/banana.png",
+            itens: [
+                { letras: "MACA", palavras: ["MACA"] },
+                { letras: "PERA", palavras: ["PERA"] },
+                { letras: "UVA", palavras: ["UVA"] },
+                { letras: "FIGO", palavras: ["FIGO"] },
+                { letras: "KIWI", palavras: ["KIWI"] }
+            ]
+        },
+        material: {
+            nome: "Material", img: "materialescolar/afia.png",
+            itens: [
+                { letras: "AFIA", palavras: ["AFIA"] },
+                { letras: "COLA", palavras: ["COLA"] },
+                { letras: "LIVRO", palavras: ["LIVRO"] },
+                { letras: "GIZ", palavras: ["GIZ"] },
+                { letras: "MAPA", palavras: ["MAPA"] }
             ]
         }
     },
