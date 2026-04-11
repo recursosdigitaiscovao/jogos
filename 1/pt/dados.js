3c477048377434341c6f304cf575a0991bfc8d82
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-11 22:25:00 +0100

    Update dados.js

--- 1/pt/dados.js
+++ 1/pt/dados.js
@@ -5,7 +5,7 @@ const JOGO_CONFIG = {
     // Caminho para os icons dos jogos (dentro da pasta local iconjogos/)
     caminhoIconsJogos: "iconjogos/",
 
-    // Nomes dos ficheiros de ícones do menu/sidebar (conforme solicitado)
+    // Nomes dos ficheiros de ícones do menu/sidebar
     iconesMenu: {
         pre: "iconpre.png",
         ano1: "icon1.png",
@@ -14,12 +14,13 @@ const JOGO_CONFIG = {
         ano4: "icon4.png"
     },
 
-    // Lista de jogos - O sistema vai criar os cards sozinho
+    // LISTA DE JOGOS - Para adicionar um jogo novo, basta copiar uma linha e mudar os dados
     listaJogos: [
         { nome: "Conta as Sílabas", icon: "silabas.png", link: "silabas/" },
         { nome: "Forma Palavras", icon: "liga_letras.png", link: "liga_letras/" },
         { nome: "Descobre a Palavra", icon: "forca.png", link: "forca/" },
         { nome: "Sopa de Letras", icon: "sopa_letras.png", link: "sopa_letras/" },
-       
+        // Exemplo de como adicionar um novo:
+        // { nome: "Novo Jogo", icon: "novo_icon.png", link: "pasta_do_jogo/" },
     ]
 };
