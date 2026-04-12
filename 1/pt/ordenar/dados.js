6396acdbeb4186683fa51c445153649d6cd33e1a
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-12 21:45:30 +0100

    Update dados.js

--- 1/pt/ordenar/dados.js
+++ 1/pt/ordenar/dados.js
@@ -1,16 +1,26 @@
 const JOGO_CONFIG = {
-    caminhoImg: "../../../img/",         // Onde estão livro_a.png, livro_as.png, livro_f.png e rd.png
+    caminhoImg: "../../../img/",         
     caminhoIcons: "../../../icons/",     
     caminhoCat: "../../../img/cat/",     
 
-    iconesMenu: { pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" },
-    relatorio: { pontosTotal: "Livros na Estante:", tempoTotal: "Tempo:" }
+    iconesMenu: { 
+        pre: "iconpre.png", 
+        ano1: "icon1.png", 
+        ano2: "icon2.png", 
+        ano3: "icon3.png", 
+        ano4: "icon4.png" 
+    },
+    
+    relatorio: {
+        pontosTotal: "Livros na Estante:",
+        tempoTotal: "Tempo:"
+    }
 };
 
 const JOGO_ORDEM_DATA = {
     categorias: {
         letras: { 
-            nome: 'Páginas Soltas (Letras)', 
+            nome: 'Letras do Alfabeto', 
             img: 'cat_letras.png', 
             niveis: [ 
                 { n: 1, qtd: 6, tipo: 'seq', titulo: 'Letras Seguidas' }, 
@@ -20,17 +30,15 @@ const JOGO_ORDEM_DATA = {
         mesma: { 
             nome: 'Secções do Dicionário', 
             img: 'cat_mesma.png', 
-            niveis: [ { n: 1, qtd: 6, tipo: 'mesma', titulo: 'Mesma Letra Inicial' } ] 
-        },
-        mix: { 
-            nome: 'Biblioteca em Caos', 
-            img: 'cat_mix.png', 
-            niveis: [ { n: 1, qtd: 6, tipo: 'mix', titulo: 'Desafio Total' } ] 
+            niveis: [ 
+                { n: 1, qtd: 6, tipo: 'mesma', titulo: 'Mesma Letra Inicial' } 
+            ] 
         }
     },
     bancoMesma: {
         C: ["Cama", "Caneta", "Carro", "Casa", "Cavalo", "Copo"],
-        M: ["Mala", "Maçã", "Mesa", "Meia", "Milho", "Mola"]
-    },
-    bancoGeral: ["Zebra", "Dado", "Foca", "Gato", "Hipopótamo", "Lua"]
+        M: ["Mala", "Maçã", "Mesa", "Meia", "Milho", "Mola"],
+        B: ["Bala", "Baleia", "Barco", "Batom", "Bexiga", "Bico"],
+        P: ["Papa", "Pato", "Pena", "Pera", "Pipa", "Poço"]
+    }
 };
