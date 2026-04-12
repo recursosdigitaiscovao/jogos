cbe6aed5d560fbb4b68009cebd5a77be0fb6ef83
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-12 23:13:22 +0100

    Update dados.js

--- 1/pt/ordenar/dados.js
+++ 1/pt/ordenar/dados.js
@@ -3,42 +3,28 @@ const JOGO_CONFIG = {
     caminhoIcons: "../../../icons/",     
     caminhoCat: "../../../img/cat/",     
 
-    iconesMenu: { 
-        pre: "iconpre.png", 
-        ano1: "icon1.png", 
-        ano2: "icon2.png", 
-        ano3: "icon3.png", 
-        ano4: "icon4.png" 
-    },
-    
-    relatorio: {
-        pontosTotal: "Livros na Estante:",
-        tempoTotal: "Tempo:"
-    }
+    iconesMenu: { pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" },
+    relatorio: { pontosTotal: "Organização:", tempoTotal: "Tempo:" }
 };
 
 const JOGO_ORDEM_DATA = {
     categorias: {
         letras: { 
-            nome: 'Letras do Alfabeto', 
+            nome: 'Páginas Soltas (Letras)', 
             img: 'cat_letras.png', 
             niveis: [ 
-                { n: 1, qtd: 6, tipo: 'seq', titulo: 'Letras Seguidas' }, 
+                { n: 1, qtd: 6, tipo: 'seq', titulo: 'O Armário das Letras' }, 
                 { n: 2, qtd: 6, tipo: 'alt', titulo: 'Letras Saltadas' } 
             ] 
         },
         mesma: { 
             nome: 'Secções do Dicionário', 
             img: 'cat_mesma.png', 
-            niveis: [ 
-                { n: 1, qtd: 6, tipo: 'mesma', titulo: 'Mesma Letra Inicial' } 
-            ] 
+            niveis: [ { n: 1, qtd: 6, tipo: 'mesma', titulo: 'Mesma Letra Inicial' } ] 
         }
     },
     bancoMesma: {
         C: ["Cama", "Caneta", "Carro", "Casa", "Cavalo", "Copo"],
-        M: ["Mala", "Maçã", "Mesa", "Meia", "Milho", "Mola"],
-        B: ["Bala", "Baleia", "Barco", "Batom", "Bexiga", "Bico"],
-        P: ["Papa", "Pato", "Pena", "Pera", "Pipa", "Poço"]
+        M: ["Mala", "Maçã", "Mesa", "Meia", "Milho", "Mola"]
     }
 };
