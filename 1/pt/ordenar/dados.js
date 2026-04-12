337a4ae465c8ab8b0571c69d7dc7513eb218ede1
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-12 23:33:40 +0100

    Update dados.js

--- 1/pt/ordenar/dados.js
+++ 1/pt/ordenar/dados.js
@@ -1,16 +1,26 @@
 const JOGO_CONFIG = {
     caminhoImg: "../../../img/",         
     caminhoIcons: "../../../icons/",     
-    caminhoCat: "../../../img/cat/",     
+    caminhoCat: "../../../imgcategorias/",     
 
-    iconesMenu: { pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" },
-    relatorio: { pontosTotal: "Organização:", tempoTotal: "Tempo:" }
+    iconesMenu: { 
+        pre: "iconpre.png", 
+        ano1: "icon1.png", 
+        ano2: "icon2.png", 
+        ano3: "icon3.png", 
+        ano4: "icon4.png" 
+    },
+    
+    relatorio: {
+        pontosTotal: "Pontos:",
+        tempoTotal: "Tempo:"
+    }
 };
 
 const JOGO_ORDEM_DATA = {
     categorias: {
         letras: { 
-            nome: 'Páginas Soltas (Letras)', 
+            nome: 'Letras do Alfabeto', 
             img: 'cat_letras.png', 
             niveis: [ 
                 { n: 1, qtd: 6, tipo: 'seq', titulo: 'O Armário das Letras' }, 
@@ -18,9 +28,9 @@ const JOGO_ORDEM_DATA = {
             ] 
         },
         mesma: { 
-            nome: 'Secções do Dicionário', 
+            nome: 'Mesma Letra Inicial', 
             img: 'cat_mesma.png', 
-            niveis: [ { n: 1, qtd: 6, tipo: 'mesma', titulo: 'Mesma Letra Inicial' } ] 
+            niveis: [ { n: 1, qtd: 6, tipo: 'mesma', titulo: 'Dicionário Mágico' } ] 
         }
     },
     bancoMesma: {
