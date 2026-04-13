2ae6c7885f2b2a44bd595ab0182f2ab691c86327
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-14 00:49:54 +0100

    Update dados.js

--- 1/pt/teste/dados.js
+++ 1/pt/teste/dados.js
@@ -1,18 +1,59 @@
-const jogoDados = {
-    niveis: [
-        {
-            nivel: 1,
-            palavraAlvo: "PATO",
-            imagem: "https://img.icons8.com/color/150/duck.png",
-            palavrasEscondidas: ["PATO", "GATO", "CÃO", "RATO"],
-            gridTamanho: 8
+const JOGO_CONFIG = {
+    caminhoImg: "../../../img/",
+    caminhoIcons: "../../../icons/",
+    iconesMenu: {
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png"
+    },
+    categorias: {
+        animais: {
+            nome: "Animais",
+            img: "animaisdomesticos/vaca.png",
+            itens: [
+                { nome: "VACA", img: "animaisdomesticos/vaca.png" },
+                { nome: "GATO", img: "animaisdomesticos/gato.png" },
+                { nome: "PATO", img: "animaisdomesticos/pato.png" },
+                { nome: "PERU", img: "animaisdomesticos/peru.png" },
+                { nome: "CÃO", img: "animaisdomesticos/cao.png" },
+                { nome: "CAVALO", img: "animaisdomesticos/cavalo.png" },
+                { nome: "COELHO", img: "animaisdomesticos/coelho.png" }
+            ]
         },
-        {
-            nivel: 2,
-            palavraAlvo: "GIRAFA",
-            imagem: "https://img.icons8.com/color/150/giraffe.png",
-            palavrasEscondidas: ["GIRAFA", "COELHO", "CAVALO", "PEIXE", "LEÃO", "TIGRE"],
-            gridTamanho: 10
+        frutos: {
+            nome: "Frutos",
+            img: "frutas/banana.png",
+            itens: [
+                { nome: "BANANA", img: "frutas/banana.png" },
+                { nome: "CEREJA", img: "frutas/cereja.png" },
+                { nome: "UVAS", img: "frutas/uvas.png" },
+                { nome: "MORANGO", img: "frutas/morango.png" },
+                { nome: "LARANJA", img: "frutas/banana.png" },
+                { nome: "PAPAIA", img: "frutas/papaia.png" },
+                { nome: "MACÃ", img: "frutas/maca.png" }
+            ]
+        },
+        material: {
+            nome: "Material Escolar",
+            img: "materialescolar/afia.png",
+            itens: [
+                { nome: "MOCHILA", img: "materialescolar/mochila.png" },
+                { nome: "TESOURA", img: "materialescolar/tesoura.png" },
+                { nome: "LÁPIS", img: "materialescolar/lapis.png" },
+                { nome: "LIVRO", img: "materialescolar/livro.png" },
+                { nome: "CAPA", img: "materialescolar/capa.png" },
+                { nome: "AFIA", img: "materialescolar/afia.png" },
+                { nome: "COLA", img: "materialescolar/cola.png" },
+                { nome: "FOLHA", img: "materialescolar/folha.png" },
+                { nome: "ESTOJO", img: "materialescolar/estojo.png" }
+            ]
         }
-    ]
+    },
+    relatorio: {
+        titulo: "MUITO BEM!",
+        pontosTotal: "Encontraste:",
+        tempoTotal: "Tempo:"
+    }
 };
