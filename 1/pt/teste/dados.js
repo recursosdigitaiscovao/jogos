5893cf768c826bdb4174c2f49fb899f17294c21b
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-14 00:37:25 +0100

    Update dados.js

--- 1/pt/teste/dados.js
+++ 1/pt/teste/dados.js
@@ -1,1 +1,18 @@
-
+const jogoDados = {
+    niveis: [
+        {
+            nivel: 1,
+            palavraAlvo: "PATO",
+            imagem: "https://img.icons8.com/color/150/duck.png",
+            palavrasEscondidas: ["PATO", "GATO", "CÃO", "RATO"],
+            gridTamanho: 8
+        },
+        {
+            nivel: 2,
+            palavraAlvo: "GIRAFA",
+            imagem: "https://img.icons8.com/color/150/giraffe.png",
+            palavrasEscondidas: ["GIRAFA", "COELHO", "CAVALO", "PEIXE", "LEÃO", "TIGRE"],
+            gridTamanho: 10
+        }
+    ]
+};
