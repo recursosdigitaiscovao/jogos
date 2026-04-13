816edd3a1f889545a467c6a807687c5999f51e0f
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-13 11:28:53 +0100

    Update dados.js

--- 1/pt/ordenar/dados.js
+++ 1/pt/ordenar/dados.js
@@ -1,22 +1,58 @@
 const JOGO_CONFIG = {
-    caminhoImg: "../../../img/",         
-    caminhoIcons: "../../../icons/",     
-    iconesMenu: { pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" }
-};
-
-const JOGO_ORDEM_DATA = {
+    caminhoImg: "../../../img/",
+    caminhoIcons: "../../../icons/",
+    caminhoCat: "../../../imgcategorias/", 
+    iconesMenu: { 
+        pre: "iconpre.png", 
+        ano1: "icon1.png", 
+        ano2: "icon2.png", 
+        ano3: "icon3.png", 
+        ano4: "icon4.png" 
+    },
     categorias: {
-        cat1: { id: 'cat1', nome: '1. Letras Consecutivas', titulo: 'Sequência do Alfabeto' },
-        cat2: { id: 'cat2', nome: '2. Letras Não Consecutivas', titulo: 'Letras Misturadas' },
-        cat3: { id: 'cat3', nome: '3. Mesma Letra Inicial', titulo: 'Dicionário Mágico' },
-        cat4: { id: 'cat4', nome: '4. Palavras Variadas', titulo: 'Ordem Alfabética Geral' }
-    },
-    bancoMesma: {
-        C: ["Cama", "Carro", "Casa", "Copo", "Cebola", "Coelho"],
-        M: ["Mala", "Maçã", "Mesa", "Meia", "Milho", "Mola"],
-        B: ["Bala", "Bota", "Bico", "Bule", "Bolo", "Balão"],
-        P: ["Pato", "Pera", "Pipa", "Pudim", "Porta", "Peixe"],
-        A: ["Abacate", "Abacaxi", "Abelha", "Agulha", "Anel", "Anta"]
-    },
-    bancoGeral: ["Abelha", "Bola", "Cachorro", "Dado", "Elefante", "Foca", "Gato", "Hipopótamo", "Jacaré", "Leão", "Macaco", "Navio", "Ovo", "Pato", "Queijo", "Rato", "Sapo", "Tigre", "Uva", "Vaca", "Zebra"]
+        cat1: {
+            nome: "Letras Consecutivas", 
+            img: "cat1.png",
+            rondas: [
+                { itens: ["A", "B", "C", "D"] },
+                { itens: ["E", "F", "G", "H"] },
+                { itens: ["L", "M", "N", "O"] },
+                { itens: ["P", "Q", "R", "S"] },
+                { itens: ["W", "X", "Y", "Z"] }
+            ]
+        },
+        cat2: {
+            nome: "Letras Baralhadas", 
+            img: "cat2.png",
+            rondas: [
+                { itens: ["A", "F", "M", "X"] },
+                { itens: ["B", "H", "K", "T"] },
+                { itens: ["C", "G", "J", "V"] },
+                { itens: ["D", "I", "L", "P"] },
+                { itens: ["E", "N", "S", "Z"] }
+            ]
+        },
+        cat3: {
+            nome: "Mesma Letra Inicial", 
+            img: "cat3.png",
+            rondas: [
+                { itens: ["ABELHA", "ANANÁS", "ANEL", "AVENTAL"] },
+                { itens: ["BOLA", "BOLO", "BONECA", "BOTA"] },
+                { itens: ["CASA", "CAVALO", "COELHO", "COPO"] },
+                { itens: ["DADO", "DEDO", "DENTE", "DOCE"] },
+                { itens: ["GATO", "GAVETA", "GELO", "GIRAFA"] }
+            ]
+        },
+        cat4: {
+            nome: "Palavras Mistas", 
+            img: "cat4.png",
+            rondas: [
+                { itens: ["CÃO", "GATO", "PATO", "RATO"] },
+                { itens: ["AZUL", "ROSA", "VERDE", "VIOLETA"] },
+                { itens: ["MÃE", "PAI", "AVÓ", "AVÔ"] },
+                { itens: ["SOL", "LUA", "MAR", "CÉU"] },
+                { itens: ["UM", "DOIS", "TRÊS", "QUATRO"] }
+            ]
+        }
+    }
 };
