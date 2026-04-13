a903b02f8343fe99868931e437f85af02cc27097
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-13 22:09:33 +0100

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,11 +1,1 @@
-// dados.js
-const PORTAL_CONFIG = {
-    caminhoIcons: "icons/", // Pasta onde estão os teus ícones
-    icones: {
-        pre: "iconpre.png",
-        ano1: "icon1.png",
-        ano2: "icon2.png",
-        ano3: "icon3.png",
-        ano4: "icon4.png"
-    }
-};
+
