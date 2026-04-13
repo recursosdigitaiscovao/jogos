3d765ad6cb0ebfbd1714aa0755d2d5b47a3d6b4d
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-13 01:05:06 +0100

    Update dados.js

--- 1/pt/ordenar/dados.js
+++ 1/pt/ordenar/dados.js
@@ -1,27 +1,37 @@
 const JOGO_CONFIG = {
     caminhoImg: "../../../img/",         
     caminhoIcons: "../../../icons/",     
-    caminhoCat: "../../../img/cat/",     
-    iconesMenu: { pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" }
+    caminhoCat: "../../../imgcategorias",     
+
+    iconesMenu: { 
+        pre: "iconpre.png", 
+        ano1: "icon1.png", 
+        ano2: "icon2.png", 
+        ano3: "icon3.png", 
+        ano4: "icon4.png" 
+    }
 };
 
 const JOGO_ORDEM_DATA = {
     categorias: {
         letras: { 
+            id: 'letras',
             nome: 'Letras do Alfabeto', 
             img: 'cat_letras.png', 
-            niveis: [ { n: 1, qtd: 4, tipo: 'seq', titulo: '4 Letras' } ] 
+            titulo: 'As Letras Soltas'
         },
         mesma: { 
+            id: 'mesma',
             nome: 'Mesma Letra Inicial', 
             img: 'cat_mesma.png', 
-            niveis: [ { n: 1, qtd: 4, tipo: 'mesma', titulo: 'Dicionário' } ] 
+            titulo: 'Dicionário Mágico'
         }
     },
     bancoMesma: {
         C: ["Cama", "Carro", "Casa", "Copo", "Cebola", "Coelho"],
-        M: ["Mala", "Maçã", "Mesa", "Mola", "Muro", "Moeda"],
+        M: ["Mala", "Maçã", "Mesa", "Meia", "Milho", "Mola"],
         B: ["Bala", "Bota", "Bico", "Bule", "Bolo", "Balao"],
-        P: ["Pato", "Pera", "Pipa", "Pudim", "Porta", "Peixe"]
+        P: ["Pato", "Pera", "Pipa", "Pudim", "Porta", "Peixe"],
+        A: ["Abacate", "Abacaxi", "Abelha", "Agulha", "Anel", "Anta"]
     }
 };
