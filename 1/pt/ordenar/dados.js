1bc54a014d2cd579d94fbf132d771aa0b14daff7
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-13 10:10:45 +0100

    Update dados.js

--- 1/pt/ordenar/dados.js
+++ 1/pt/ordenar/dados.js
@@ -8,7 +8,7 @@ const JOGO_ORDEM_DATA = {
     categorias: {
         cat1: { id: 'cat1', nome: '1. Letras Consecutivas', titulo: 'Sequência do Alfabeto' },
         cat2: { id: 'cat2', nome: '2. Letras Não Consecutivas', titulo: 'Letras Misturadas' },
-        cat3: { id: 'cat3', nome: '3. Mesma Letra Inicial', titulo: 'Palavras com a Mesma Letra' },
+        cat3: { id: 'cat3', nome: '3. Mesma Letra Inicial', titulo: 'Dicionário Mágico' },
         cat4: { id: 'cat4', nome: '4. Palavras Variadas', titulo: 'Ordem Alfabética Geral' }
     },
     bancoMesma: {
@@ -18,5 +18,5 @@ const JOGO_ORDEM_DATA = {
         P: ["Pato", "Pera", "Pipa", "Pudim", "Porta", "Peixe"],
         A: ["Abacate", "Abacaxi", "Abelha", "Agulha", "Anel", "Anta"]
     },
-    bancoGeral: ["Abelha", "Bola", "Cachorro", "Dado", "Elefante", "Foca", "Gato", "Jacaré", "Leão", "Macaco", "Navio", "Ovo", "Pato", "Queijo", "Rato", "Sapo", "Tigre", "Uva", "Vaca", "Zebra"]
+    bancoGeral: ["Abelha", "Bola", "Cachorro", "Dado", "Elefante", "Foca", "Gato", "Hipopótamo", "Jacaré", "Leão", "Macaco", "Navio", "Ovo", "Pato", "Queijo", "Rato", "Sapo", "Tigre", "Uva", "Vaca", "Zebra"]
 };
