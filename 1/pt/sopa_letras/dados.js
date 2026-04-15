1b69307feffb5aa6b0c1768ecbb2f2886563ec8a
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-15 04:18:40 +0100

    Update dados.js

--- 1/pt/sopa_letras/dados.js
+++ 1/pt/sopa_letras/dados.js
@@ -2,16 +2,25 @@ const JOGO_CONFIG = {
     caminhoImg: "../../../img/",
     caminhoIcons: "../../../icons/",
     iconesMenu: {
+        home: "home.png",
         pre: "iconpre.png",
         ano1: "icon1.png",
         ano2: "icon2.png",
         ano3: "icon3.png",
         ano4: "icon4.png"
     },
+    links: {
+        home: "/jogos", 
+        pre: "/jogos/pre",
+        ano1: "/jogos/1",
+        ano2: "/jogos/2",
+        ano3: "/jogos/3",
+        ano4: "/jogos/4"
+    },
     categorias: {
         animais: {
             nome: "Animais",
-            img: "animaisdomesticos/vaca.png",
+            imgCapa: "animaisdomesticos/gato.png",
             itens: [
                 { nome: "VACA", img: "animaisdomesticos/vaca.png" },
                 { nome: "GATO", img: "animaisdomesticos/gato.png" },
@@ -24,21 +33,20 @@ const JOGO_CONFIG = {
         },
         frutos: {
             nome: "Frutos",
-            img: "frutas/banana.png",
+            imgCapa: "frutas/morango.png",
             itens: [
                 { nome: "BANANA", img: "frutas/banana.png" },
                 { nome: "CEREJA", img: "frutas/cereja.png" },
-                { nome: "BANANA", img: "frutas/banana.png" },
                 { nome: "UVAS", img: "frutas/uvas.png" },
                 { nome: "MORANGO", img: "frutas/morango.png" },
-                { nome: "LARANJA", img: "frutas/banana.png" }, // Reutilizando imagem do seu config
+                { nome: "LARANJA", img: "frutas/laranja.png" },
                 { nome: "PAPAIA", img: "frutas/papaia.png" },
-                { nome: "MACÃ", img: "frutas/maca.png" } // Adicionado para completar 5
+                { nome: "MAÇÃ", img: "frutas/maca.png" }
             ]
         },
         material: {
             nome: "Material Escolar",
-            img: "materialescolar/afia.png",
+            imgCapa: "materialescolar/afia.png",
             itens: [
                 { nome: "MOCHILA", img: "materialescolar/mochila.png" },
                 { nome: "TESOURA", img: "materialescolar/tesoura.png" },
