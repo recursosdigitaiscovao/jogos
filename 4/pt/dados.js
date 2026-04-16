3871b07dfa900d17e63d81664ac861bf7bf31ddf
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-16 23:29:48 +0100

    Update dados.js

--- 4/pt/dados.js
+++ 4/pt/dados.js
@@ -1,20 +1,39 @@
 const JOGO_CONFIG = {
-    // Caminho para os icons do menu (sobe dois níveis para jogos/icons/)
+    // Títulos e Textos da Página
+    textos: {
+        tituloLinha1: "Mestres da",
+        tituloLinha2: "Leitura",
+        subtitulo: "Português | 4º Ano",
+        intro: "Explora os livros e diverte-te a ler!",
+        rodape: "&copy; Mestres da Leitores - Recursos Educativos"
+    },
+
+    // Caminhos
     caminhoIconsMenu: "../../icons/",
-    
-    // Caminho para os icons dos jogos (dentro da pasta local iconjogos/)
     caminhoIconsJogos: "iconjogos/",
 
-    // Nomes dos ficheiros de ícones do menu/sidebar (conforme solicitado)
+    // Ícones
     iconesMenu: {
+        home: "home.png",
         pre: "iconpre.png",
         ano1: "icon1.png",
         ano2: "icon2.png",
         ano3: "icon3.png",
-        ano4: "icon4.png"
+        ano4: "icon4.png",
+        voltar: "voltar.png"
     },
 
-    // Lista de jogos - O sistema vai criar os cards sozinho
+    // Links do Menu
+    links: {
+        home: "/jogos", 
+        pre: "/jogos/pre",
+        ano1: "/jogos/1",
+        ano2: "/jogos/2",
+        ano3: "/jogos/3",
+        ano4: "/jogos/4"
+    },
+    
+    // Lista de Jogos
     listaJogos: [
         { nome: "jogo1", icon: "1.png", link: "nome1/" },
         { nome: "jogo2", icon: "2.png", link: "nome2/" },
