86552d5abc351111d05206ad4049469b66a28e1b
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-16 10:36:01 +0100

    Update dados.js

--- 1/pt/dados.js
+++ 1/pt/dados.js
@@ -1,33 +1,28 @@
-const JOGO_CONFIG = {
-    // Caminho para os icons do menu (sobe dois níveis para jogos/icons/)
-    caminhoIconsMenu: "../../icons/",
-    
-    // Caminho para os icons dos jogos (dentro da pasta local iconjogos/)
-    caminhoIconsJogos: "iconjogos/",
+// Caminho para os icons dos jogos (dentro da pasta local iconjogos/)
+caminhoIconsJogos: "iconjogos/",
 
-    // Nomes dos ficheiros de ícones do menu/sidebar
-    iconesMenu: {
-        pre: "iconpre.png",
-        ano1: "icon1.png",
-        ano2: "icon2.png",
-        ano3: "icon3.png",
-        ano4: "icon4.png"
-    },
-    links: {
-        home: "/jogos", 
-        pre: "/jogos/pre",
-        ano1: "/jogos/1",
-        ano2: "/jogos/2",
-        ano3: "/jogos/3",
-        ano4: "/jogos/4"
-    },
-    
-    // LISTA DE JOGOS
-    listaJogos: [
-        { nome: "Conta as Sílabas", icon: "silabas.png", link: "silabas/" },
-        { nome: "Forma Palavras", icon: "liga_letras.png", link: "liga_letras/" },
-        { nome: "Descobre a Palavra", icon: "forca.png", link: "forca/" },
-        { nome: "Sopa de Letras", icon: "sopa_letras.png", link: "sopa_letras/" },
-        { nome: "Copia Palavras", icon: "copia_palavras.png", link: "copia_palavras/" }
-    ]
-};
+// Nomes dos ficheiros de ícones do menu/sidebar
+iconesMenu: {
+    pre: "iconpre.png",
+    ano1: "icon1.png",
+    ano2: "icon2.png",
+    ano3: "icon3.png",
+    ano4: "icon4.png"
+},
+links: {
+    home: "/jogos", 
+    pre: "/jogos/pre",
+    ano1: "/jogos/1",
+    ano2: "/jogos/2",
+    ano3: "/jogos/3",
+    ano4: "/jogos/4"
+},
+
+// LISTA DE JOGOS
+listaJogos: [
+    { nome: "Conta as Sílabas", icon: "silabas.png", link: "silabas/" },
+    { nome: "Forma Palavras", icon: "liga_letras.png", link: "liga_letras/" },
+    { nome: "Descobre a Palavra", icon: "forca.png", link: "forca/" },
+    { nome: "Sopa de Letras", icon: "sopa_letras.png", link: "sopa_letras/" },
+    { nome: "Copia Palavras", icon: "copia_palavras.png", link: "copia_palavras/" }
+]
