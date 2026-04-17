4d76b612d9f1c9c6cf7cd6e7a12b48b99fa31569
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-18 00:04:24 +0100

    Update dados.js

--- 1/dados.js
+++ 1/dados.js
@@ -25,9 +25,15 @@ const JOGO_CONFIG = {
         ano3: "/jogos/3",
         ano4: "/jogos/4"
     },
+    // Definição de cores para os cartões (Moldura e Fundo)
+    temaCores: {
+        azul: { borda: "#5ba4e5", fundo: "#e1f0ff" },
+        verde: { borda: "#58bc8c", fundo: "#e8f7f0" },
+        castanho: { borda: "#8b4513", fundo: "#ffe8cc" }
+    },
     listaJogos: [
-        { nome: "Português", icon: "pt.png", link: "pt/" },
-        { nome: "Matemática", icon: "mat.png", link: "mat/" },
-        { nome: "Estudo do Meio", icon: "em.png", link: "em/" },
+        { nome: "Português", icon: "pt.png", link: "pt/", tema: "azul" },
+        { nome: "Matemática", icon: "mat.png", link: "mat/", tema: "verde" },
+        { nome: "Estudo do Meio", icon: "em.png", link: "em/", tema: "castanho" },
     ]
 };
