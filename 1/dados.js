76e6eecc71aa3793512b3987af4acb54b2c29587
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-17 14:42:57 +0100

    Update dados.js

--- 1/dados.js
+++ 1/dados.js
@@ -1,1 +1,37 @@
+const JOGO_CONFIG = {
+    textos: {
+        tituloLinha1: "PEQUENOS",
+        tituloLinha2: "LEITORES",
+        subtitulo: "Português | 2º Ano",
+        intro: "O que vamos aprender hoje?",
+        rodape: "&copy; Jovens Leitores - Recursos Educativos"
+    },
 
+    caminhoIconsMenu: "icons/", 
+    caminhoIconsJogos: "icons/", 
+
+    iconesMenu: {
+        home: "home.png",
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png",
+        voltar: "voltar.png"
+    },
+
+    links: {
+        home: "/jogos", 
+        pre: "/jogos/pre",
+        ano1: "/jogos/1",
+        ano2: "/jogos/2",
+        ano3: "/jogos/3",
+        ano4: "/jogos/4"
+    },
+    
+    listaJogos: [
+        { nome: "Conta as Sílabas", icon: "silabas.png", link: "silabas/", cor: "#5ba4e5" },
+        { nome: "Forma Palavras", icon: "palavras.png", link: "palavras/", cor: "#58bc8c" },
+        { nome: "Descobre a Palavra", icon: "descobre.png", link: "descobre/", cor: "#e85a5a" }
+    ]
+};
