b8a882cdc61c4b7bde9698cecf46a63c47102770
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-17 14:36:16 +0100

    Update dados.js

--- 1/dados.js
+++ 1/dados.js
@@ -1,14 +1,18 @@
 const JOGO_CONFIG = {
+    // Títulos e Textos da Página
     textos: {
-        titulo: "PEQUENOS LEITORES",
-        subtitulo: "Português | 1º Ano",
-        intro: "O que vamos aprender hoje?",
-        rodape: "Recursos Digitais Covão"
+        tituloLinha1: "Jovens",
+        tituloLinha2: "Leitores",
+        subtitulo: "Português | 2º Ano",
+        intro: "Explora as palavras, as frases e diverte-te a ler!",
+        rodape: "&copy; Jovens Leitores - Recursos Educativos"
     },
 
-    caminhoIconsMenu: "../icons/",
-    caminhoIconsJogos: "iconjogos/",
+    // Caminhos (Ajustado para a pasta 'icons')
+    caminhoIconsMenu: "icons/", 
+    caminhoIconsJogos: "icons/", 
 
+    // Ícones
     iconesMenu: {
         home: "home.png",
         pre: "iconpre.png",
@@ -19,8 +23,8 @@ const JOGO_CONFIG = {
         voltar: "voltar.png"
     },
 
+    // Links do Menu
     links: {
-        voltar: "../index.html",
         home: "/jogos", 
         pre: "/jogos/pre",
         ano1: "/jogos/1",
@@ -29,10 +33,10 @@ const JOGO_CONFIG = {
         ano4: "/jogos/4"
     },
     
-    // Lista de Jogos com as cores da imagem
+    // Lista de Jogos (Certifica-te que estes ficheiros .png existem na pasta icons/)
     listaJogos: [
-        { nome: "Português", icon: "pt.png", link: "pt/", classe: "card-blue" },
-        { nome: "Matemática", icon: "mat.png", link: "mat/", classe: "card-green" },
-        { nome: "Estudo do Meio", icon: "em.png", link: "em/", classe: "card-red" },
+        { nome: "Português", icon: "pt.png", link: "pt/" },
+        { nome: "Matemática", icon: "mat.png", link: "mat/" },
+        { nome: "Estudo do Meio", icon: "em.png", link: "em/" }
     ]
 };
