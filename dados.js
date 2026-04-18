3886fa4763edcabaa388426acce174406e1ec3ab
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-18 15:19:21 +0100

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,1 +1,42 @@
-
+const JOGO_CONFIG = {
+    textos: {
+        tituloLinha1: "Mundo",
+        tituloLinha2: "dos jogos",
+        subtitulo: "Atividades | Pré e 1º Ciclo",
+        intro: "Escolhe o teu ano e diverte-te!",
+        rodape: "&copy; Mundo do Jogos - Recursos Educativos"
+    },
+    caminhoIconsMenu: "icons/", 
+    caminhoIconsJogos: "icons/", 
+    iconesMenu: {
+        home: "home.png",
+        pre: "iconpre.png",
+        ano1: "icon1.png",
+        ano2: "icon2.png",
+        ano3: "icon3.png",
+        ano4: "icon4.png",
+        voltar: "voltar.png"
+    },
+    links: {
+        home: "index.html", 
+        pre: "pre/index.html",
+        ano1: "1/index.html",
+        ano2: "2/index.html",
+        ano3: "3/index.html",
+        ano4: "4/index.html"
+    },
+    temaCores: {
+        rosa: { borda: "#FFFFFF", fundo: "#E691A7" },
+        laranja: { borda: "#FFFFFF", fundo: "#E6A75A" },
+        verde: { borda: "#FFFFFF", fundo: "#45CFA8" },
+        azul: { borda: "#FFFFFF", fundo: "#5EA2E6" },
+        roxo: { borda: "#FFFFFF", fundo: "#8A81E6" }
+    },
+    listaJogos: [
+        { nome: "Pré-Escolar", icon: "iconpre.png", link: "pre/", tema: "rosa" },
+        { nome: "1º Ano", icon: "icon1.png", link: "1/", tema: "laranja" },
+        { nome: "2º Ano", icon: "icon2.png", link: "2/", tema: "verde" },
+        { nome: "3º Ano", icon: "icon3.png", link: "3/", tema: "azul" },
+        { nome: "4º Ano", icon: "icon4.png", link: "4/", tema: "roxo" }
+    ]
+};
