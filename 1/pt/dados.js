33401f861ec0190f4a9132206f756859fee95e48
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-20 23:41:39 +0100

    Update dados.js

--- 1/pt/dados.js
+++ 1/pt/dados.js
@@ -36,7 +36,7 @@ const JOGO_CONFIG = {
     // Lista de Jogos
     listaJogos: [
         { nome: "Conta as Sílabas", icon: "conta_silabas.png", link: "conta_silabas/" },
-        { nome: "Forma Palavras", icon: "liga_letras.png", link: "liga_letras/" },
+        { nome: "Ordena Sílabas", icon: "silabas.png", link: "silabas/" },
         { nome: "Forma Palavras", icon: "liga_letras.png", link: "liga_letras/" },
         { nome: "Forma Palavras", icon: "liga_letras.png", link: "liga_letras/" },
         { nome: "Forma Palavras", icon: "liga_letras.png", link: "liga_letras/" },
