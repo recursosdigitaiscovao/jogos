4397a593d693f7d98fd1358e5104711d64b5c6ee
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-21 01:37:45 +0100

    Update dados.js

--- 1/pt/copia_palavras/dados.js
+++ 1/pt/copia_palavras/dados.js
@@ -1,48 +1,132 @@
 const JOGO_CONFIG = {
-    caminhoImg: "../../../img/",         // Caminho base para as imagens
-    caminhoIcons: "../../../icons/",     // Caminho para ícones do sistema
-    caminhoCat: "../../../img/letras/",  // Pasta das imagens das categorias (letras)
-    iconesMenu: { 
-        pre: "iconpre.png", 
-        ano1: "icon1.png", 
-        ano2: "icon2.png", 
-        ano3: "icon3.png", 
-        ano4: "icon4.png" 
+    nomeJogo: "ESCREVER PALAVRAS",
+    linkVoltar: "../", 
+    textoVoltar: "VOLTAR",
+    caminhoImg: "../../../img/",
+    caminhoIcons: "../../../icons/",
+    
+    pontuacao: {
+        acertoNivel1: 100, 
+        acertoNivel2: 150, 
+        erroFinal: 50           
     },
+
+    textos: {
+        tituloPagina: "Pequenos Leitores - Escrever Palavras",
+        tituloLinha1: "PEQUENOS",
+        tituloLinha2: "LEITORES",
+        subtitulo: "Português | 1º Ano",
+        rodape: "&copy; Pequenos Leitores - Recursos Educativos",
+        instrucao: "Escreve a palavra correspondente à imagem!"
+    },
+
+    sons: {
+        acerto: "https://cdn.pixabay.com/audio/2021/08/04/audio_0625c1539c.mp3",
+        erro: "https://cdn.pixabay.com/audio/2022/03/10/audio_c330c67761.mp3",
+        vitoria: "https://cdn.pixabay.com/audio/2024/02/07/audio_293963428f.mp3"
+    },
+
+    iconesMenu: { home: "home.png", pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" },
+    links: { home: "/jogos", pre: "/jogos/pre", ano1: "/jogos/1", ano2: "/jogos/2", ano3: "/jogos/3", ano4: "/jogos/4" },
+
     categorias: {
-        A: { nome: "Letra A", img: "letra_a.png", itens: [
-            { palavra: "AVIÃO", img: "transportes/aviao.png" }, 
-            { palavra: "ABELHA", img: "animaisselvagens/abelha.png" }, 
-            { palavra: "ANEL", img: "objetos/anel.png" }, 
-            { palavra: "ARANHA", img: "animaisselvagens/aranha.png" }, 
-            { palavra: "ÁRVORE", img: "natureza/arvore.png" }
-        ]},
-        B: { nome: "Letra B", img: "letra_b.png", itens: [
-            { palavra: "BOLA", img: "objetos/bola.png" }, 
-            { palavra: "BONECA", img: "objetos/boneca.png" }, 
-            { palavra: "BALEIA", img: "animaisselvagens/baleia.png" }, 
-            { palavra: "BANANA", img: "frutas/banana.png" }, 
-            { palavra: "BARCO", img: "transportes/barco.png" }
-        ]},
-        C: { nome: "Letra C", img: "letra_c.png", itens: [
-            { palavra: "CASA", img: "habitacao/casa.png" }, 
-            { palavra: "CARRO", img: "transportes/carro.png" }, 
-            { palavra: "CAVALO", img: "animaisdomesticos/cavalo.png" }, 
-            { palavra: "COELHO", img: "animaisdomesticos/coelho.png" }, 
-            { palavra: "CADERNO", img: "materialescolar/caderno.png" }
-        ]},
-        D: { nome: "Letra D", img: "letra_d.png", itens: [
-            { palavra: "DADO", img: "objetos/dado.png" }, 
-            { palavra: "DEDO", img: "corpo/dedo.png" }, 
-            { palavra: "DOCE", img: "comida/doce.png" }, 
-            { palavra: "DENTE", img: "corpo/dente.png" }, 
-            { palavra: "DRAGÃO", img: "objetos/dragao.png" }
-        ]}
-        // Podes acrescentar E, F, G... aqui em baixo seguindo o mesmo padrão
+        numeros: {
+            nome: "Números",
+            imgCapa: "numeros/cinco.png",
+            itens: [
+                { nome: "ZERO", img: "numeros/zero.png" },
+                { nome: "UM", img: "numeros/um.png" },
+                { nome: "DOIS", img: "numeros/dois.png" },
+                { nome: "TRÊS", img: "numeros/tres.png" },
+                { nome: "QUATRO", img: "numeros/quatro.png" },
+                { nome: "CINCO", img: "numeros/cinco.png" },
+                { nome: "SEIS", img: "numeros/seis.png" },
+                { nome: "SETE", img: "numeros/sete.png" },
+                { nome: "OITO", img: "numeros/oito.png" },
+                { nome: "NOVE", img: "numeros/nove.png" },
+                { nome: "DEZ", img: "numeros/dez.png" }
+            ]
+        },
+        animais: {
+            nome: "Animais",
+            imgCapa: "animaisdomesticos/gato.png",
+            itens: [
+                { nome: "VACA", img: "animaisdomesticos/vaca.png" },
+                { nome: "RATO", img: "animaisselvagens/rato.png" },
+                { nome: "LEÃO", img: "animaisselvagens/leao.png" },
+                { nome: "GATO", img: "animaisdomesticos/gato.png" },
+                { nome: "POMBO", img: "animaisselvagens/pombo.png" },
+                { nome: "PATO", img: "animaisdomesticos/pato.png" },
+                { nome: "GALO", img: "animaisdomesticos/galo.png" },
+                { nome: "URSO", img: "animaisselvagens/urso.png" },
+                { nome: "FOCA", img: "animaisselvagens/foca.png" },
+                { nome: "MACACO", img: "animaisselvagens/macaco.png" },
+                { nome: "CAVALO", img: "animaisdomesticos/cavalo.png" },
+                { nome: "GALINHA", img: "animaisdomesticos/galinha.png" },
+                { nome: "COELHO", img: "animaisdomesticos/coelho.png" }
+            ]
+        },
+        objetos: {
+            nome: "Objetos",
+            imgCapa: "objetos/vassoura.png",
+            itens: [
+                { nome: "VASSOURA", img: "objetos/vassoura.png" },
+                { nome: "GARFO", img: "objetos/garfo.png" },
+                { nome: "PINCEL", img: "objetos/pincel.png" },
+                { nome: "ESCOVA", img: "objetos/escova.png" },
+                { nome: "GARRAFA", img: "objetos/garrafa.png" },
+                { nome: "COPO", img: "objetos/copo.png" },
+                { nome: "BALDE", img: "objetos/balde.png" },
+                { nome: "ÓCULOS", img: "objetos/oculos.png" },
+                { nome: "CESTO", img: "objetos/cesto.png" },
+                { nome: "COLHER", img: "objetos/colher.png" }
+            ]
+        },
+        frutos: {
+            nome: "Frutos",
+            imgCapa: "frutas/morango.png",
+            itens: [
+                { nome: "BANANA", img: "frutas/banana.png" },
+                { nome: "CEREJA", img: "frutas/cereja.png" },
+                { nome: "UVAS", img: "frutas/uvas.png" },
+                { nome: "MORANGO", img: "frutas/morango.png" },
+                { nome: "MELANCIA", img: "frutas/melancia.png" },
+                { nome: "LARANJA", img: "frutas/laranja.png" },
+                { nome: "PAPAIA", img: "frutas/papaia.png" },
+                { nome: "MAÇÃ", img: "frutas/maca.png" },
+                { nome: "AMORA", img: "frutas/amora.png" },
+                { nome: "MELÃO", img: "frutas/melao.png" },
+                { nome: "MARACUJÁ", img: "frutas/maracuja.png" },
+                { nome: "LIMÃO", img: "frutas/limao.png" },
+                { nome: "GOIABA", img: "frutas/goiaba.png" },
+                { nome: "FIGO", img: "frutas/figo.png" },
+                { nome: "DIÓSPIRO", img: "frutas/diospiro.png" },
+                { nome: "CASTANHA", img: "frutas/castanha.png" },
+                { nome: "ROMÃ", img: "frutas/roma.png" }
+            ]
+        },
+        material: {
+            nome: "Material Escolar",
+            imgCapa: "materialescolar/afia.png",
+            itens: [
+                { nome: "MOCHILA", img: "materialescolar/mochila.png" },
+                { nome: "TESOURA", img: "materialescolar/tesoura.png" },
+                { nome: "LÁPIS", img: "materialescolar/lapis.png" },
+                { nome: "LIVRO", img: "materialescolar/livro.png" },
+                { nome: "AFIA", img: "materialescolar/afia.png" },
+                { nome: "CANETA", img: "materialescolar/caneta.png" },
+                { nome: "RÉGUA", img: "materialescolar/regua.png" },
+                { nome: "BORRACHA", img: "materialescolar/borracha.png" },
+                { nome: "COLA", img: "materialescolar/cola.png" },
+                { nome: "ESTOJO", img: "materialescolar/estojo.png" }
+            ]
+        }
     },
-    relatorio: { 
-        titulo: "MUITO BEM!", 
-        pontosTotal: "Pontos Totais:", 
-        tempoTotal: "Tempo Total:" 
-    }
+
+    relatorios: [
+        { min: 1200, titulo: "BRILHANTE!", img: "taca_1.png" },
+        { min: 800, titulo: "MUITO BEM!", img: "taca_2.png" },
+        { min: 500, titulo: "BOM TRABALHO!", img: "taca_3.png" },
+        { min: 0, titulo: "TENTA OUTRA VEZ!", img: "taca_4.png" }
+    ]
 };
