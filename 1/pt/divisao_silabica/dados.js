e088c2a91fef87f1c94487a4d8e339dbf0d77d01
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-21 21:45:04 +0100

    Update dados.js

--- 1/pt/divisao_silabica/dados.js
+++ 1/pt/divisao_silabica/dados.js
@@ -1,1 +1,72 @@
+const JOGO_CONFIG = {
+    nomeJogo: "CORTA-SÍLABAS",
+    linkVoltar: "../", 
+    textoVoltar: "VOLTAR",
+    caminhoImg: "../../../img/",
+    caminhoIcons: "../../../icons/",
+    
+    pontuacao: {
+        acerto: 100, 
+        erro: 50            
+    },
 
+    textos: {
+        tituloPagina: "Pequenos Leitores - Corta-Sílabas",
+        tituloLinha1: "PEQUENOS",
+        tituloLinha2: "LEITORES",
+        subtitulo: "Português | 1º Ano",
+        rodape: "&copy; Pequenos Leitores - Recursos Educativos",
+        instrucao: "Clica entre as letras para separar as sílabas!"
+    },
+
+    sons: {
+        acerto: "https://cdn.pixabay.com/audio/2021/08/04/audio_0625c1539c.mp3",
+        erro: "https://cdn.pixabay.com/audio/2022/03/10/audio_c330c67761.mp3",
+        vitoria: "https://cdn.pixabay.com/audio/2024/02/07/audio_293963428f.mp3"
+    },
+
+    iconesMenu: { home: "home.png", pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" },
+    links: { home: "/jogos", pre: "/jogos/pre", ano1: "/jogos/1", ano2: "/jogos/2", ano3: "/jogos/3", ano4: "/jogos/4" },
+
+    categorias: {
+        animais: {
+            nome: "Animais",
+            imgCapa: "animaisdomesticos/gato.png",
+            itens: [
+                { nome: "GATO", silabas: ["GA", "TO"], img: "animaisdomesticos/gato.png" },
+                { nome: "VACA", silabas: ["VA", "CA"], img: "animaisdomesticos/vaca.png" },
+                { nome: "PATO", silabas: ["PA", "TO"], img: "animaisdomesticos/pato.png" },
+                { nome: "CAVALO", silabas: ["CA", "VA", "LO"], img: "animaisdomesticos/cavalo.png" },
+                { nome: "MACACO", silabas: ["MA", "CA", "CO"], img: "animaisselvagens/macaco.png" },
+                { nome: "GIRAFA", silabas: ["GI", "RA", "FA"], img: "animaisselvagens/girafa.png" },
+                { nome: "COELHO", silabas: ["CO", "E", "LHO"], img: "animaisdomesticos/coelho.png" },
+                { nome: "GALINHA", silabas: ["GA", "LI", "NHA"], img: "animaisdomesticos/galinha.png" },
+                { nome: "CARRO", silabas: ["CA", "RRO"], img: "objetos/carro.png" }, // Exemplo RR junto
+                { nome: "PÁSSARO", silabas: ["PÁ", "SSARO"], img: "animaisselvagens/pombo.png" } // Exemplo SS junto
+            ]
+        },
+        frutos: {
+            nome: "Frutos",
+            imgCapa: "frutas/morango.png",
+            itens: [
+                { nome: "MAÇÃ", silabas: ["MA", "ÇÃ"], img: "frutas/maca.png" },
+                { nome: "UVAS", silabas: ["U", "VAS"], img: "frutas/uvas.png" },
+                { nome: "BANANA", silabas: ["BA", "NA", "NA"], img: "frutas/banana.png" },
+                { nome: "MORANGO", silabas: ["MO", "RAN", "GO"], img: "frutas/morango.png" },
+                { nome: "CEREJA", silabas: ["CE", "RE", "JA"], img: "frutas/cereja.png" },
+                { nome: "LIMÃO", silabas: ["LI", "MÃO"], img: "frutas/limao.png" },
+                { nome: "LARANJA", silabas: ["LA", "RAN", "JA"], img: "frutas/laranja.png" },
+                { nome: "MELANCIA", silabas: ["ME", "LAN", "CI", "A"], img: "frutas/melancia.png" },
+                { nome: "ABACATE", silabas: ["A", "BA", "CA", "TE"], img: "frutas/abacate.png" },
+                { nome: "PÊSSEGO", silabas: ["PÊ", "SSEGO"], img: "frutas/pessego.png" } // SS junto
+            ]
+        }
+    },
+
+    relatorios: [
+        { min: 1000, titulo: "BRILHANTE!", img: "taca_1.png" },
+        { min: 700, titulo: "MUITO BEM!", img: "taca_2.png" },
+        { min: 400, titulo: "BOM TRABALHO!", img: "taca_3.png" },
+        { min: 0, titulo: "TENTA OUTRA VEZ!", img: "taca_4.png" }
+    ]
+};
