6e7bb5dc1bbde9c4c9e1323c8787d5be85cd4b0e
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-21 09:57:00 +0100

    Update jogo.js

--- 1/pt/ordenar/jogo.js
+++ 1/pt/ordenar/jogo.js
@@ -14,12 +14,12 @@ const sndAcerto = new Audio(JOGO_CONFIG.sons.acerto);
 const sndErro = new Audio(JOGO_CONFIG.sons.erro);
 const sndVitoria = new Audio(JOGO_CONFIG.sons.vitoria);
 
-// --- COORDENADAS RECALIBRADAS (Cartão 1 movido 5% para a direita) ---
+// --- COORDENADAS RECALIBRADAS (Cartão 1 movido 2% para a esquerda) ---
 const BOX_CFG = {
     top: 38,          
     height: 38,       
     width: 21.4,      
-    lefts: [4.8, 25.2, 50.6, 75.0] // O primeiro valor mudou de -0.2 para 4.8
+    lefts: [2.8, 25.2, 50.6, 75.0] // O primeiro valor mudou de 4.8 para 2.8
 };
 
 function startLogic() {
