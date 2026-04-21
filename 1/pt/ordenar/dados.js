3cfb8ea27bdaafc47d1e652ab55f327c9b6297a1
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-21 06:35:36 +0100

    Update dados.js

--- 1/pt/ordenar/dados.js
+++ 1/pt/ordenar/dados.js
@@ -4,7 +4,6 @@ const JOGO_CONFIG = {
     textoVoltar: "VOLTAR",
     caminhoImg: "../../../img/",
     caminhoIcons: "../../../icons/",
-    caminhoCat: "../../../imgcategorias/", 
     
     pontuacao: {
         acertoNivel1: 100,
@@ -18,7 +17,7 @@ const JOGO_CONFIG = {
         tituloLinha2: "LEITORES",
         subtitulo: "Português | 1º Ano",
         rodape: "&copy; Pequenos Leitores - Recursos Educativos",
-        instrucao: "Ordena os cartões no armário mágico!"
+        instrucao: "Ordena os cartões do menor para o maior (A-Z)!"
     },
 
     sons: {
@@ -33,24 +32,46 @@ const JOGO_CONFIG = {
     categorias: {
         cat1: {
             nome: "Letras Consecutivas", 
-            img: "cat1.png",
+            imgCapa: "cat1.png",
             rondas: [
                 { itens: ["A", "B", "C", "D"] }, { itens: ["E", "F", "G", "H"] },
                 { itens: ["I", "J", "K", "L"] }, { itens: ["M", "N", "O", "P"] },
                 { itens: ["Q", "R", "S", "T"] }, { itens: ["U", "V", "W", "X"] },
-                { itens: ["C", "D", "E", "F"] }, { itens: ["O", "P", "Q", "R"] },
-                { itens: ["G", "H", "I", "J"] }, { itens: ["K", "L", "M", "N"] }
+                { itens: ["G", "H", "I", "J"] }, { itens: ["O", "P", "Q", "R"] },
+                { itens: ["B", "C", "D", "E"] }, { itens: ["S", "T", "U", "V"] }
             ]
         },
         cat2: {
+            nome: "Letras Alternadas", 
+            imgCapa: "cat2.png",
+            rondas: [
+                { itens: ["A", "X", "M", "E"] }, { itens: ["B", "L", "O", "V"] },
+                { itens: ["C", "G", "Z", "Q"] }, { itens: ["D", "H", "K", "W"] },
+                { itens: ["P", "Y", "U", "S"] }, { itens: ["F", "R", "T", "J"] },
+                { itens: ["N", "I", "K", "A"] }, { itens: ["E", "Z", "W", "M"] },
+                { itens: ["H", "C", "L", "P"] }, { itens: ["V", "D", "R", "G"] }
+            ]
+        },
+        cat3: {
+            nome: "Mesma Letra Inicial", 
+            imgCapa: "cat3.png",
+            rondas: [
+                { itens: ["AMORA", "ANANÁS", "ANEL", "AVENTAL"] }, { itens: ["BOLA", "BOLO", "BONECA", "BOTA"] },
+                { itens: ["CASA", "CAVALO", "COELHO", "COPO"] }, { itens: ["DADO", "DEDO", "DENTE", "DOCE"] },
+                { itens: ["GATO", "GAVETA", "GELO", "GIRAFA"] }, { itens: ["MÃE", "MALA", "MAPA", "MEIA"] },
+                { itens: ["PAPA", "PATO", "PENA", "PIPA"] }, { itens: ["RATO", "REDE", "RISO", "RODA"] },
+                { itens: ["SAPA", "SELO", "SINO", "SOFA"] }, { itens: ["TATA", "TETO", "TINA", "TOCA"] }
+            ]
+        },
+        cat4: {
             nome: "Palavras Mistas", 
-            img: "cat2.png",
+            imgCapa: "cat4.png",
             rondas: [
                 { itens: ["CÃO", "GATO", "PATO", "RATO"] }, { itens: ["SOL", "LUA", "MAR", "CÉU"] },
                 { itens: ["UM", "DOIS", "TRÊS", "QUATRO"] }, { itens: ["MÃE", "PAI", "AVÓ", "AVÔ"] },
                 { itens: ["AZUL", "ROSA", "VERDE", "VERMELHO"] }, { itens: ["BOLA", "DADO", "PEÃO", "URSO"] },
-                { itens: ["MAÇÃ", "PERA", "UVA", "FIGO"] }, { itens: ["LISBOA", "PORTO", "FARO", "BRAGA"] },
-                { itens: ["LÁPIS", "COLA", "AFIA", "LIVRO"] }, { itens: ["PAPAIA", "AMORA", "ROMÃ", "KIWI"] }
+                { itens: ["MAÇÃ", "PERA", "UVA", "FIGO"] }, { itens: ["FARO", "BRAGA", "EVORA", "BEJA"] },
+                { itens: ["LÁPIS", "COLA", "AFIA", "LIVRO"] }, { itens: ["BOLO", "SUMO", "PÃO", "LEITE"] }
             ]
         }
     }
