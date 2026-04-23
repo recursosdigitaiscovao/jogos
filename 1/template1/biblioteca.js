8a63c3c36219a0e02f3e36fec947905a81852cb1
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-23 13:53:48 +0100

    Update biblioteca.js

--- 1/template1/biblioteca.js
+++ 1/template1/biblioteca.js
@@ -1,1 +1,26 @@
-
+const BIBLIOTECA_TEMAS = {
+    "portugues": {
+        tituloL2: "Leitores",
+        corPagina: "#e9f0f8",
+        corPrimaria: "#5ba4e5",
+        corEscura: "#3d7db8",
+        corTexto: "#5d7082",
+        voltarMobile: "voltar_az.png" // Ícone azul
+    },
+    "matematica": {
+        tituloL2: "Matemáticos",
+        corPagina: "#e8f9f4",
+        corPrimaria: "#45cfa8",
+        corEscura: "#2BA886",
+        corTexto: "#45cfa8",
+        voltarMobile: "voltar_vr.png" // Ícone verde
+    },
+    "estudo": {
+        tituloL2: "Exploradores",
+        corPagina: "#EAE2E5",
+        corPrimaria: "#994D4D",
+        corEscura: "#6C3737",
+        corTexto: "#994D4D",
+        voltarMobile: "voltar_rs.png" // Ícone castanho/rosa
+    }
+};
