9df7194b3196f938ca4d96039a5a8d6f020f145b
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-24 00:06:04 +0100

    Update dados.js

--- 3/mat/dados.js
+++ 3/mat/dados.js
@@ -1,44 +1,26 @@
 const JOGO_CONFIG = {
-    // Títulos e Textos da Página
-    textos: {
-        tituloLinha1: "Super",
-        tituloLinha2: "Matemáticos",
-        subtitulo: "Matemática | 3º Ano",
-        intro: "Explora as operações e diverte-te a calcular!",
-        rodape: "&copy; Super Matemáticos - Recursos Educativos"
-    },
-
-    // Caminhos
-    caminhoIconsMenu: "../../icons/",
+    areaAtiva: "matematica", // Escolha: "portugues", "matematica", "estudo" ou "pre"
+    anoAtivo: "ano3",      // Escolha: "pre", "ano1", "ano2", "ano3", "ano4"
+    
+    caminhoIconsMenu: "../../icons/", 
     caminhoIconsJogos: "iconjogos/",
-
-    // Ícones
+    
     iconesMenu: {
-        home: "home.png",
-        pre: "iconpre.png",
-        ano1: "icon1.png",
-        ano2: "icon2.png",
-        ano3: "icon3.png",
-        ano4: "icon4.png",
-        voltar: "voltar.png"
-    },
-
-    // Links do Menu
-    links: {
-        home: "/jogos", 
-        pre: "/jogos/pre",
-        ano1: "/jogos/1",
-        ano2: "/jogos/2",
-        ano3: "/jogos/3",
-        ano4: "/jogos/4"
+        home: "home.png", pre: "iconpre.png", ano1: "icon1.png", 
+        ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png", voltar: "voltar.png"
     },
     
-    // Lista de Jogos
+    links: {
+        home: "/jogos", pre: "/jogos/pre", ano1: "/jogos/1", 
+        ano2: "/jogos/2", ano3: "/jogos/3", ano4: "/jogos/4"
+    },
+    
     listaJogos: [
         { nome: "jogo1", icon: "1.png", link: "nome1/" },
         { nome: "jogo2", icon: "2.png", link: "nome2/" },
         { nome: "jogo3", icon: "3.png", link: "nome3/" },
         { nome: "jogo4", icon: "4.png", link: "nome4/" },
-        { nome: "jogo5", icon: "5.png", link: "nome5/" },
+        { nome: "jogo5", icon: "5.png", link: "nome4/" },
+        // ... adicione os restantes
     ]
 };
