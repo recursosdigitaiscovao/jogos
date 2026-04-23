9772774eba4428c885c24f2215b888c989704992
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-23 10:08:51 +0100

    Update dados.js

--- 1/mat/dados.js
+++ 1/mat/dados.js
@@ -1,5 +1,4 @@
 const JOGO_CONFIG = {
-    // Títulos e Textos da Página
     textos: {
         tituloLinha1: "Pequenos",
         tituloLinha2: "Matemáticos",
@@ -7,33 +6,17 @@ const JOGO_CONFIG = {
         intro: "Explora os números e diverte-te a contar!",
         rodape: "&copy; Pequenos Matemáticos - Recursos Educativos"
     },
-
-    // Caminhos
     caminhoIconsMenu: "../../icons/",
     caminhoIconsJogos: "iconjogos/",
-
-    // Ícones
     iconesMenu: {
-        home: "home.png",
-        pre: "iconpre.png",
-        ano1: "icon1.png",
-        ano2: "icon2.png",
-        ano3: "icon3.png",
-        ano4: "icon4.png",
-        voltar: "voltar.png"
+        home: "home.png", pre: "iconpre.png", ano1: "icon1.png", 
+        ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png", voltar: "voltar.png"
     },
-
-    // Links do Menu
     links: {
-        home: "/jogos", 
-        pre: "/jogos/pre",
-        ano1: "/jogos/1",
-        ano2: "/jogos/2",
-        ano3: "/jogos/3",
-        ano4: "/jogos/4"
+        home: "/jogos", pre: "/jogos/pre", ano1: "/jogos/1", 
+        ano2: "/jogos/2", ano3: "/jogos/3", ano4: "/jogos/4",
+        voltar: "../" // Retrocede apenas um nível
     },
-    
-    // Lista de Jogos
     listaJogos: [
         { nome: "Contar os Números", icon: "conta.png", link: "conta/index.html" },
         { nome: "Formas Geométricas", icon: "formas.png", link: "formas/index.html" },
