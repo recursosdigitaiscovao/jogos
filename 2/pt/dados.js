e22389378609ed48e6d076c9c4271866df509e18
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-23 11:02:22 +0100

    Update dados.js

--- 2/pt/dados.js
+++ 2/pt/dados.js
@@ -1,5 +1,4 @@
 const JOGO_CONFIG = {
-    // Títulos e Textos da Página
     textos: {
         tituloLinha1: "Jovens",
         tituloLinha2: "Leitores",
@@ -7,38 +6,26 @@ const JOGO_CONFIG = {
         intro: "Explora as palavras, as frases e diverte-te a ler!",
         rodape: "&copy; Jovens Leitores - Recursos Educativos"
     },
-
-    // Caminhos
     caminhoIconsMenu: "../../icons/",
     caminhoIconsJogos: "iconjogos/",
-
-    // Ícones
     iconesMenu: {
-        home: "home.png",
-        pre: "iconpre.png",
-        ano1: "icon1.png",
-        ano2: "icon2.png",
-        ano3: "icon3.png",
-        ano4: "icon4.png",
-        voltar: "voltar.png"
+        home: "home.png", pre: "iconpre.png", ano1: "icon1.png", 
+        ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png", voltar: "voltar.png"
     },
-
-    // Links do Menu
     links: {
-        home: "/jogos", 
-        pre: "/jogos/pre",
-        ano1: "/jogos/1",
-        ano2: "/jogos/2",
-        ano3: "/jogos/3",
-        ano4: "/jogos/4"
+        home: "/jogos", pre: "/jogos/pre", ano1: "/jogos/1", 
+        ano2: "/jogos/2", ano3: "/jogos/3", ano4: "/jogos/4",
+        voltar: "../" // Retrocede apenas um nível
     },
-    
-    // Lista de Jogos
     listaJogos: [
-        { nome: "jogo1", icon: "1.png", link: "nome1/" },
-        { nome: "jogo2", icon: "2.png", link: "nome2/" },
-        { nome: "jogo3", icon: "3.png", link: "nome3/" },
-        { nome: "jogo4", icon: "4.png", link: "nome4/" },
-        { nome: "jogo5", icon: "5.png", link: "nome4/" },
+        { nome: "Primeira Letra", icon: "primeira_letra.png", link: "primeira_letra/" },
+        { nome: "Conta as Sílabas", icon: "conta_silabas.png", link: "conta_silabas/" },
+        { nome: "Ordena Sílabas", icon: "silabas.png", link: "silabas/" },
+        { nome: "Forma Palavras", icon: "liga_letras.png", link: "liga_letras/" },
+        { nome: "Corta Sílabas", icon: "divisao_silabica.png", link: "divisao_silabica/" },
+        { nome: "Ordem Alfabética", icon: "ordenar.png", link: "ordenar/" },
+        { nome: "Descobre a Palavra", icon: "forca.png", link: "forca/" },
+        { nome: "Sopa de Letras", icon: "sopa_letras.png", link: "sopa_letras/" },
+        { nome: "Copia Palavras", icon: "copia_palavras.png", link: "copia_palavras/" }
     ]
 };
