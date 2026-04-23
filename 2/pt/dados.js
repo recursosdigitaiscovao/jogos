0f1d7516a35c0df834ac3439039052ae79b253b8
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-23 11:07:02 +0100

    Update dados.js

--- 2/pt/dados.js
+++ 2/pt/dados.js
@@ -18,14 +18,10 @@ const JOGO_CONFIG = {
         voltar: "../" // Retrocede apenas um nível
     },
     listaJogos: [
-        { nome: "Primeira Letra", icon: "primeira_letra.png", link: "primeira_letra/" },
-        { nome: "Conta as Sílabas", icon: "conta_silabas.png", link: "conta_silabas/" },
-        { nome: "Ordena Sílabas", icon: "silabas.png", link: "silabas/" },
-        { nome: "Forma Palavras", icon: "liga_letras.png", link: "liga_letras/" },
-        { nome: "Corta Sílabas", icon: "divisao_silabica.png", link: "divisao_silabica/" },
-        { nome: "Ordem Alfabética", icon: "ordenar.png", link: "ordenar/" },
-        { nome: "Descobre a Palavra", icon: "forca.png", link: "forca/" },
-        { nome: "Sopa de Letras", icon: "sopa_letras.png", link: "sopa_letras/" },
-        { nome: "Copia Palavras", icon: "copia_palavras.png", link: "copia_palavras/" }
+        { nome: "jogo1", icon: "1.png", link: "nome1/" },
+        { nome: "jogo2", icon: "2.png", link: "nome2/" },
+        { nome: "jogo3", icon: "3.png", link: "nome3/" },
+        { nome: "jogo4", icon: "4.png", link: "nome4/" },
+        { nome: "jogo5", icon: "5.png", link: "nome4/" },
     ]
 };
