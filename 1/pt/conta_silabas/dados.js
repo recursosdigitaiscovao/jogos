6e0bb90378f1df4f79604a8a8a62bdc0f787fe47
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-24 22:01:04 +0100

    Update dados.js

--- 1/pt/conta_silabas/dados.js
+++ 1/pt/conta_silabas/dados.js
@@ -1,3 +1,4 @@
+// === CONFIGURAÇÃO MESTRE ===
 const CONFIG_MESTRE = {
     ano: "ano1",        
     area: "portugues",  
@@ -39,28 +40,83 @@ const JOGO_CONFIG = {
     links: { home: "/jogos", pre: "/jogos/pre", ano1: "/jogos/1", ano2: "/jogos/2", ano3: "/jogos/3", ano4: "/jogos/4" },
     categorias: {
         animais: {
-            nome: "Animais", exemplo: "CA-VA-LO", exemploImg: "animaisdomesticos/cavalo.png", total: 3, imgCapa: "animaisdomesticos/gato.png",
+            nome: "Animais", exemplo: "E-LE-FAN-TE", exemploImg: "animaisselvagens/elefante.png", total: 4, imgCapa: "animaisselvagens/leao.png",
             itens: [
-                { nome: "VACA", silabas: 2, img: "animaisdomesticos/vaca.png" },
-                { nome: "GATO", silabas: 2, img: "animaisdomesticos/gato.png" },
-                { nome: "PATO", silabas: 2, img: "animaisdomesticos/pato.png" },
-                { nome: "PERU", silabas: 2, img: "animaisdomesticos/peru.png" },
-                { nome: "CÃO", silabas: 1, img: "animaisdomesticos/cao.png" },
-                { nome: "CAVALO", silabas: 3, img: "animaisdomesticos/cavalo.png" },
-                { nome: "BURRO", silabas: 2, img: "animaisdomesticos/burro.png" },
-                { nome: "GALINHA", silabas: 3, img: "animaisdomesticos/galinha.png" },
-                { nome: "COELHO", silabas: 3, img: "animaisdomesticos/coelho.png" },
-                { nome: "PORCO", silabas: 2, img: "animaisdomesticos/porco.png" }
+                { nome: "ABELHA", silabas: 3, img: "animaisselvagens/abelha.png" },
+                { nome: "ÁGUIA", silabas: 3, img: "animaisselvagens/aguia.png" },
+                { nome: "ARANHA", silabas: 3, img: "animaisselvagens/aranha.png" },
+                { nome: "BALEIA", silabas: 3, img: "animaisselvagens/baleia.png" },
+                { nome: "CANGURU", silabas: 3, img: "animaisselvagens/canguru.png" },
+                { nome: "CARANGUEIJO", silabas: 4, img: "animaisselvagens/carangueijo.png" },
+                { nome: "COALA", silabas: 3, img: "animaisselvagens/coala.png" },
+                { nome: "CROCODILO", silabas: 4, img: "animaisselvagens/crocodilo.png" },
+                { nome: "ELEFANTE", silabas: 4, img: "animaisselvagens/elefante.png" },
+                { nome: "FOCA", silabas: 2, img: "animaisselvagens/foca.png" },
+                { nome: "GORILA", silabas: 3, img: "animaisselvagens/gorila.png" },
+                { nome: "LEÃO", silabas: 2, img: "animaisselvagens/leao.png" },
+                { nome: "MACACO", silabas: 3, img: "animaisselvagens/macaco.png" },
+                { nome: "PAPAGAIO", silabas: 4, img: "animaisselvagens/papagaio.png" }
             ]
         },
         frutos: {
-            nome: "Frutos", exemplo: "MA-ÇÃ", exemploImg: "frutas/maca.png", total: 2, imgCapa: "frutas/morango.png",
+            nome: "Frutos", exemplo: "MO-RAN-GO", exemploImg: "frutas/morango.png", total: 3, imgCapa: "frutas/maca.png",
             itens: [
+                { nome: "AMORA", silabas: 3, img: "frutas/amora.png" },
+                { nome: "ANANÁS", silabas: 3, img: "frutas/ananas.png" },
                 { nome: "BANANA", silabas: 3, img: "frutas/banana.png" },
+                { nome: "CASTANHA", silabas: 3, img: "frutas/castanha.png" },
                 { nome: "CEREJA", silabas: 3, img: "frutas/cereja.png" },
-                { nome: "UVAS", silabas: 2, img: "frutas/uvas.png" },
-                { nome: "MORANGO", silabas: 3, img: "frutas/morango.png" },
-                { nome: "MAÇÃ", silabas: 2, img: "frutas/maca.png" }
+                { nome: "DIÓSPIRO", silabas: 3, img: "frutas/diospiro.png" },
+                { nome: "FIGO", silabas: 2, img: "frutas/figo.png" },
+                { nome: "GOIABA", silabas: 3, img: "frutas/goiaba.png" },
+                { nome: "KIWI", silabas: 2, img: "frutas/kiwi.png" },
+                { nome: "LARANJA", silabas: 3, img: "frutas/laranja.png" },
+                { nome: "LIMÃO", silabas: 2, img: "frutas/limao.png" },
+                { nome: "MAÇÃ", silabas: 2, img: "frutas/maca.png" },
+                { nome: "MARACUJÁ", silabas: 4, img: "frutas/maracuja.png" },
+                { nome: "MELANCIA", silabas: 4, img: "frutas/melancia.png" },
+                { nome: "MELÃO", silabas: 2, img: "frutas/melao.png" },
+                { nome: "MORANGO", silabas: 3, img: "frutas/morango.png" }
+            ]
+        },
+        objetos: {
+            nome: "Objetos", exemplo: "BO-NE-CA", exemploImg: "objetos/boneca.png", total: 3, imgCapa: "objetos/bola.png",
+            itens: [
+                { nome: "BALDE", silabas: 2, img: "objetos/balde.png" },
+                { nome: "BOLA", silabas: 2, img: "objetos/bola.png" },
+                { nome: "BONECA", silabas: 3, img: "objetos/boneca.png" },
+                { nome: "CARRINHO", silabas: 3, img: "objetos/carrinho.png" },
+                { nome: "CESTO", silabas: 2, img: "objetos/cesto.png" },
+                { nome: "COLHER", silabas: 2, img: "objetos/colher.png" },
+                { nome: "COPO", silabas: 2, img: "objetos/copo.png" },
+                { nome: "DADO", silabas: 2, img: "objetos/dado.png" },
+                { nome: "DEDAL", silabas: 2, img: "objetos/dedal.png" },
+                { nome: "DRAGÃO", silabas: 2, img: "objetos/dragao.png" },
+                { nome: "ESCOVA", silabas: 3, img: "objetos/escova.png" },
+                { nome: "GARFO", silabas: 2, img: "objetos/garfo.png" },
+                { nome: "GARRAFA", silabas: 3, img: "objetos/garrafa.png" },
+                { nome: "ISQUEIRO", silabas: 3, img: "objetos/isqueiro.png" },
+                { nome: "ÓCULOS", silabas: 3, img: "objetos/oculos.png" }
+            ]
+        },
+        material: {
+            nome: "Material Escolar", exemplo: "MO-CHI-LA", exemploImg: "materialescolar/mochila.png", total: 3, imgCapa: "materialescolar/estojo.png",
+            itens: [
+                { nome: "AFIA", silabas: 2, img: "materialescolar/afia.png" },
+                { nome: "BORRACHA", silabas: 3, img: "materialescolar/borracha.png" },
+                { nome: "CADERNO", silabas: 3, img: "materialescolar/caderno.png" },
+                { nome: "CANETA", silabas: 3, img: "materialescolar/caneta.png" },
+                { nome: "CAPA", silabas: 2, img: "materialescolar/capa.png" },
+                { nome: "COLA", silabas: 2, img: "materialescolar/cola.png" },
+                { nome: "ESQUADRO", silabas: 3, img: "materialescolar/esquadro.png" },
+                { nome: "ESTOJO", silabas: 3, img: "materialescolar/estojo.png" },
+                { nome: "FOLHA", silabas: 2, img: "materialescolar/folha.png" },
+                { nome: "LÁPIS", silabas: 2, img: "materialescolar/lapis.png" },
+                { nome: "LIVRO", silabas: 2, img: "materialescolar/livro.png" },
+                { nome: "MOCHILA", silabas: 3, img: "materialescolar/mochila.png" },
+                { nome: "PINCEL", silabas: 2, img: "materialescolar/pincel.png" },
+                { nome: "RÉGUA", silabas: 2, img: "materialescolar/regua.png" },
+                { nome: "TESOURA", silabas: 3, img: "materialescolar/tesoura.png" }
             ]
         }
     },
