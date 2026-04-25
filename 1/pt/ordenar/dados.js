24532d7192ea32ff26f000ca334c7088740a1739
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-26 00:14:10 +0100

    Update dados.js

--- 1/pt/ordenar/dados.js
+++ 1/pt/ordenar/dados.js
@@ -12,7 +12,7 @@ const BIBLIOTECA_CONTEUDO = {
     "ano1": {
         "portugues": { 
             t1: "ORDEM", t2: "ALFABÉTICA", sub: "Português | 1º Ano", 
-            intro: "Clica nos cartões seguindo a ordem do alfabeto!", 
+            intro: "Clica ou arrasta os cartões seguindo a ordem do alfabeto!", 
             rodape: "&copy; Pequenos Leitores" 
         }
     }
@@ -48,28 +48,22 @@ const JOGO_CONFIG = {
             itens: [
                 { nome: "A", img: "letras/letra_a.png" }, { nome: "C", img: "letras/letra_c.png" }, { nome: "E", img: "letras/letra_e.png" }, { nome: "G", img: "letras/letra_g.png" },
                 { nome: "I", img: "letras/letra_i.png" }, { nome: "L", img: "letras/letra_l.png" }, { nome: "M", img: "letras/letra_m.png" }, { nome: "O", img: "letras/letra_o.png" },
-                { nome: "P", img: "letras/letra_p.png" }, { nome: "R", img: "letras/letra_r.png" }, { nome: "S", img: "letras/letra_s.png" }, { nome: "U", img: "letras/letra_u.png" },
-                { nome: "V", img: "letras/letra_v.png" }, { nome: "X", img: "letras/letra_x.png" }, { nome: "Z", img: "letras/letra_z.png" }, { nome: "B", img: "letras/letra_b.png" },
-                { nome: "D", img: "letras/letra_d.png" }, { nome: "F", img: "letras/letra_f.png" }, { nome: "H", img: "letras/letra_h.png" }, { nome: "J", img: "letras/letra_j.png" },
-                { nome: "N", img: "letras/letra_n.png" }, { nome: "T", img: "letras/letra_t.png" }
+                { nome: "P", img: "letras/letra_p.png" }, { nome: "R", img: "letras/letra_r.png" }, { nome: "S", img: "letras/letra_s.png" }, { nome: "T", img: "letras/letra_t.png" },
+                { nome: "U", img: "letras/letra_u.png" }, { nome: "V", img: "letras/letra_v.png" }, { nome: "X", img: "letras/letra_x.png" }, { nome: "Z", img: "letras/letra_z.png" },
+                { nome: "B", img: "letras/letra_b.png" }, { nome: "D", img: "letras/letra_d.png" }, { nome: "F", img: "letras/letra_f.png" }, { nome: "H", img: "letras/letra_h.png" }
             ]
         },
         mesma_inicial: {
             nome: "Mesma Inicial", exemplo: "Abelha-Águia", exemploImg: "animaisselvagens/abelha.png", imgCapa: "animaisselvagens/abelha.png",
             itens: [
-                // Iniciais em A
                 { nome: "ABELHA", img: "animaisselvagens/abelha.png" }, { nome: "ÁGUIA", img: "animaisselvagens/aguia.png" }, { nome: "ARANHA", img: "animaisselvagens/aranha.png" },
                 { nome: "AFIA", img: "materialescolar/afia.png" }, { nome: "ANEL", img: "objetos/anel.png" }, { nome: "AMORA", img: "frutas/amora.png" },
                 { nome: "ANANÁS", img: "frutas/ananas.png" }, { nome: "AVESTRUZ", img: "animaisselvagens/avestruz.png" },
-                // Iniciais em B
                 { nome: "BANANA", img: "frutas/banana.png" }, { nome: "BALEIA", img: "animaisselvagens/baleia.png" }, { nome: "BURRO", img: "animaisdomesticos/burro.png" },
                 { nome: "BALDE", img: "objetos/balde.png" }, { nome: "BOLA", img: "objetos/bola.png" }, { nome: "BONECA", img: "objetos/boneca.png" },
-                { nome: "BORRACHA", img: "materialescolar/borracha.png" },
-                // Iniciais em C
-                { nome: "CÃO", img: "animaisdomesticos/cao.png" }, { nome: "CAVALO", img: "animaisdomesticos/cavalo.png" }, { nome: "COELHO", img: "animaisdomesticos/coelho.png" },
-                { nome: "CADERNO", img: "materialescolar/caderno.png" }, { nome: "CANETA", img: "materialescolar/caneta.png" }, { nome: "CAPA", img: "materialescolar/capa.png" },
-                { nome: "CEREJA", img: "frutas/cereja.png" }, { nome: "CESTO", img: "objetos/cesto.png" }, { nome: "COLHER", img: "objetos/colher.png" },
-                { nome: "COPO", img: "objetos/copo.png" }
+                { nome: "BORRACHA", img: "materialescolar/borracha.png" }, { nome: "CABRA", img: "animaisdomesticos/cabra.png" },
+                { nome: "CAVALO", img: "animaisdomesticos/cavalo.png" }, { nome: "CADERNO", img: "materialescolar/caderno.png" },
+                { nome: "CANETA", img: "materialescolar/caneta.png" }, { nome: "CEREJA", img: "frutas/cereja.png" }
             ]
         },
         palavras_mistas: {
@@ -78,19 +72,17 @@ const JOGO_CONFIG = {
                 { nome: "BURRO", img: "animaisdomesticos/burro.png" }, { nome: "CABRA", img: "animaisdomesticos/cabra.png" }, { nome: "CÃO", img: "animaisdomesticos/cao.png" },
                 { nome: "COELHO", img: "animaisdomesticos/coelho.png" }, { nome: "DADO", img: "objetos/dado.png" }, { nome: "ESCOVA", img: "objetos/escova.png" },
                 { nome: "FOCA", img: "animaisdomesticos/foca.png" }, { nome: "GATO", img: "animaisdomesticos/gato.png" }, { nome: "HIENA", img: "animaisselvagens/hiena.png" },
-                { nome: "ISQUEIRO", img: "objetos/isqueiro.png" }, { nome: "JANELA", img: "objetos/janela.png" }, { nome: "KIWI", img: "frutas/kiwi.png" },
                 { nome: "LEÃO", img: "animaisselvagens/leao.png" }, { nome: "MAÇÃ", img: "frutas/maca.png" }, { nome: "NOVE", img: "numeros/nove.png" },
                 { nome: "OVELHA", img: "animaisdomesticos/ovelha.png" }, { nome: "PANDA", img: "animaisselvagens/panda.png" }, { nome: "RÉGUA", img: "materialescolar/regua.png" },
                 { nome: "SAPA", img: "objetos/pa.png" }, { nome: "TIGRE", img: "animaisselvagens/tigre.png" }, { nome: "UVA", img: "frutas/uva.png" },
-                { nome: "VELA", img: "objetos/vela.png" }, { nome: "ZEBRA", img: "animaisselvagens/zebra.png" }, { nome: "GALO", img: "animaisdomesticos/galo.png" },
-                { nome: "PATO", img: "animaisdomesticos/pato.png" }, { nome: "PERU", img: "animaisdomesticos/peru.png" }, { nome: "PORCO", img: "animaisdomesticos/porco.png" }
+                { nome: "VELA", img: "objetos/vela.png" }, { nome: "ZEBRA", img: "animaisselvagens/zebra.png" }
             ]
         }
     },
     relatorios: [
         { min: 90, max: 100, titulo: "És um craque do alfabeto!", img: "taca_1.png" },
         { min: 70, max: 89, titulo: "Muito bem!", img: "taca_2.png" },
-        { min: 50, max: 69, titulo: "Estás quase lá!", img: "taca_2.png" },
+        { min: 50, max: 69, titulo: "Estás quase lá!", img: "taca_3.png" },
         { min: 0, max: 49, titulo: "Continua a tentar!", img: "taca_4.png" }
     ]
 };
