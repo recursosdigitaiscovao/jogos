2b67ae1caaad53f5f99870d615075b81d63f837e
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-27 01:43:33 +0100

    Update categorias.js

--- pre/sombrasmagicas/categorias.js
+++ pre/sombrasmagicas/categorias.js
@@ -1,42 +1,16 @@
 const BIBLIOTECA_CATEGORIAS = {
     animais: {
-        nome: "Sombras: Animais", exemplo: "LEÃO", exemploImg: "animaisselvagens/leao.png", total: 14, imgCapa: "animaisselvagens/leao.png",
+        nome: "Sombras: Animais", exemploImg: "animaisselvagens/leao.png", imgCapa: "animaisselvagens/leao.png",
         itens: [
-            { nome: "ABELHA", silabas: 1, img: "animaisselvagens/abelha.png" },
-            { nome: "ÁGUIA", silabas: 1, img: "animaisselvagens/aguia.png" },
-            { nome: "ARANHA", silabas: 1, img: "animaisselvagens/aranha.png" },
-            { nome: "BALEIA", silabas: 1, img: "animaisselvagens/baleia.png" },
-            { nome: "CANGURU", silabas: 1, img: "animaisselvagens/canguru.png" },
-            { nome: "CARANGUEIJO", silabas: 1, img: "animaisselvagens/carangueijo.png" },
-            { nome: "COALA", silabas: 1, img: "animaisselvagens/coala.png" },
-            { nome: "CROCODILO", silabas: 1, img: "animaisselvagens/crocodilo.png" },
-            { nome: "ELEFANTE", silabas: 1, img: "animaisselvagens/elefante.png" },
-            { nome: "FOCA", silabas: 1, img: "animaisselvagens/foca.png" },
-            { nome: "GORILA", silabas: 1, img: "animaisselvagens/gorila.png" },
-            { nome: "LEÃO", silabas: 1, img: "animaisselvagens/leao.png" },
-            { nome: "MACACO", silabas: 1, img: "animaisselvagens/macaco.png" },
-            { nome: "PAPAGAIO", silabas: 1, img: "animaisselvagens/papagaio.png" }
+            { nome: "ABELHA", img: "animaisselvagens/abelha.png" }, { nome: "ÁGUIA", img: "animaisselvagens/aguia.png" }, { nome: "ARANHA", img: "animaisselvagens/aranha.png" }, { nome: "BALEIA", img: "animaisselvagens/baleia.png" }, { nome: "CANGURU", img: "animaisselvagens/canguru.png" }, { nome: "COALA", img: "animaisselvagens/coala.png" }, { nome: "ELEFANTE", img: "animaisselvagens/elefante.png" }, { nome: "LEÃO", img: "animaisselvagens/leao.png" }, { nome: "MACACO", img: "animaisselvagens/macaco.png" }, { nome: "PAPAGAIO", img: "animaisselvagens/papagaio.png" }
         ]
     },
     frutos: {
-        nome: "Sombras: Frutos", exemplo: "MORANGO", exemploImg: "frutas/morango.png", total: 16, imgCapa: "frutas/maca.png",
+        nome: "Sombras: Frutos", exemploImg: "frutas/morango.png", imgCapa: "frutas/maca.png",
         itens: [
-            { nome: "AMORA", silabas: 1, img: "frutas/amora.png" }, { nome: "ANANÁS", silabas: 1, img: "frutas/ananas.png" }, { nome: "BANANA", silabas: 1, img: "frutas/banana.png" }, { nome: "CASTANHA", silabas: 1, img: "frutas/castanha.png" }, { nome: "CEREJA", silabas: 1, img: "frutas/cereja.png" }, { nome: "DIÓSPIRO", silabas: 1, img: "frutas/diospiro.png" }, { nome: "FIGO", silabas: 1, img: "frutas/figo.png" }, { nome: "GOIABA", silabas: 1, img: "frutas/goiaba.png" }, { nome: "KIWI", silabas: 1, img: "frutas/kiwi.png" }, { nome: "LARANJA", silabas: 1, img: "frutas/laranja.png" }, { nome: "LIMÃO", silabas: 1, img: "frutas/limao.png" }, { nome: "MAÇÃ", silabas: 1, img: "frutas/maca.png" }, { nome: "MARACUJÁ", silabas: 1, img: "frutas/maracuja.png" }, { nome: "MELANCIA", silabas: 1, img: "frutas/melancia.png" }, { nome: "MELÃO", silabas: 1, img: "frutas/melao.png" }, { nome: "MORANGO", silabas: 1, img: "frutas/morango.png" }
-        ]
-    },
-    objetos: {
-        nome: "Sombras: Objetos", exemplo: "BOLA", exemploImg: "objetos/bola.png", total: 15, imgCapa: "objetos/bola.png",
-        itens: [
-            { nome: "BALDE", silabas: 1, img: "objetos/balde.png" }, { nome: "BOLA", silabas: 1, img: "objetos/bola.png" }, { nome: "BONECA", silabas: 1, img: "objetos/boneca.png" }, { nome: "CARRINHO", silabas: 1, img: "objetos/carrinho.png" }, { nome: "CESTO", silabas: 1, img: "objetos/cesto.png" }, { nome: "COLHER", silabas: 1, img: "objetos/colher.png" }, { nome: "COPO", silabas: 1, img: "objetos/copo.png" }, { nome: "DADO", silabas: 1, img: "objetos/dado.png" }, { nome: "DEDAL", silabas: 1, img: "objetos/dedal.png" }, { nome: "DRAGÃO", silabas: 1, img: "objetos/dragao.png" }, { nome: "ESCOVA", silabas: 1, img: "objetos/escova.png" }, { nome: "GARFO", silabas: 1, img: "objetos/garfo.png" }, { nome: "GARRAFA", silabas: 1, img: "objetos/garrafa.png" }, { nome: "ISQUEIRO", silabas: 1, img: "objetos/isqueiro.png" }, { nome: "ÓCULOS", silabas: 1, img: "objetos/oculos.png" }
-        ]
-    },
-    material: {
-        nome: "Sombras: Material", exemplo: "MOCHILA", exemploImg: "materialescolar/mochila.png", total: 15, imgCapa: "materialescolar/estojo.png",
-        itens: [
-            { nome: "AFIA", silabas: 1, img: "materialescolar/afia.png" }, { nome: "BORRACHA", silabas: 1, img: "materialescolar/borracha.png" }, { nome: "CADERNO", silabas: 1, img: "materialescolar/caderno.png" }, { nome: "CANETA", silabas: 1, img: "materialescolar/caneta.png" }, { nome: "CAPA", silabas: 1, img: "materialescolar/capa.png" }, { nome: "COLA", silabas: 1, img: "materialescolar/cola.png" }, { nome: "ESQUADRO", silabas: 1, img: "materialescolar/esquadro.png" }, { nome: "ESTOJO", silabas: 1, img: "materialescolar/estojo.png" }, { nome: "FOLHA", silabas: 1, img: "materialescolar/folha.png" }, { nome: "LÁPIS", silabas: 1, img: "materialescolar/lapis.png" }, { nome: "LIVRO", silabas: 1, img: "materialescolar/livro.png" }, { nome: "MOCHILA", silabas: 1, img: "materialescolar/mochila.png" }, { nome: "PINCEL", silabas: 1, img: "materialescolar/pincel.png" }, { nome: "RÉGUA", silabas: 1, img: "materialescolar/regua.png" }, { nome: "TESOURA", silabas: 1, img: "materialescolar/tesoura.png" }
+            { nome: "AMORA", img: "frutas/amora.png" }, { nome: "ANANÁS", img: "frutas/ananas.png" }, { nome: "BANANA", img: "frutas/banana.png" }, { nome: "CEREJA", img: "frutas/cereja.png" }, { nome: "KIWI", img: "frutas/kiwi.png" }, { nome: "LARANJA", img: "frutas/laranja.png" }, { nome: "MAÇÃ", img: "frutas/maca.png" }, { nome: "MELANCIA", img: "frutas/melancia.png" }, { nome: "MORANGO", img: "frutas/morango.png" }, { nome: "UVA", img: "frutas/uva.png" }
         ]
     }
 };
 
-// Vincula as categorias ao objeto principal de configuração
 JOGO_CONFIG.categorias = BIBLIOTECA_CATEGORIAS;
