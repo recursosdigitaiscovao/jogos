c3e31951f59114aa067d26cfd7ae48b1168ee8ed
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-28 22:33:54 +0100

    Update categorias.js

--- pre/pinta/categorias.js
+++ pre/pinta/categorias.js
@@ -1,18 +1,6 @@
-// === DEFINIÇÃO DAS CATEGORIAS (TAMANHOS DE GRELHA) ===
+// === CONFIGURAÇÃO DE DIFICULDADE ===
 const JOGO_CATEGORIAS = {
-    "facil": {
-        nome: "Grelha 3x3",
-        imgCapa: "nivel1.png", // Podes usar um ícone genérico
-        tamanho: 3
-    },
-    "medio": {
-        nome: "Grelha 4x4",
-        imgCapa: "nivel2.png",
-        tamanho: 4
-    },
-    "dificil": {
-        nome: "Grelha 5x5",
-        imgCapa: "nivel3.png",
-        tamanho: 5
-    }
+    "facil": { nome: "Grelha 3x3", tamanho: 3 },
+    "medio": { nome: "Grelha 4x4", tamanho: 4 },
+    "dificil": { nome: "Grelha 5x5", tamanho: 5 }
 };
