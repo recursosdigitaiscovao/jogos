a49e3c4138dbe858d9fdc5f69f378d645ce5b13b
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-29 07:00:17 +0100

    Update categorias.js

--- pre/labirinto/categorias.js
+++ pre/labirinto/categorias.js
@@ -1,1 +1,45 @@
-
+const JOGO_CATEGORIAS = {
+    "natureza": {
+        nome: "Jardim da Abelha",
+        imgCapa: "animaisselvagens/abelha.png",
+        personagem: "animaisselvagens/abelha.png",
+        objetivo: "objetos/flor.png", // Imagem da flor
+        itens: [
+            { 
+                nivel: 1, 
+                tamanho: 5, // Grelha 5x5
+                mapa: [
+                    [0, 0, 1, 0, 0],
+                    [1, 0, 1, 0, 1],
+                    [0, 0, 0, 0, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 0, 0, 1, 0] 
+                ],
+                inicio: [0, 0], // x, y
+                fim: [4, 4]
+            }
+        ]
+    },
+    "quinta": {
+        nome: "Caminho do Rato",
+        imgCapa: "animaisselvagens/rato.png",
+        personagem: "animaisselvagens/rato.png",
+        objetivo: "objetos/queijo.png",
+        itens: [
+            { 
+                nivel: 1, 
+                tamanho: 6,
+                mapa: [
+                    [0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 1, 0],
+                    [1, 1, 0, 0, 0, 0],
+                    [0, 0, 1, 1, 1, 1],
+                    [0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 1, 0]
+                ],
+                inicio: [0, 0],
+                fim: [5, 5]
+            }
+        ]
+    }
+};
