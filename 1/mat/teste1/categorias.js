5cffd9274f30e85de15b746812fa5bbda99c6993
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-30 11:14:43 +0100

    Update categorias.js

--- 1/mat/teste1/categorias.js
+++ 1/mat/teste1/categorias.js
@@ -1,1 +1,24 @@
-
+// === DEFINIÇÃO DAS DIFICULDADES DO COMBOIO ===
+const JOGO_CATEGORIAS = {
+    "facil": {
+        nome: "Números até 10",
+        imgCapa: "rd.png", // Usa o ícone padrão ou um comboio.png se tiveres
+        maxNum: 10,
+        passo: 1,
+        carruagens: 4
+    },
+    "medio": {
+        nome: "Números até 20",
+        imgCapa: "rd.png",
+        maxNum: 20,
+        passo: 1,
+        carruagens: 5
+    },
+    "dificil": {
+        nome: "Contar de 2 em 2",
+        imgCapa: "rd.png",
+        maxNum: 30,
+        passo: 2,
+        carruagens: 5
+    }
+};
