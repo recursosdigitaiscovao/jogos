be82ee790a7695a849a2477cdf0afb35a7b1d6fd
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-04-30 11:40:50 +0100

    Update categorias.js

--- 1/mat/teste3/categorias.js
+++ 1/mat/teste3/categorias.js
@@ -1,1 +1,21 @@
-
+// === DEFINIÇÃO DAS MISSÕES ESPACIAIS ===
+const JOGO_CATEGORIAS = {
+    "facil": {
+        nome: "Números até 10",
+        imgCapa: "rd.png", // Podes trocar por um ícone de foguetão
+        range: [1, 10],
+        quantidade: 4
+    },
+    "medio": {
+        nome: "Missão até 50",
+        imgCapa: "rd.png",
+        range: [1, 50],
+        quantidade: 4
+    },
+    "dificil": {
+        nome: "Exploração até 100",
+        imgCapa: "rd.png",
+        range: [1, 100],
+        quantidade: 4
+    }
+};
