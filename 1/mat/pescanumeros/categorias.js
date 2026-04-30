aa218771f41049f5985dc48f868efece86a55e5e
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-01 00:45:30 +0100

    Update categorias.js

--- 1/mat/pescanumeros/categorias.js
+++ 1/mat/pescanumeros/categorias.js
@@ -4,18 +4,18 @@ const JOGO_CATEGORIAS = {
         nome: "Mar Calmo (1-10)",
         imgCapa: "peixe01.png", 
         maxNum: 10,
-        velocidadeBase: 7 // Segundos para atravessar
+        velocidadeBase: 10 // Segundos para atravessar
     },
     "mar-profundo": {
         nome: "Mar Profundo (1-20)",
         imgCapa: "peixe03.png",
         maxNum: 20,
-        velocidadeBase: 5.5
+        velocidadeBase: 10
     },
     "oceano": {
         nome: "Oceano (1-50)",
         imgCapa: "peixe05.png",
         maxNum: 50,
-        velocidadeBase: 4.5
+        velocidadeBase: 10
     }
 };
