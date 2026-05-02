b32955a05db6adc93ec1145a68914d1a7aa5787f
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-02 21:41:10 +0100

    Update categorias.js

--- 1/mat/teste1/categorias.js
+++ 1/mat/teste1/categorias.js
@@ -2,28 +2,28 @@
 const JOGO_CATEGORIAS = {
     "Nível 1": {
         nome: "Números até 10",
-        imgCapa: "rd.png", // Usa o ícone padrão ou um comboio.png se tiveres
+        imgCapa: "seq_ate_10.png", // Usa o ícone padrão ou um comboio.png se tiveres
         maxNum: 10,
         passo: 1,
         carruagens: 4
     },
     "Nível 2": {
         nome: "Números até 20",
-        imgCapa: "rd.png",
+        imgCapa: "seq_ate_20.png",
         maxNum: 20,
         passo: 1,
         carruagens: 4
     },
     "Nível 3": {
         nome: "Números até 20",
-        imgCapa: "rd.png",
-        maxNum: 20,
+        imgCapa: "seq_ate_50.png",
+        maxNum: 50,
         passo: 1,
         carruagens: 4
     },
     "Nível 4": {
         nome: "Contar até 100",
-        imgCapa: "rd.png",
+        imgCapa: "seq_ate_100.png",
         maxNum: 100,
         passo: 1,
         carruagens: 4
