935979be87cd80a172f1c3c4abfb7a6a3c08907f
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-03 16:55:35 +0100

    Update categorias.js

--- 1/mat/conta/categorias.js
+++ 1/mat/conta/categorias.js
@@ -1,1 +1,27 @@
-
+// === DEFINIÇÃO DAS DIFICULDADES: CONTAR OBJETOS ===
+const JOGO_CATEGORIAS = {
+    "Nível 1": {
+        nome: "Contar até 5",
+        imgCapa: "rd.png", 
+        maxNum: 5,
+        itemImg: "item_contagem.png" // Nome da imagem do objeto na pasta img
+    },
+    "Nível 2": {
+        nome: "Contar até 10",
+        imgCapa: "rd.png",
+        maxNum: 10,
+        itemImg: "item_contagem.png"
+    },
+    "Nível 3": {
+        nome: "Contar até 15",
+        imgCapa: "rd.png",
+        maxNum: 15,
+        itemImg: "item_contagem.png"
+    },
+    "Nível 4": {
+        nome: "Contar até 20",
+        imgCapa: "rd.png",
+        maxNum: 20,
+        itemImg: "item_contagem.png"
+    }
+};
