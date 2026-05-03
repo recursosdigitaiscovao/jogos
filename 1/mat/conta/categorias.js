55d15cec1e194c42f4a65f66ece525f3c236b393
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-03 17:04:46 +0100

    Update categorias.js

--- 1/mat/conta/categorias.js
+++ 1/mat/conta/categorias.js
@@ -1,27 +1,31 @@
 // === DEFINIÇÃO DAS DIFICULDADES: CONTAR OBJETOS ===
 const JOGO_CATEGORIAS = {
     "Nível 1": {
-        nome: "Contar até 5",
+        nome: "Frutas até 5",
         imgCapa: "rd.png", 
         maxNum: 5,
-        itemImg: "item_contagem.png" // Nome da imagem do objeto na pasta img
+        pasta: "frutas/",
+        itens: ["amora.png", "ananas.png", "banana.png", "castanha.png", "cereja.png", "diospiro.png", "figo.png", "goiaba.png", "kiwi.png", "laranja.png", "limao.png", "maca.png", "maracuja.png", "melancia.png", "melao.png", "morango.png", "papaia.png", "pera.png", "roma.png", "uvas.png"]
     },
     "Nível 2": {
-        nome: "Contar até 10",
+        nome: "Frutas até 10",
         imgCapa: "rd.png",
         maxNum: 10,
-        itemImg: "item_contagem.png"
+        pasta: "frutas/",
+        itens: ["amora.png", "ananas.png", "banana.png", "castanha.png", "cereja.png", "diospiro.png", "figo.png", "goiaba.png", "kiwi.png", "laranja.png", "limao.png", "maca.png", "maracuja.png", "melancia.png", "melao.png", "morango.png", "papaia.png", "pera.png", "roma.png", "uvas.png"]
     },
     "Nível 3": {
-        nome: "Contar até 15",
+        nome: "Material Escolar até 15",
         imgCapa: "rd.png",
         maxNum: 15,
-        itemImg: "item_contagem.png"
+        pasta: "materialescolar/",
+        itens: ["afia.png", "borracha.png", "caderno.png", "caneta.png", "capa.png", "cola.png", "esquadro.png", "estojo.png", "folha.png", "lapis.png", "livro.png", "mochila.png", "pincel.png", "regua.png", "tesoura.png"]
     },
     "Nível 4": {
-        nome: "Contar até 20",
+        nome: "Material Escolar até 20",
         imgCapa: "rd.png",
         maxNum: 20,
-        itemImg: "item_contagem.png"
+        pasta: "materialescolar/",
+        itens: ["afia.png", "borracha.png", "caderno.png", "caneta.png", "capa.png", "cola.png", "esquadro.png", "estojo.png", "folha.png", "lapis.png", "livro.png", "mochila.png", "pincel.png", "regua.png", "tesoura.png"]
     }
 };
