206c78af5e56f8e60a782306e246d6b81db8e53f
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-03 17:10:08 +0100

    Update categorias.js

--- 1/mat/conta/categorias.js
+++ 1/mat/conta/categorias.js
@@ -1,28 +1,29 @@
 // === DEFINIÇÃO DAS DIFICULDADES: CONTAR OBJETOS ===
 const JOGO_CATEGORIAS = {
     "Nível 1": {
-        nome: "Frutas até 5",
+        nome: "Números até 5",
         imgCapa: "rd.png", 
         maxNum: 5,
-        pasta: "frutas/",
-        itens: ["amora.png", "ananas.png", "banana.png", "castanha.png", "cereja.png", "diospiro.png", "figo.png", "goiaba.png", "kiwi.png", "laranja.png", "limao.png", "maca.png", "maracuja.png", "melancia.png", "melao.png", "morango.png", "papaia.png", "pera.png", "roma.png", "uvas.png"]
+        pasta: "animais/",
+        // Ajusta estes nomes conforme os ficheiros que tens na tua pasta animais/
+        itens: ["cao.png", "gato.png", "leao.png", "girafa.png", "macaco.png", "pato.png", "peixe.png", "vaca.png", "ovelha.png", "porco.png"]
     },
     "Nível 2": {
-        nome: "Frutas até 10",
+        nome: "Números até 10",
         imgCapa: "rd.png",
         maxNum: 10,
+        pasta: "animais/",
+        itens: ["cao.png", "gato.png", "leao.png", "girafa.png", "macaco.png", "pato.png", "peixe.png", "vaca.png", "ovelha.png", "porco.png"]
+    },
+    "Nível 3": {
+        nome: "Números até 15",
+        imgCapa: "rd.png",
+        maxNum: 15,
         pasta: "frutas/",
         itens: ["amora.png", "ananas.png", "banana.png", "castanha.png", "cereja.png", "diospiro.png", "figo.png", "goiaba.png", "kiwi.png", "laranja.png", "limao.png", "maca.png", "maracuja.png", "melancia.png", "melao.png", "morango.png", "papaia.png", "pera.png", "roma.png", "uvas.png"]
     },
-    "Nível 3": {
-        nome: "Material Escolar até 15",
-        imgCapa: "rd.png",
-        maxNum: 15,
-        pasta: "materialescolar/",
-        itens: ["afia.png", "borracha.png", "caderno.png", "caneta.png", "capa.png", "cola.png", "esquadro.png", "estojo.png", "folha.png", "lapis.png", "livro.png", "mochila.png", "pincel.png", "regua.png", "tesoura.png"]
-    },
     "Nível 4": {
-        nome: "Material Escolar até 20",
+        nome: "Números até 20",
         imgCapa: "rd.png",
         maxNum: 20,
         pasta: "materialescolar/",
