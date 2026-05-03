f5c89963973f8f105f0e42f5e3ad123b929d1eca
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-03 17:37:52 +0100

    Update categorias.js

--- 1/mat/conta/categorias.js
+++ 1/mat/conta/categorias.js
@@ -2,29 +2,29 @@
 const JOGO_CATEGORIAS = {
     "Nível 1": {
         nome: "Números até 5",
-        imgCapa: "rd.png", 
+        imgCapa: "cnivel1.png", 
         maxNum: 5,
-        pasta: "animais/",
+        pasta: "animaisselvagens/",
         // Ajusta estes nomes conforme os ficheiros que tens na tua pasta animais/
         itens: ["cao.png", "gato.png", "leao.png", "girafa.png", "macaco.png", "pato.png", "peixe.png", "vaca.png", "ovelha.png", "porco.png"]
     },
     "Nível 2": {
         nome: "Números até 10",
-        imgCapa: "rd.png",
+        imgCapa: "cnivel2.png",
         maxNum: 10,
-        pasta: "animais/",
+        pasta: "animaisselvagens/",
         itens: ["cao.png", "gato.png", "leao.png", "girafa.png", "macaco.png", "pato.png", "peixe.png", "vaca.png", "ovelha.png", "porco.png"]
     },
     "Nível 3": {
         nome: "Números até 15",
-        imgCapa: "rd.png",
+        imgCapa: "cnivel3.png",
         maxNum: 15,
         pasta: "frutas/",
         itens: ["amora.png", "ananas.png", "banana.png", "castanha.png", "cereja.png", "diospiro.png", "figo.png", "goiaba.png", "kiwi.png", "laranja.png", "limao.png", "maca.png", "maracuja.png", "melancia.png", "melao.png", "morango.png", "papaia.png", "pera.png", "roma.png", "uvas.png"]
     },
     "Nível 4": {
         nome: "Números até 20",
-        imgCapa: "rd.png",
+        imgCapa: "cnivel4.png",
         maxNum: 20,
         pasta: "materialescolar/",
         itens: ["afia.png", "borracha.png", "caderno.png", "caneta.png", "capa.png", "cola.png", "esquadro.png", "estojo.png", "folha.png", "lapis.png", "livro.png", "mochila.png", "pincel.png", "regua.png", "tesoura.png"]
