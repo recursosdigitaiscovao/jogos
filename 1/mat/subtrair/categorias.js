08de84ecbf420794f6809d0fc0a407a2e7535a42
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-04 19:08:41 +0100

    Update categorias.js

--- 1/mat/subtrair/categorias.js
+++ 1/mat/subtrair/categorias.js
@@ -1,1 +1,27 @@
-
+// === DEFINIÇÃO DAS DIFICULDADES: SUBTRAÇÕES ===
+const JOGO_CATEGORIAS = {
+    "Nível 1": {
+        nome: "Subtrair até 10",
+        imgCapa: "cnivel1.png", 
+        maxNum: 10,
+        mostrarAjuda: true
+    },
+    "Nível 2": {
+        nome: "Subtrair até 20",
+        imgCapa: "cnivel2.png",
+        maxNum: 20,
+        mostrarAjuda: true
+    },
+    "Nível 3": {
+        nome: "Subtrair até 50",
+        imgCapa: "cnivel3.png",
+        maxNum: 50,
+        mostrarAjuda: false // Números grandes demais para bolinhas
+    },
+    "Nível 4": {
+        nome: "Subtrair até 100",
+        imgCapa: "cnivel4.png",
+        maxNum: 100,
+        mostrarAjuda: false
+    }
+};
