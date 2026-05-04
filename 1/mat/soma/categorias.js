19dbb5a1791d5c3b7a8372600b622c02ce3f549d
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-04 18:37:58 +0100

    Update categorias.js

--- 1/mat/soma/categorias.js
+++ 1/mat/soma/categorias.js
@@ -1,1 +1,23 @@
-
+// === DEFINIÇÃO DAS DIFICULDADES: SOMAS ===
+const JOGO_CATEGORIAS = {
+    "Nível 1": {
+        nome: "Somas até 10",
+        imgCapa: "cnivel1.png", 
+        maxResultado: 10
+    },
+    "Nível 2": {
+        nome: "Somas até 20",
+        imgCapa: "cnivel2.png",
+        maxResultado: 20
+    },
+    "Nível 3": {
+        nome: "Somas até 50",
+        imgCapa: "cnivel3.png",
+        maxResultado: 50
+    },
+    "Nível 4": {
+        nome: "Somas até 100",
+        imgCapa: "cnivel4.png",
+        maxResultado: 100
+    }
+};
