d8c1ab401baea97050255c80291a2e85d55b358b
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-04 19:06:37 +0100

    Update dados.js

--- 1/mat/subtrair/dados.js
+++ 1/mat/subtrair/dados.js
@@ -2,7 +2,7 @@
 const CONFIG_MESTRE = {
     ano: "ano1",        
     area: "matematica",  
-    nomeJogo: "Somas"
+    nomeJogo: "Subtrações"
 };
 
 // === BIBLIOTECA DE TEMAS (CORES PADRÃO POR ÁREA) ===
