eca1624368710fdf9d4c787bfa332043f067dfe6
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-06 14:21:16 +0100

    Update categorias.js

--- 1/mat/compara/categorias.js
+++ 1/mat/compara/categorias.js
@@ -1,1 +1,17 @@
-
+// === DEFINIÇÃO DAS DIFICULDADES: COMPARAR FRUTAS ===
+const JOGO_CATEGORIAS = {
+    "Nível 1": {
+        nome: "Comparar até 10",
+        imgCapa: "cnivel1.png", 
+        maxNum: 10,
+        pasta: "frutas/",
+        itens: ["amora.png", "ananas.png", "banana.png", "castanha.png", "cereja.png", "diospiro.png", "figo.png", "goiaba.png", "kiwi.png", "laranja.png", "limao.png", "maca.png", "maracuja.png", "melancia.png", "melao.png", "morango.png", "papaia.png", "pera.png", "roma.png", "uvas.png"]
+    },
+    "Nível 2": {
+        nome: "Comparar até 20",
+        imgCapa: "cnivel2.png",
+        maxNum: 20,
+        pasta: "frutas/",
+        itens: ["amora.png", "ananas.png", "banana.png", "castanha.png", "cereja.png", "diospiro.png", "figo.png", "goiaba.png", "kiwi.png", "laranja.png", "limao.png", "maca.png", "maracuja.png", "melancia.png", "melao.png", "morango.png", "papaia.png", "pera.png", "roma.png", "uvas.png"]
+    }
+};
