727a38ed95bd7ec53770ea0a98f43c5a61689958
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-06 07:56:33 +0100

    Update categorias.js

--- 1/mat/liga/categorias.js
+++ 1/mat/liga/categorias.js
@@ -1,1 +1,27 @@
-
+// === DEFINIÇÃO DAS DIFICULDADES: LIGAR NÚMEROS ===
+const JOGO_CATEGORIAS = {
+    "Nível 1": {
+        nome: "Números de 1 a 10",
+        imgCapa: "cnivel1.png", 
+        min: 1, max: 10,
+        paresPorRonda: 4
+    },
+    "Nível 2": {
+        nome: "Números de 10 a 20",
+        imgCapa: "cnivel2.png",
+        min: 10, max: 20,
+        paresPorRonda: 4
+    },
+    "Nível 3": {
+        nome: "Números de 20 a 50",
+        imgCapa: "cnivel3.png",
+        min: 20, max: 50,
+        paresPorRonda: 5
+    },
+    "Nível 4": {
+        nome: "Números de 50 a 100",
+        imgCapa: "cnivel4.png",
+        min: 50, max: 100,
+        paresPorRonda: 5
+    }
+};
