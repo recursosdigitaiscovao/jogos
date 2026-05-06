a6d7dcc65908a99cd56faf2244d9a4c7c0fff2e6
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-07 00:51:54 +0100

    Update categorias.js

--- 1/mat/compara/categorias.js
+++ 1/mat/compara/categorias.js
@@ -1,17 +1,18 @@
-// === DEFINIÇÃO DAS DIFICULDADES: COMPARAR FRUTAS ===
+// === DEFINIÇÃO DAS DIFICULDADES: COMPARAR ANIMAIS ===
 const JOGO_CATEGORIAS = {
     "Nível 1": {
         nome: "Comparar até 10",
         imgCapa: "cnivel1.png", 
         maxNum: 10,
-        pasta: "frutas/",
-        itens: ["amora.png", "ananas.png", "banana.png", "castanha.png", "cereja.png", "diospiro.png", "figo.png", "goiaba.png", "kiwi.png", "laranja.png", "limao.png", "maca.png", "maracuja.png", "melancia.png", "melao.png", "morango.png", "papaia.png", "pera.png", "roma.png", "uvas.png"]
+        pasta: "animaisdomesticos/",
+        // Lista de ficheiros na tua pasta animaisdomesticos/
+        itens: ["cao.png", "gato.png", "vaca.png", "porco.png", "galinha.png", "ovelha.png", "pato.png", "cavalo.png", "coelho.png", "peru.png"]
     },
     "Nível 2": {
         nome: "Comparar até 20",
         imgCapa: "cnivel2.png",
         maxNum: 20,
-        pasta: "frutas/",
-        itens: ["amora.png", "ananas.png", "banana.png", "castanha.png", "cereja.png", "diospiro.png", "figo.png", "goiaba.png", "kiwi.png", "laranja.png", "limao.png", "maca.png", "maracuja.png", "melancia.png", "melao.png", "morango.png", "papaia.png", "pera.png", "roma.png", "uvas.png"]
+        pasta: "animaisdomesticos/",
+        itens: ["cao.png", "gato.png", "vaca.png", "porco.png", "galinha.png", "ovelha.png", "pato.png", "cavalo.png", "coelho.png", "peru.png"]
     }
 };
