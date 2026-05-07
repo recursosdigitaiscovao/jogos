a61dc6549d56f2795da00c05b20b70d2f23073c6
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-07 17:28:29 +0100

    Update categorias.js

--- 1/pt/completapalavras/categorias.js
+++ 1/pt/completapalavras/categorias.js
@@ -1,1 +1,47 @@
-
+// === DEFINIÇÃO DAS CATEGORIAS: COMPLETAR PALAVRAS ===
+const JOGO_CATEGORIAS = {
+    "Nível 1": {
+        nome: "Animais Simples",
+        imgCapa: "cnivel1.png",
+        pasta: "animaisdomesticos/",
+        palavras: [
+            { termo: "PATO", img: "pato.png" },
+            { termo: "VACA", img: "vaca.png" },
+            { termo: "GATO", img: "gato.png" },
+            { termo: "CÃO", img: "cao.png" }
+        ]
+    },
+    "Nível 2": {
+        nome: "Animais da Quinta",
+        imgCapa: "cnivel2.png",
+        pasta: "animaisdomesticos/",
+        palavras: [
+            { termo: "PORCO", img: "porco.png" },
+            { termo: "OVELHA", img: "ovelha.png" },
+            { termo: "CAVALO", img: "cavalo.png" },
+            { termo: "COELHO", img: "coelho.png" }
+        ]
+    },
+    "Nível 3": {
+        nome: "Material Escolar",
+        imgCapa: "cnivel3.png",
+        pasta: "materialescolar/",
+        palavras: [
+            { termo: "LIVRO", img: "livro.png" },
+            { termo: "COLA", img: "cola.png" },
+            { termo: "AFIA", img: "afia.png" },
+            { termo: "LAPIS", img: "lapis.png" }
+        ]
+    },
+    "Nível 4": {
+        nome: "Frutas",
+        imgCapa: "cnivel4.png",
+        pasta: "frutas/",
+        palavras: [
+            { termo: "MACA", img: "maca.png" },
+            { termo: "PERA", img: "pera.png" },
+            { termo: "BANANA", img: "banana.png" },
+            { termo: "UVA", img: "uvas.png" }
+        ]
+    }
+};
