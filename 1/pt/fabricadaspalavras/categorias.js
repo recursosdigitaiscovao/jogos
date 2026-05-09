cfcd672f1a678631d1150d2fe59f762f101ea603
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-09 21:05:09 +0100

    Update categorias.js

--- 1/pt/fabricadaspalavras/categorias.js
+++ 1/pt/fabricadaspalavras/categorias.js
@@ -1,38 +1,16 @@
-// === DICIONÁRIO MESTRE DA FÁBRICA (PT-PT) ===
 const DICIONARIO_GLOBAL = {
-    // Palavras reais de 2 sílabas (Estrutura CV-CV)
-    dissilabos: [
-        "BOLA", "BOLO", "BOTA", "BOCA", "BODE", "CASA", "CAMA", "CADA", "CABO", "COLA", 
-        "COPA", "COMA", "DADO", "DATA", "DAMA", "DEDO", "DOCE", "FADA", "FACA", "FAMA", 
-        "FOGO", "FOTO", "GATO", "GALA", "GALO", "GOLA", "GOTA", "LAMA", "LATA", "LADO", 
-        "LOBO", "LUAR", "MALA", "MANA", "MANO", "MAPA", "MATA", "MATO", "MEDO", "MESA", 
-        "MIMO", "MOTA", "PATA", "PATO", "PAPA", "PARA", "PERA", "PENA", "PICO", "PIPA", 
-        "RALA", "RATA", "RATO", "RODA", "RODO", "ROCA", "ROMA", "ROSA", "ROTA", "SACA", 
-        "SACO", "SALA", "SAPO", "SETA", "SELO", "SOPA", "SORO", "TACA", "TACO", "TALA", 
-        "TALO", "TAMA", "TAPA", "TARA", "TATO", "TOCA", "TODO", "TODA", "TOMA", "VACA", 
-        "VALA", "VAGA", "VELA", "VILA", "VOTO"
-    ],
-    // Palavras reais de 3 sílabas simples
-    trissilabos: [
-        "MACACO", "BATATA", "BANANA", "JANELA", "PANELA", "CANELA", "CANETA", "CAJADO", 
-        "CAMADA", "COMIDA", "COMETA", "CORADA", "BALADA", "CAVALO", "BONECA", "PIPOCA", 
-        "SALADA", "SAPATO", "SACADA", "PELADA", "PETECA", "AMADO", "APARADA", "FALADA", 
-        "MAMADA", "MANADA", "PIRATA", "RECADA", "TAMPADA", "TAPADA", "GELADO", "COCOLO"
-    ]
+    dissilabos: ["BOLA","BOLO","BOTA","BOCA","BODE","CASA","CAMA","CADA","CABO","COLA","COPA","COMA","DADO","DATA","DAMA","DEDO","DOCE","FADA","FACA","FAMA","FOGO","FOTO","GATO","GALA","GALO","GOLA","GOTA","LAMA","LATA","LADO","LOBO","LUAR","MALA","MANA","MANO","MAPA","MATA","MATO","MEDO","MESA","MIMO","MOTA","PATA","PATO","PAPA","PARA","PERA","PENA","PICO","PIPA","RALA","RATA","RATO","RODA","RODO","ROCA","ROMA","ROSA","ROTA","SACA","SACO","SALA","SAPO","SETA","SELO","SOPA","SORO","TACA","TACO","TALA","TALO","TAMA","TAPA","TAPO","TARA","TATO","TOCA","TODO","TODA","TOMA","VACA","VALA","VAGA","VELA","VILA","VOTO"],
+    trissilabos: ["MACACO","BATATA","BANANA","JANELA","PANELA","CANELA","CANETA","CAJADO","CAMADA","COMIDA","COMETA","CORADA","BALADA","CAVALO","BONECA","PIPOCA","SALADA","SAPATO","SACADA","PELADA","PETECA","AMADO","APARADA","FALADA","MAMADA","MANADA","PIRATA","RECADA","TAMPADA","TAPADA","GELADO"]
 };
 
 const JOGO_CATEGORIAS = {
     "Nível 1": {
-        nome: "Fábrica: 2 Sílabas",
-        slots: 2,
-        target: 10,
+        nome: "Fábrica: 2 Sílabas", slots: 2, target: 10,
         pool: DICIONARIO_GLOBAL.dissilabos,
         extras: ["BA","CA","DA","LA","MA","PA","RA","SA","TA","VA","BO","CO","DO","FO","GO","MO","NO","PO","RO","SO","TO"]
     },
     "Nível 2": {
-        nome: "Fábrica: 3 Sílabas",
-        slots: 3,
-        target: 10,
+        nome: "Fábrica: 3 Sílabas", slots: 3, target: 10,
         pool: DICIONARIO_GLOBAL.trissilabos,
         extras: ["BA","CA","DA","LA","MA","PA","RA","TA","VA","NE","JA","MI","CO","PI","PO","RE","TE","DE","BO","LI"]
     }
