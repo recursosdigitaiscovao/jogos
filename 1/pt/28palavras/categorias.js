576a5dba5f32e8b9f8f129eab240662ed774f430
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-09 22:40:52 +0100

    Update categorias.js

--- 1/pt/28palavras/categorias.js
+++ 1/pt/28palavras/categorias.js
@@ -1,30 +1,30 @@
 // === MÉTODO DAS 28 PALAVRAS: ESTRUTURA DE DESAFIOS ===
 const DESAFIOS_FABRICA = {
     nivel1: [
-        // Cada desafio foca numa palavra-mestre do método e nas suas derivações
-        { bank: ["ME", "NI", "NA", "NO"], slots: 3 },       // Palavras-Mestre: MENINA, MENINO (Derivadas: NINA, NINO, MANA, NONO)
-        { bank: ["SA", "PA", "TO", "GA"], slots: 3 },       // Palavra-Mestre: SAPATO (Derivadas: SAPO, PATO, TAPA, GATO, TOGA)
-        { bank: ["BO", "NE", "CA", "MA"], slots: 3 },       // Palavra-Mestre: BONECA (Derivadas: BOCA, CANECA, BONÉ, CAMA, MAMA)
-        { bank: ["MA", "LA", "VE", "RA"], slots: 2 },       // Palavra-Mestre: MALA, VELA (Derivadas: LAMA, VALA, RALA, MAMA, VERA)
-        { bank: ["DA", "DO", "RE", "DE"], slots: 2 },       // Palavra-Mestre: DADO, REDE (Derivadas: DEDO, DADA, DEDÉ, RODO)
-        { bank: ["PEI", "XE", "E", "RE"], slots: 2 },        // Palavra-Mestre: PEIXE (Derivadas: PEPE, REI, EIXO)
-        { bank: ["CO", "E", "LHO", "LA"], slots: 3 },       // Palavra-Mestre: COELHO (Derivadas: COLA, HELO, ECHO)
-        { bank: ["CA", "VA", "LO", "TA"], slots: 3 },       // Palavra-Mestre: CAVALO (Derivadas: COLA, VALA, TALA, CALA)
-        { bank: ["JA", "NE", "LA", "PA"], slots: 3 },       // Palavra-Mestre: JANELA (Derivadas: PANELA, NELA, PALA, JACA)
-        { bank: ["PI", "PO", "CA", "DO"], slots: 3 }        // Palavra-Mestre: PIPOCA (Derivadas: PICO, COPA, DOCA, DADO)
+        // Sílabas baralhadas nos "bank" para incentivar a leitura
+        { bank: ["NO", "NA", "ME", "NI"], slots: 3 },       // MENINA, MENINO
+        { bank: ["TO", "GA", "PA", "SA"], slots: 3 },       // SAPATO, SAPO, PATO
+        { bank: ["CA", "MA", "BO", "NE"], slots: 3 },       // BONECA, BOCA, CAMA
+        { bank: ["RA", "VE", "LA", "MA"], slots: 2 },       // MALA, VELA, LAMA
+        { bank: ["DE", "RE", "DO", "DA"], slots: 2 },       // DADO, REDE, DEDO
+        { bank: ["E", "RE", "XE", "PEI"], slots: 2 },       // PEIXE, REI
+        { bank: ["LHO", "LA", "E", "CO"], slots: 3 },       // COELHO, COLA
+        { bank: ["LO", "TA", "VA", "CA"], slots: 3 },       // CAVALO, COLA, VALA
+        { bank: ["LA", "PA", "JA", "NE"], slots: 3 },       // JANELA, PANELA
+        { bank: ["CA", "DO", "PO", "PI"], slots: 3 }        // PIPOCA, PICO, DOCA
     ],
     nivel2: [
-        // Trissílabos e combinações mais complexas das 28 palavras
-        { bank: ["GI", "RA", "FA", "TA", "PA"], slots: 3 }, // Palavra-Mestre: GIRAFA (Derivadas: FATA, RATA, PARA, PAGA)
-        { bank: ["GA", "LI", "NHA", "BA", "TA"], slots: 3 },// Palavra-Mestre: GALINHA (Derivadas: GALA, BANHA, BATA, LINHA)
-        { bank: ["CRO", "CO", "DI", "LO", "MA"], slots: 4 },// Palavra-Mestre: CROCODILO (Derivadas: COLO, LAMA, DILO)
-        { bank: ["ES", "TO", "JO", "BO", "CA"], slots: 3 }, // Palavra-Mestre: ESTOJO (Derivadas: BOCA, TOCA, BOJO)
-        { bank: ["MO", "CHI", "LA", "MA", "TA"], slots: 3 },// Palavra-Mestre: MOCHILA (Derivadas: MALA, MATA, MOLA, CHILA)
-        { bank: ["TE", "SOU", "RA", "RO", "SA"], slots: 3 },// Palavra-Mestre: TESOURA (Derivadas: RATA, ROSA, ROUPA, SOU)
-        { bank: ["FO", "GUE", "TE", "GA", "TA"], slots: 3 },// Palavra-Mestre: FOGUETE (Derivadas: FOGO, GATA, GUETO, TETA)
-        { bank: ["BA", "LEI", "A", "MA", "RE"], slots: 3 }, // Palavra-Mestre: BALEIA (Derivadas: BALA, LEIA, MARE, MEIA)
-        { bank: ["LI", "VRO", "CA", "SA", "DA"], slots: 2 },// Palavra-Mestre: LIVRO (Derivadas: CASA, CADA, SALA, LIRA)
-        { bank: ["CA", "DER", "NO", "MA", "PA"], slots: 3 } // Palavra-Mestre: CADERNO (Derivadas: CANO, MAPA, NADA, DANO)
+        // Trissílabos e combinações mais complexas
+        { bank: ["FA", "TA", "GI", "PA", "RA"], slots: 3 }, // GIRAFA
+        { bank: ["NHA", "BA", "GA", "TA", "LI"], slots: 3 },// GALINHA
+        { bank: ["DI", "MA", "CRO", "LO", "CO"], slots: 4 },// CROCODILO
+        { bank: ["JO", "BO", "ES", "CA", "TO"], slots: 3 }, // ESTOJO
+        { bank: ["LA", "MA", "MO", "TA", "CHI"], slots: 3 },// MOCHILA
+        { bank: ["RA", "RO", "TE", "SA", "SOU"], slots: 3 },// TESOURA
+        { bank: ["TE", "GA", "FO", "TA", "GUE"], slots: 3 },// FOGUETE
+        { bank: ["A", "MA", "BA", "RE", "LEI"], slots: 3 }, // BALEIA
+        { bank: ["SA", "DA", "LI", "VRO", "CA"], slots: 2 },// LIVRO
+        { bank: ["NO", "MA", "CA", "PA", "DER"], slots: 3 } // CADERNO
     ]
 };
 
@@ -41,20 +41,20 @@ const JOGO_CATEGORIAS = {
     }
 };
 
-// DICIONÁRIO MESTRE (PT-PT) baseado nas 28 palavras e suas combinações
+// DICIONÁRIO MESTRE (PT-PT)
 const DICIONARIO_MESTRE = [
     // Palavras-Mestre
     "MENINA", "MENINO", "SAPATO", "GATO", "BONECA", "MALA", "VELA", "DADO", "REDE", "PEIXE", 
     "COELHO", "CAVALO", "JANELA", "PANELA", "PIPOCA", "GIRAFA", "GALINHA", "CROCODILO", 
     "ESTOJO", "MOCHILA", "TESOURA", "FOGUETE", "BALEIA", "LIVRO", "CADERNO", "ESCOLA",
 
-    // Derivadas do Nível 1 (2 e 3 sílabas)
+    // Derivadas do Nível 1
     "NINA", "NINO", "MANA", "MANO", "NONO", "SAPO", "PATO", "TAPA", "TOGA", "GAGO", 
     "BOCA", "CANECA", "CABO", "CAMA", "MAMA", "LAMA", "VALA", "RALA", "VERA", "DEDO", 
     "DADA", "RODO", "REI", "EIXO", "COLA", "HELO", "TALA", "CALA", "NELA", "PALA", 
     "JACA", "PICO", "COPA", "DOCA", "BOTA", "BOLO", "BALA", "BATA", "RATO", "RODA",
 
-    // Derivadas do Nível 2 (Combinações mais ricas)
+    // Derivadas do Nível 2
     "FATA", "RATA", "PARA", "PAGA", "GALA", "BANHA", "LINHA", "LAMA", "DILO", "TOCA", 
     "BOJO", "MATA", "MOLA", "CHILA", "ROSA", "ROUPA", "FOGO", "GUETO", "TETA", "LEIA", 
     "MARE", "MEIA", "CASA", "CADA", "SALA", "LIRA", "CANO", "MAPA", "NADA", "DANO"
