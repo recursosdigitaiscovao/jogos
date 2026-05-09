53f96d60182b0526e307bddb42f6f0752b0188fa
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-09 09:57:04 +0100

    Update categorias.js

--- 1/pt/fabricadaspalavras/categorias.js
+++ 1/pt/fabricadaspalavras/categorias.js
@@ -1,1 +1,33 @@
-
+// === DEFINIÇÃO DAS CATEGORIAS: COMPLETAR SÍLABAS ===
+const JOGO_CATEGORIAS = {
+    "Nível 1": {
+        nome: "2 Sílabas",
+        imgCapa: "cnivel1.png",
+        palavras: [
+            { silabas: ["BA", "LA"] },
+            { silabas: ["CA", "SA"] },
+            { silabas: ["DO", "CE"] },
+            { silabas: ["FO", "CO"] },
+            { silabas: ["GA", "TO"] },
+            { silabas: ["LA", "TA"] },
+            { silabas: ["MA", "LA"] },
+            { silabas: ["PA", "TO"] },
+            { silabas: ["RA", "TO"] },
+            { silabas: ["SA", "PO"] }
+        ]
+    },
+    "Nível 2": {
+        nome: "3 Sílabas",
+        imgCapa: "cnivel2.png",
+        palavras: [
+            { silabas: ["BO", "NE", "CA"] },
+            { silabas: ["CA", "VA", "LO"] },
+            { silabas: ["CA", "DER", "NO"] },
+            { silabas: ["JA", "NE", "LA"] },
+            { silabas: ["MA", "CA", "CO"] },
+            { silabas: ["PI", "PO", "CA"] },
+            { silabas: ["SA", "PA", "TO"] },
+            { silabas: ["TA", "PE", "TE"] }
+        ]
+    }
+};
