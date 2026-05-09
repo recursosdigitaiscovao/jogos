dabd457fa27638253fe4d4945cf2f49051b03dc3
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-09 12:12:21 +0100

    Update categorias.js

--- 1/pt/fabricadaspalavras/categorias.js
+++ 1/pt/fabricadaspalavras/categorias.js
@@ -1,24 +1,17 @@
 // === DEFINIÇÃO DAS CATEGORIAS: A FÁBRICA DE PALAVRAS ===
 const JOGO_CATEGORIAS = {
     "Nível 1": {
-        nome: "Estação de 2 Sílabas",
-        imgCapa: "cnivel1.png",
-        target: 5, // Meta de palavras para vencer
-        bank: ["GA", "TO", "LO", "BO", "CA", "MA", "VA", "PA", "LA", "ME"],
-        valid: ["GATO", "LOBO", "GALO", "BOLO", "VACA", "PAPA", "PATO", "CAMA", "BOCA", "MALA", "MACA", "LAMA", "BOTA", "VALA", "VAGA", "MESA"]
+        nome: "Estação: 2 Peças",
+        slots: 2, // Define que o aluno deve usar 2 sílabas
+        target: 8, 
+        bank: ["BA", "CA", "DA", "GA", "LA", "MA", "PA", "RA", "SA", "TA", "TO", "BO", "LO", "DE", "ME"],
+        valid: ["BALA", "BOLO", "BOTA", "BOCA", "CAMA", "CASA", "CADA", "CABO", "COLA", "DADO", "DATA", "DAMA", "DEDO", "DOCE", "GATO", "GALA", "GALO", "LAMA", "LATA", "LADO", "LOBO", "MALA", "MATA", "MATO", "MEDO", "MESA", "MOTA", "PATA", "PATO", "PAPA", "PARA", "PERA", "PENA", "RALA", "RATA", "RATO", "RODA", "RODO", "ROSA", "SACA", "SALA", "SAPO", "SETA", "SELO", "SOPA", "TACA", "TALA", "TATO", "TELA", "TOGA", "TOMA"]
     },
     "Nível 2": {
-        nome: "Estação Mista",
-        imgCapa: "cnivel2.png",
-        target: 5,
-        bank: ["PI", "CO", "NE", "LA", "JA", "SA", "BA", "TA", "RE", "CA", "PA"],
-        valid: ["PIPA", "COLA", "NELA", "SALA", "BALA", "TELA", "RETA", "PICO", "COPO", "BATA", "SACO", "BICO", "JANELA", "PANELA", "CANETA", "SAPATO", "BONECA", "CANECA", "PATA"]
-    },
-    "Nível 3": {
-        nome: "Engenharia de Palavras",
-        imgCapa: "cnivel3.png",
+        nome: "Estação: 3 Peças",
+        slots: 3, // Define que o aluno deve usar 3 sílabas
         target: 6,
-        bank: ["DE", "DO", "FA", "DA", "ME", "SA", "RA", "TO", "LI", "XO", "PE", "TE"],
-        valid: ["DEDO", "FADA", "MESA", "RATO", "LIXO", "DADO", "MEDO", "FAMA", "RETA", "DATA", "RETE", "TELA", "TAPETE", "SAPATO", "DOCE"]
+        bank: ["PI", "PO", "CA", "NE", "LA", "JA", "SA", "BA", "TA", "RE", "CO", "MI", "DA", "MA", "PA"],
+        valid: ["PIPOCA", "PANELA", "PACATA", "CANETA", "CANELA", "CAJADO", "CAMADA", "COMIDA", "COMETA", "CORADA", "JANELA", "SACOLA", "SALADA", "SAPATO", "SACADA", "BATATA", "BANANA", "BALADA"]
     }
 };
