08a18f320090e388aa399fb9020a657fee692ac2
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-10 16:22:07 +0100

    Update categorias.js

--- 1/pt/antonimos/categorias.js
+++ 1/pt/antonimos/categorias.js
@@ -1,1 +1,16 @@
-
+const JOGO_CATEGORIAS = {
+    "antonimos_1": {
+        nome: "Antónimos Divertidos",
+        imgCapa: "capa_antonimos.png",
+        pares: [
+            { a: "Alto", b: "Baixo" },
+            { a: "Quente", b: "Frio" },
+            { a: "Aberto", b: "Fechado" },
+            { a: "Grande", b: "Pequeno" },
+            { a: "Alegre", b: "Triste" },
+            { a: "Dia", b: "Noite" },
+            { a: "Limpo", b: "Sujo" },
+            { a: "Dentro", b: "Fora" }
+        ]
+    }
+};
