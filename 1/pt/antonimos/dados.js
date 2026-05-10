71c75cce94a498931797409577b86c0b8a77d0f6
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-10 18:54:37 +0100

    Update dados.js

--- 1/pt/antonimos/dados.js
+++ 1/pt/antonimos/dados.js
@@ -2,7 +2,7 @@
 const CONFIG_MESTRE = {
     ano: "ano1",        
     area: "portugues",  
-    nomeJogo: "Fábrica das Palavras"
+    nomeJogo: "Antónimos"
 };
 
 // === BIBLIOTECA DE TEMAS (CORES PADRÃO POR ÁREA) ===
