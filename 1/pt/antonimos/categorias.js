fb257223becb3b7aa101639634339c2f0a846b5e
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-10 18:08:11 +0100

    Update categorias.js

--- 1/pt/antonimos/categorias.js
+++ 1/pt/antonimos/categorias.js
@@ -30,16 +30,18 @@ const JOGO_CATEGORIAS = {
     "Nível 1": { 
         nome: "Puzzle: 2 Sílabas", 
         target: 10, 
-        desafios: DESAFIOS_FABRICA.nivel1 
+        desafios: DESAFIOS_FABRICA.nivel1,
+        imgCapa: "cnivel1.png" // Ícone para o Nível 1
     },
     "Nível 2": { 
         nome: "Puzzle: 3 Sílabas", 
         target: 10, 
-        desafios: DESAFIOS_FABRICA.nivel2 
+        desafios: DESAFIOS_FABRICA.nivel2,
+        imgCapa: "cnivel2.png" // Ícone para o Nível 2
     }
 };
 
-// Dicionário de validação PT-PT (Focado nos teus exemplos e variações possíveis)
+// Dicionário de validação PT-PT
 const DICIONARIO_MESTRE = [
     // --- Nível 1 ---
     "PATA", "SOPA", "SOMA", "MAPA", "MATA", "TAPA",
