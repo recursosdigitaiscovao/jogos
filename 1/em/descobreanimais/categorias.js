43ad2981355b9f64c08e0764ebc8f10c7a119bf2
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-14 13:28:34 +0100

    Update categorias.js

--- 1/em/descobreanimais/categorias.js
+++ 1/em/descobreanimais/categorias.js
@@ -3,26 +3,24 @@ const JOGO_CATEGORIAS = {
         nome: "Nível 1",
         imgCapa: "cnivel1.png",
         titulo: "Explorador Iniciante",
-        descricao: "Encontra os animais domésticos na quinta!",
+        descricao: "Usa a lanterna para encontrar os animais domésticos escondidos no escuro!",
         totalRondas: 10,
-        // Configuração específica do conteúdo
         pastaAlvos: "animaisdomesticos/",
         pastaDistracoes: "animaisselvagens/",
         alvos: ['cao.png', 'gato.png', 'vaca.png', 'porco.png', 'ovelha.png', 'galinha.png', 'cavalo.png', 'cabra.png'],
         distracoes: ['leao.png', 'tigre.png', 'elefante.png', 'girafa.png', 'lobo.png', 'raposa.png'],
-        quantidadeDistracoes: 4 // Menos animais para ser mais fácil
+        quantidadeDistracoes: 5 
     },
     "nivel2": {
         nome: "Nível 2",
         imgCapa: "cnivel2.png",
         titulo: "Mestre da Selva",
-        descricao: "Cuidado! A selva está muito concorrida!",
+        descricao: "Cuidado! Agora há muito mais animais selvagens para te confundir!",
         totalRondas: 10,
-        // Configuração específica do conteúdo
         pastaAlvos: "animaisdomesticos/",
         pastaDistracoes: "animaisselvagens/",
         alvos: ['cao.png', 'gato.png', 'vaca.png', 'porco.png', 'ovelha.png', 'galinha.png', 'cavalo.png', 'cabra.png'],
         distracoes: ['leao.png', 'tigre.png', 'elefante.png', 'girafa.png', 'lobo.png', 'raposa.png', 'cobra.png', 'jacare.png', 'urso.png', 'zebra.png'],
-        quantidadeDistracoes: 10 // Mais distrações para dificultar
+        quantidadeDistracoes: 12 
     }
 };
