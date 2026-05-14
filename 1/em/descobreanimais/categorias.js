49bf5ea5eb7cf58d16713b4a22fc9b9508ae55e3
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-14 14:09:46 +0100

    Update categorias.js

--- 1/em/descobreanimais/categorias.js
+++ 1/em/descobreanimais/categorias.js
@@ -8,9 +8,24 @@ const JOGO_CATEGORIAS = {
         tipoAlvo: "domestico",
         pastaAlvos: "animaisdomesticos/",
         pastaDistracoes: "animaisselvagens/",
-        alvos: ['cao.png', 'gato.png', 'vaca.png', 'porco.png', 'ovelha.png', 'galinha.png', 'cavalo.png', 'cabra.png'],
-        distracoes: ['leao.png', 'tigre.png', 'elefante.png', 'girafa.png', 'lobo.png', 'raposa.png', 'cobra.png', 'zebra.png'],
-        quantidadeDistracoes: 11 
+        // Lista baseada na tua primeira imagem + vaca
+        alvos: [
+            'burro.png', 'cabra.png', 'cao.png', 'cavalo.png', 'coelho.png', 
+            'galinha.png', 'galo.png', 'gato.png', 'ovelha.png', 'pato.png', 
+            'peru.png', 'porco.png', 'vaca.png'
+        ],
+        // Lista baseada na tua segunda imagem
+        distracoes: [
+            'abelha.png', 'abutre.png', 'aguia.png', 'aranha.png', 'avestruz.png', 
+            'baleia.png', 'borboleta.png', 'canguru.png', 'caracol.png', 'caranguejo.png', 
+            'coala.png', 'crocodilo.png', 'elefante.png', 'foca.png', 'formiga.png', 
+            'girafa.png', 'gorila.png', 'hiena.png', 'hipopotamo.png', 'leao.png', 
+            'lobo.png', 'macaco.png', 'morcego.png', 'panda.png', 'papagaio.png', 
+            'peixe.png', 'polvo.png', 'pombo.png', 'raia.png', 'raposa.png', 
+            'rato.png', 'rinoceronte.png', 'tartaruga.png', 'texugo.png', 'tigre.png', 
+            'tubarao.png', 'tucano.png', 'urso.png', 'zebra.png'
+        ],
+        quantidadeDistracoes: 11 // Total de 12 animais no ecrã
     },
     "nivel2": {
         nome: "Nível 2",
@@ -21,8 +36,23 @@ const JOGO_CATEGORIAS = {
         tipoAlvo: "selvagem",
         pastaAlvos: "animaisselvagens/",
         pastaDistracoes: "animaisdomesticos/",
-        alvos: ['leao.png', 'tigre.png', 'elefante.png', 'girafa.png', 'lobo.png', 'raposa.png', 'cobra.png', 'jacare.png', 'urso.png', 'zebra.png'],
-        distracoes: ['cao.png', 'gato.png', 'vaca.png', 'porco.png', 'ovelha.png', 'galinha.png', 'cavalo.png', 'cabra.png'],
-        quantidadeDistracoes: 11 
+        // No nível 2, os alvos são os selvagens
+        alvos: [
+            'abelha.png', 'abutre.png', 'aguia.png', 'aranha.png', 'avestruz.png', 
+            'baleia.png', 'borboleta.png', 'canguru.png', 'caracol.png', 'caranguejo.png', 
+            'coala.png', 'crocodilo.png', 'elefante.png', 'foca.png', 'formiga.png', 
+            'girafa.png', 'gorila.png', 'hiena.png', 'hipopotamo.png', 'leao.png', 
+            'lobo.png', 'macaco.png', 'morcego.png', 'panda.png', 'papagaio.png', 
+            'peixe.png', 'polvo.png', 'pombo.png', 'raia.png', 'raposa.png', 
+            'rato.png', 'rinoceronte.png', 'tartaruga.png', 'texugo.png', 'tigre.png', 
+            'tubarao.png', 'tucano.png', 'urso.png', 'zebra.png'
+        ],
+        // E as distrações são os domésticos
+        distracoes: [
+            'burro.png', 'cabra.png', 'cao.png', 'cavalo.png', 'coelho.png', 
+            'galinha.png', 'galo.png', 'gato.png', 'ovelha.png', 'pato.png', 
+            'peru.png', 'porco.png', 'vaca.png'
+        ],
+        quantidadeDistracoes: 11 // Total de 12 animais no ecrã
     }
 };
