51d80b4b621f7b67499321775965295f0b086670
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-14 13:57:28 +0100

    Update categorias.js

--- 1/em/descobreanimais/categorias.js
+++ 1/em/descobreanimais/categorias.js
@@ -2,25 +2,27 @@ const JOGO_CATEGORIAS = {
     "nivel1": {
         nome: "Nível 1",
         imgCapa: "cnivel1.png",
-        titulo: "Explorador Iniciante",
-        descricao: "Usa a lanterna para encontrar os animais domésticos escondidos no escuro!",
+        titulo: "Explorador de Quintas",
+        descricao: "Usa a lanterna para encontrar os animais DOMÉSTICOS!",
         totalRondas: 10,
+        tipoAlvo: "domestico", // O que o jogador deve procurar
         pastaAlvos: "animaisdomesticos/",
         pastaDistracoes: "animaisselvagens/",
         alvos: ['cao.png', 'gato.png', 'vaca.png', 'porco.png', 'ovelha.png', 'galinha.png', 'cavalo.png', 'cabra.png'],
-        distracoes: ['leao.png', 'tigre.png', 'elefante.png', 'girafa.png', 'lobo.png', 'raposa.png'],
-        quantidadeDistracoes: 5 
+        distracoes: ['leao.png', 'tigre.png', 'elefante.png', 'girafa.png', 'lobo.png', 'raposa.png', 'cobra.png', 'zebra.png'],
+        quantidadeDistracoes: 8 
     },
     "nivel2": {
         nome: "Nível 2",
         imgCapa: "cnivel2.png",
-        titulo: "Mestre da Selva",
-        descricao: "Cuidado! Agora há muito mais animais selvagens para te confundir!",
+        titulo: "Explorador da Selva",
+        descricao: "Agora o desafio mudou! Encontra os animais SELVAGENS!",
         totalRondas: 10,
-        pastaAlvos: "animaisdomesticos/",
-        pastaDistracoes: "animaisselvagens/",
-        alvos: ['cao.png', 'gato.png', 'vaca.png', 'porco.png', 'ovelha.png', 'galinha.png', 'cavalo.png', 'cabra.png'],
-        distracoes: ['leao.png', 'tigre.png', 'elefante.png', 'girafa.png', 'lobo.png', 'raposa.png', 'cobra.png', 'jacare.png', 'urso.png', 'zebra.png'],
+        tipoAlvo: "selvagem", // O que o jogador deve procurar
+        pastaAlvos: "animaisselvagens/",
+        pastaDistracoes: "animaisdomesticos/",
+        alvos: ['leao.png', 'tigre.png', 'elefante.png', 'girafa.png', 'lobo.png', 'raposa.png', 'cobra.png', 'jacare.png', 'urso.png', 'zebra.png'],
+        distracoes: ['cao.png', 'gato.png', 'vaca.png', 'porco.png', 'ovelha.png', 'galinha.png', 'cavalo.png', 'cabra.png'],
         quantidadeDistracoes: 12 
     }
 };
