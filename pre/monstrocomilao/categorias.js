a9fc6a97cc3bde61582c9365fa5cee0b34e68080
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-17 19:46:46 +0100

    Update categorias.js

--- pre/monstrocomilao/categorias.js
+++ pre/monstrocomilao/categorias.js
@@ -1,34 +1,16 @@
 const JOGO_CATEGORIAS = {
     saudavel: {
         nome: "Comida Saudável",
-        descricao: "Alimenta o monstrinho apenas com alimentos SAUDÁVEIS!",
+        descricao: "Dá apenas ao monstrinho alimentos SAUDÁVEIS!",
         imgCapa: "comida/maca.png",
-        // Alvos: agua, alface, banana, broculos, cenoura, couve, ervilhas, figo, kiwi, laranja, maca, morango, pera
-        alvos: [
-            'agua.png', 'alface.png', 'banana.png', 'broculos.png', 'cenoura.png', 
-            'couve.png', 'ervilhas.png', 'figo.png', 'kiwi.png', 'laranja.png', 
-            'maca.png', 'morango.png', 'pera.png'
-        ],
-        // Distrações: Doces e Refrigerantes
-        distracoes: [
-            'bolo.png', 'chocolate.png', 'chupa.png', 'cocacola.png', 'doce.png', 
-            'goma.png', 'goma1.png', 'queque.png', 'rebucado.png', 'rebucado1.png', 'soda.png'
-        ]
+        alvos: ['agua.png', 'alface.png', 'banana.png', 'broculos.png', 'cenoura.png', 'couve.png', 'ervilhas.png', 'figo.png', 'kiwi.png', 'laranja.png', 'maca.png', 'morango.png', 'pera.png'],
+        distracoes: ['bolo.png', 'chocolate.png', 'chupa.png', 'cocacola.png', 'doce.png', 'goma.png', 'goma1.png', 'queque.png', 'rebucado.png', 'rebucado1.png', 'soda.png']
     },
     doces: {
         nome: "Doces",
-        descricao: "O monstrinho hoje só quer comer DOCES e GULOSEIMAS!",
+        descricao: "Dá apenas ao monstrinho DOCES!",
         imgCapa: "comida/chocolate.png",
-        // Alvos: bolo, chocolate, chupa, cocacola, doce, goma, goma1, queque, rebucado, rebucado1, soda
-        alvos: [
-            'bolo.png', 'chocolate.png', 'chupa.png', 'cocacola.png', 'doce.png', 
-            'goma.png', 'goma1.png', 'queque.png', 'rebucado.png', 'rebucado1.png', 'soda.png'
-        ],
-        // Distrações: Saudáveis
-        distracoes: [
-            'agua.png', 'alface.png', 'banana.png', 'broculos.png', 'cenoura.png', 
-            'couve.png', 'ervilhas.png', 'figo.png', 'kiwi.png', 'laranja.png', 
-            'maca.png', 'morango.png', 'pera.png'
-        ]
+        alvos: ['bolo.png', 'chocolate.png', 'chupa.png', 'cocacola.png', 'doce.png', 'goma.png', 'goma1.png', 'queque.png', 'rebucado.png', 'rebucado1.png', 'soda.png'],
+        distracoes: ['agua.png', 'alface.png', 'banana.png', 'broculos.png', 'cenoura.png', 'couve.png', 'ervilhas.png', 'figo.png', 'kiwi.png', 'laranja.png', 'maca.png', 'morango.png', 'pera.png']
     }
 };
