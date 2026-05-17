f325b7bc78de75c4b9c43d1b36cf701126045c0b
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-17 16:04:12 +0100

    Update categorias.js

--- pre/chuva_letras/categorias.js
+++ pre/chuva_letras/categorias.js
@@ -1,1 +1,16 @@
-
+const JOGO_CATEGORIAS = {
+    maiusculas: {
+        nome: "Letras Maiúsculas",
+        descricao: "Clica nas letras maiúsculas que aparecem a cair!",
+        imgCapa: "letras_abc.png", // Imagem na tua pasta img
+        letras: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+        tipo: "upper"
+    },
+    minusculas: {
+        nome: "Letras Minúsculas",
+        descricao: "Clica nas letras minúsculas que aparecem a cair!",
+        imgCapa: "letras_abc_min.png", // Imagem na tua pasta img
+        letras: "abcdefghijklmnopqrstuvwxyz".split(""),
+        tipo: "lower"
+    }
+};
