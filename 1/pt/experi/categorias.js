072b03ba83e150fbf451e130d8293188b9b6bd4e
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-18 23:43:42 +0100

    Update categorias.js

--- 1/pt/experi/categorias.js
+++ 1/pt/experi/categorias.js
@@ -1,1 +1,16 @@
-
+const JOGO_CATEGORIAS = {
+    nivel1: {
+        nome: "Animais",
+        descricao: "Observa a metade do animal e encontra a outra parte para o completar!",
+        imgCapa: "animaisdomesticos/cao.png",
+        pasta: "animaisdomesticos/", // Podes misturar com selvagens se preferires
+        itens: ['cao.png', 'gato.png', 'vaca.png', 'porco.png', 'ovelha.png', 'galinha.png', 'cavalo.png', 'burro.png', 'coelho.png', 'leao.png', 'tigre.png', 'elefante.png']
+    },
+    nivel2: {
+        nome: "Frutas",
+        descricao: "Consegues completar estas frutas? Escolhe a metade que falta!",
+        imgCapa: "comida/maca.png",
+        pasta: "comida/",
+        itens: ['maca.png', 'banana.png', 'laranja.png', 'morango.png', 'pera.png', 'kiwi.png', 'figo.png', 'alface.png', 'broculos.png', 'cenoura.png']
+    }
+};
