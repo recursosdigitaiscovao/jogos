9b1789bba4ef3d800061b6e319f895051cd6820f
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-20 00:38:43 +0100

    Update categorias.js

--- 2/pt/divisao_silabica/categorias.js
+++ 2/pt/divisao_silabica/categorias.js
@@ -1,18 +1,18 @@
 const JOGO_CATEGORIAS = {
     simples: {
         nome: "Palavras Simples",
-        descricao: "Divide as palavras pedacinho a pedacinho (sílabas)!",
+        descricao: "Ouve os sons e divide as palavras corretamente!",
         imgCapa: "icones/balao.png",
         itens: [
             { palavra: "BONECA", divisao: "BO-NE-CA", opcoes: ["BON-E-CA", "BO-NE-CA", "BONE-CA"] },
             { palavra: "PIPOCA", divisao: "PI-PO-CA", opcoes: ["PI-PO-CA", "PIP-O-CA", "PI-POC-A"] },
             { palavra: "SAPATO", divisao: "SA-PA-TO", opcoes: ["SAP-A-TO", "SA-PAT-O", "SA-PA-TO"] },
-            { palavra: "GAVETA", divisao: "GA-VE-TA", opcoes: ["GAV-E-TA", "GA-VE-TA", "GA-VET-A"] }
+            { palavra: "CANETA", divisao: "CA-NE-TA", opcoes: ["CA-NET-A", "CA-NE-TA", "CAN-E-TA"] }
         ]
     },
     gemeos: {
-        nome: "Os Gémeos (RR/SS)",
-        descricao: "Regra: No nosso jogo, o RR e o SS ficam sempre de mãos dadas!",
+        nome: "O RR e o SS",
+        descricao: "Regra: O RR e o SS ficam sempre na mesma sílaba. CA-RRO!",
         imgCapa: "icones/carro.png",
         itens: [
             { palavra: "CARRO", divisao: "CA-RRO", opcoes: ["CAR-RO", "CA-RRO", "CARR-O"] },
@@ -23,14 +23,13 @@ const JOGO_CATEGORIAS = {
         ]
     },
     inseparaveis: {
-        nome: "Inseparáveis (CH/LH/NH)",
-        descricao: "Regra: O CH, LH e NH nunca se separam! Ficam sempre juntos.",
+        nome: "CH, LH e NH",
+        descricao: "Regra: O CH, LH e NH nunca se separam! CHU-VA!",
         imgCapa: "icones/coelho.png",
         itens: [
             { palavra: "CHUVA", divisao: "CHU-VA", opcoes: ["CH-U-VA", "CHU-VA", "CHUV-A"] },
             { palavra: "COELHO", divisao: "CO-E-LHO", opcoes: ["CO-EL-HO", "CO-E-LHO", "COE-LHO"] },
             { palavra: "GALINHA", divisao: "GA-LI-NHA", opcoes: ["GA-LIN-HA", "GA-LI-NHA", "GAL-I-NHA"] },
-            { palavra: "NINHO", divisao: "NI-NHO", opcoes: ["NIN-HO", "NI-NHO", "NI-NH-O"] },
             { palavra: "PALHAÇO", divisao: "PA-LHA-ÇO", opcoes: ["PAL-HA-ÇO", "PA-LHA-ÇO", "PA-LH-AÇO"] }
         ]
     }
