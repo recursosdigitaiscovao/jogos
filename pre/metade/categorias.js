45053283b8c0a665261fe05f975ea96a9bc250bc
recursosdigitaiscovao <recursosdigitaiscovao@gmail.com>
2026-05-19 23:43:39 +0100

    Update categorias.js

--- pre/metade/categorias.js
+++ pre/metade/categorias.js
@@ -1,1 +1,16 @@
-
+const JOGO_CATEGORIAS = {
+    nivel1: {
+        nome: "Animais",
+        descricao: "Observa a metade do animal e encontra a outra parte para o completar!",
+        imgCapa: "animaisdomesticos/cao.png",
+        pasta: "animaisdomesticos/", // Podes misturar com selvagens se preferires
+        itens: ['cao.png', 'gato.png', 'vaca.png', 'porco.png', 'ovelha.png', 'galinha.png', 'cavalo.png', 'burro.png', 'galo.png', 'peru.png', 'pato.png', 'cabra.png']
+    },
+    nivel2: {
+        nome: "Frutas",
+        descricao: "Consegues completar estas frutas? Escolhe a metade que falta!",
+        imgCapa: "frutas/maca.png",
+        pasta: "comida/",
+        itens: ['maca.png', 'banana.png', 'laranja.png', 'morango.png', 'pera.png', 'kiwi.png', 'figo.png', 'diospiro.png', 'castanha.png', 'amora.png']
+    }
+};
